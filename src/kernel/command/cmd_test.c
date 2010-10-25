5adc0b882e2c816efc4dabf463e5b666f4579426
jiang <jerryjianjun@gmail.com>
2010-10-25 15:53:43 +0800

    add default font

--- src/kernel/command/cmd_test.c
+++ src/kernel/command/cmd_test.c
@@ -51,6 +51,66 @@
 
 static x_s32 test(x_s32 argc, const x_s8 **argv)
 {
+	struct fb * fb;
+	struct font_glyph * glyph;
+	struct color * c;
+	x_u32 color;
+	x_s32 i;
+
+	fb = search_framebuffer("fb");
+	if(!fb)
+	{
+		printk("no found framebuffer device\r\n");
+		return -1;
+	}
+
+/*
+	glyph = malloc(sizeof(struct font_glyph));
+	if(!glyph)
+	{
+		printk("malloc glyph fail\r\n");
+		return -1;
+	}
+
+	c = get_color_by_name("red");
+	color = fb_map_color(fb, c->r, c->g, c->b, c->a);
+	fb_fill_rect(fb, color, 0, 0, 100, 100);
+*/
+
+	c = get_color_by_name("mediumturquoise");
+	color = fb_map_color(fb, c->r, c->g, c->b, c->a);
+	fb_draw_text(fb, "abcdefghLLLasdf阿哦俄", NULL, color, 100-4, 100-8);
+
+
+/*
+	c = get_color_by_name("peru");
+	color = fb_map_color(fb, c->r, c->g, c->b, c->a);
+
+	for(i=0; i< 800; i++)
+	{
+		bitmap_set_pixel(&(fb->info->bitmap), i, 100, color);
+		bitmap_set_pixel(&(fb->info->bitmap), i, 300, color);
+	}
+	for(i=0; i< 480; i++)
+	{
+		bitmap_set_pixel(&(fb->info->bitmap), 100, i, color);
+		bitmap_set_pixel(&(fb->info->bitmap), 300, i, color);
+	}
+
+	struct rect rect, to;
+	rect_set(&rect, 100,100, 300, 300);
+
+	//rect_set(&to, 100+20,100+20, 100+50, 100+80);
+
+	rect_set(&to, 20,20, 50+300, 80);
+
+	rect_align(&rect, &to, ALIGN_LEFT_TOP);
+
+	c = get_color_by_name("yellow");
+	color = fb_map_color(fb, c->r, c->g, c->b, c->a);
+	fb_fill_rect(fb, color, to.left, to.top, to.right- to.left, to.bottom - to.top);
+*/
+
 	return 0;
 }
 

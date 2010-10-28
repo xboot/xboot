b8869811e50ee70b0270d6cfb52bae3a4678988f
jiang <jerryjianjun@gmail.com>
2010-10-28 18:38:02 +0800

    console

--- src/include/console/console.h
+++ src/include/console/console.h
@@ -5,18 +5,69 @@
 #include <default.h>
 
 /*
- * define the struct of input console
+ * console color enum type.
  */
-struct console_input
+enum console_color {
+	CONSOLE_NONE,
+	CONSOLE_BLACK,
+	CONSOLE_RED,
+	CONSOLE_GREEN,
+	CONSOLE_YELLOW,
+	CONSOLE_BULE,
+	CONSOLE_MAGENTA,
+	CONSOLE_CYAN,
+	CONSOLE_WHITE
+};
+
+/*
+ * define the struct of console
+ */
+struct console
 {
-	/* the name of input console */
+	/* the name of console */
 	char * name;
 
-	/* console read */
-	x_s32 (*read)(struct console_input * input, x_u8 * buf, x_s32 count);
+	/* get console's width and height */
+	x_bool(*getwh)(struct console * console, x_s32 * w, x_s32 * h);
+
+	/* get cursor position */
+	x_bool(*getxy)(struct console * console, x_s32 * x, x_s32 * y);
+
+	/* set cursor position */
+	x_bool(*gotoxy)(struct console * console, x_s32 x, x_s32 y);
+
+	/* turn on/off the cursor */
+	x_bool(*setcursor)(struct console * console, x_bool on);
+
+	/* set console's front color and background color */
+	x_bool(*setcolor)(struct console * console, enum console_color f, enum console_color b);
+
+	/* get console front color and background color */
+	x_bool(*getcolor)(struct console * console, enum console_color * f, enum console_color * b);
+
+	/* clear screen */
+	x_bool(*cls)(struct console * console);
+
+	/* update the screen */
+	x_bool(*refresh)(struct console * console);
+
+	/* get a unicode character */
+	x_u32(*getchar)(struct console * console);
+
+	/* put a unicode character */
+	x_bool(*putchar)(struct console * console, x_u32 c);
 
 	/* private data */
 	void * priv;
+};
+
+/*
+ * the list of console.
+ */
+struct console_list
+{
+	struct console * console;
+	struct hlist_node node;
 };
 
 #endif /* __CONSOLE_H__ */

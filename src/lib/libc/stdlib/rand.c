3c754ef375b7a0107abcbd7317409d58788f6c63
jiang jianjun <jerryjianjun@gmail.com>
2011-05-04 17:52:14 +0800

    remove comment for rand.c

--- src/lib/libc/stdlib/rand.c
+++ src/lib/libc/stdlib/rand.c
@@ -4,22 +4,14 @@
 
 #include <stdlib.h>
 
-/*
- * the rand poll.
- */
+
 static int hold_rand = 1;
 
-/*
- * seed rand
- */
 void srand(int seed)
 {
 	hold_rand = seed;
 }
 
-/*
- * rand
- */
 int rand(void)
 {
 	return (((hold_rand = hold_rand * 214013L + 2531011L) >> 16) & 0x7fff);

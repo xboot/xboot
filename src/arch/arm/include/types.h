aa1a5aece70fef84c2dd55be7a14ee0c9646d6db
jiang jianjun <jerryjianjun@gmail.com>
2011-05-06 23:55:06 +0800

    move types.h to arch

---
+++ src/arch/arm/include/types.h
@@ -0,0 +1,21 @@
+#ifndef __ARM_TYPES_H__
+#define __ARM_TYPES_H__
+
+typedef signed char			__s8;
+typedef unsigned char		__u8;
+
+typedef signed short		__s16;
+typedef unsigned short		__u16;
+
+typedef signed int			__s32;
+typedef unsigned int		__u32;
+
+typedef signed long long	__s64;
+typedef unsigned long long	__u64;
+
+typedef unsigned long		size_t;
+typedef signed long			ssize_t;
+typedef signed long			ptrdiff_t;
+
+
+#endif /* __ARM_TYPES_H__tytypa */
--- src/include/types.h
+++
@@ -1,5 +0,0 @@
-#ifndef __TYPES_H__
-#define __TYPES_H__
-
-
-#endif /* __TYPES_H__ */

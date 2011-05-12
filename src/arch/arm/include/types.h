6d9b4255dab0c83d764cc9e8b4758d96a01e4b3f
Jiang jianjun <jjj@ThinkPad.(none)>
2011-05-12 10:22:03 +0800

    fix format

--- src/arch/arm/include/types.h
+++ src/arch/arm/include/types.h
@@ -5,27 +5,27 @@
 typedef signed char				s8_t;
 typedef unsigned char			u8_t;
 
-typedef signed short				s16_t;
+typedef signed short			s16_t;
 typedef unsigned short			u16_t;
 
 typedef signed int				s32_t;
-typedef unsigned int				u32_t;
+typedef unsigned int			u32_t;
 
-typedef signed long long			s64_t;
+typedef signed long long		s64_t;
 typedef unsigned long long		u64_t;
 
 typedef signed int				ptrdiff_t;
 typedef signed int				intptr_t;
 typedef unsigned int 			uintptr_t;
 
-typedef unsigned int				size_t;
+typedef unsigned int			size_t;
 typedef signed int				ssize_t;
 
 typedef signed int				off_t;
-typedef signed long long			loff_t;
+typedef signed long long		loff_t;
 
 typedef signed int				bool_t;
 typedef signed int				register_t;
 
 
-#endif /* __ARM_TYPES_H__tytypa */
+#endif /* __ARM_TYPES_H__ */
--- src/arch/arm/mach-realview/include/configs.h
+++ src/arch/arm/mach-realview/include/configs.h
@@ -4,19 +4,19 @@
 /*
  * define the basic data types.
  */
-typedef signed char				x_s8;
-typedef unsigned char			x_u8;
+typedef signed char					x_s8;
+typedef unsigned char				x_u8;
 
 typedef signed short int			x_s16;
-typedef unsigned short int		x_u16;
+typedef unsigned short int			x_u16;
 
-typedef long signed int			x_s32;
-typedef long unsigned int		x_u32;
+typedef long signed int				x_s32;
+typedef long unsigned int			x_u32;
 
 typedef signed long long			x_s64;
-typedef unsigned long long		x_u64;
+typedef unsigned long long			x_u64;
 
-typedef signed int				x_bool;
+typedef signed int					x_bool;
 typedef unsigned int				x_sys;
 typedef signed long long			x_size;
 typedef signed long long			x_off;
@@ -26,13 +26,13 @@ typedef void*						x_addr;
  * define the format of endian, little endian or big endian.
  */
 #define		__LITTLE_ENDIAN
-#undef			__BIG_ENDIAN
+#undef		__BIG_ENDIAN
 
 /*
  * define the bits of system, 32bits or 64bits.
  */
 #define		__SYS_32BIT
-#undef			__SYS_64BIT
+#undef		__SYS_64BIT
 
 /*
  * malloc heap size

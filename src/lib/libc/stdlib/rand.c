6e872d7d040a5d12daaa4c3edb9b438e6b278c24
jerryjianjun <jerryjianjun@gmail.com>
2011-05-26 20:55:25 +0800

    fix rand function

--- src/lib/libc/stdlib/rand.c
+++ src/lib/libc/stdlib/rand.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 
-static unsigned short __rand48_seed[3];
+static unsigned short __rand48_seed[3] = {1, 1, 1};
 
 static long jrand48(unsigned short xsubi[3])
 {
@@ -14,29 +14,29 @@ static long jrand48(unsigned short xsubi[3])
 	/*
 	 * The xsubi[] array is little endian by spec
 	 */
-	x = (u64_t) (u16_t) xsubi[0] +
-	    ((u64_t) (u16_t) xsubi[1] << 16) +
-	    ((u64_t) (u16_t) xsubi[2] << 32);
+	x = (u64_t) (u16_t)xsubi[0] +
+	    ((u64_t) (u16_t)xsubi[1] << 16) +
+	    ((u64_t) (u16_t)xsubi[2] << 32);
 
 	x = (0x5deece66dULL * x) + 0xb;
 
-	xsubi[0] = (unsigned short)(u16_t) x;
-	xsubi[1] = (unsigned short)(u16_t) (x >> 16);
-	xsubi[2] = (unsigned short)(u16_t) (x >> 32);
+	xsubi[0] = (unsigned short)(u16_t)x;
+	xsubi[1] = (unsigned short)(u16_t)(x >> 16);
+	xsubi[2] = (unsigned short)(u16_t)(x >> 32);
 
-	return (long)(s32_t) (x >> 16);
+	return (long)(s32_t)(x >> 16);
 }
 
 static long lrand48(void)
 {
-	return (u32_t) jrand48(__rand48_seed) >> 1;
+	return (u32_t)jrand48(__rand48_seed) >> 1;
 }
 
 static void srand48(long seedval)
 {
 	__rand48_seed[0] = 0x330e;
 	__rand48_seed[1] = (unsigned short)seedval;
-	__rand48_seed[2] = (unsigned short)((u32_t) seedval >> 16);
+	__rand48_seed[2] = (unsigned short)((u32_t)seedval >> 16);
 }
 
 int rand(void)

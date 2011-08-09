cf1d3b4574a96c84d7a0e8488cf28090ee1cfd8c
jiang jianjun <jerryjianjun@gmail.com>
2011-08-09 17:46:25 +0800

    fix mkv310, with no secure booting

--- documents/s5pv310/mkv310/mkv310.c
+++ documents/s5pv310/mkv310/mkv310.c
@@ -2,20 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-/*
- * IROM code reads first 14K bytes from boot device.
- * It then calculates the checksum of 14K-4 bytes and compare with data at
- * 14K-4 offset.
- */
-
-#define CHECKSUM_OFFSET		(14*1024 - 4)
-#define BUFSIZE			(16*1024)
+#define BUFFER_SIZE		(16 * 1024)
+#define HEADER_SIZE		(16)
 
 int main (int argc, char *argv[])
 {
-	FILE * fp;	
-	unsigned char buffer[BUFSIZE];
-	unsigned char header[4];
+	FILE * fp;
+	unsigned char buffer[BUFFER_SIZE];
+	unsigned char header[HEADER_SIZE];
 	unsigned int checksum, count;
 	int i, len;
 
@@ -34,7 +28,7 @@ int main (int argc, char *argv[])
 
 	fseek(fp, 0L, SEEK_END);
 	len = ftell(fp);
-	count = (len < CHECKSUM_OFFSET) ? len : CHECKSUM_OFFSET;
+	count = (len < BUFFER_SIZE) ? len : BUFFER_SIZE;
 
 	fseek(fp, 0L, SEEK_SET);
 	memset(buffer, 0, sizeof(buffer));
@@ -45,17 +39,44 @@ int main (int argc, char *argv[])
 		return -1;
 	}
 
-	for (i = 0, checksum = 0; i < CHECKSUM_OFFSET; i++)
+	for(i = 16, checksum = 0; i < count; i++)
 	{
 		checksum += buffer[i] & 0xff;
 	}
 
-	header[3] = (checksum >> 24) & 0xff;
-	header[2] = (checksum >> 16) & 0xff;
-	header[1] = (checksum >> 8) & 0xff;
-	header[0] = (checksum >> 0) & 0xff;
+	header[3] = (0x1f >> 24) & 0xff;
+	header[2] = (0x1f >> 16) & 0xff;
+	header[1] = (0x1f >> 8) & 0xff;
+	header[0] = (0x1f >> 0) & 0xff;
 
-	fseek(fp, CHECKSUM_OFFSET, SEEK_SET);
+	header[7] = (checksum >> 24) & 0xff;
+	header[6] = (checksum >> 16) & 0xff;
+	header[5] = (checksum >> 8) & 0xff;
+	header[4] = (checksum >> 0) & 0xff;
+
+	header[0] ^= 0x53;
+	header[1] ^= 0x35;
+	header[2] ^= 0x50;
+	header[3] ^= 0x43;
+	header[4] ^= 0x32;
+	header[5] ^= 0x31;
+	header[6] ^= 0x30;
+	header[7] ^= 0x20;
+	header[8] ^= 0x48;
+	header[9] ^= 0x45;
+	header[10] ^= 0x41;
+	header[11] ^= 0x44;
+	header[12] ^= 0x45;
+	header[13] ^= 0x52;
+	header[14] ^= 0x20;
+	header[15] ^= 0x20;
+
+	for (i = 1; i < HEADER_SIZE; i++)
+	{
+		header[i] ^= header[i-1];
+	}
+
+	fseek(fp, 0L, SEEK_SET);
 	if(fwrite(header, 1, sizeof(header), fp) != sizeof(header))
 	{
 		printf("Write header error %s\n", argv[1]);
@@ -65,6 +86,6 @@ int main (int argc, char *argv[])
 
 	fclose(fp);
 
-	printf("The checksum is 0x%08x for %d bytes [0x%08x ~ 0x%08x]\n", checksum, count, 0, count - 1);
+	printf("The checksum is 0x%08x for %d bytes [0x%08x ~ 0x%08x]\n", checksum, (count - sizeof(header)), sizeof(header), (count - 1));
 	return 0;
 }

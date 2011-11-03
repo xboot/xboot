df37d8eda833ec9744c50eb2050e0a9ee0ab42a2
jiang jianjun <jerryjianjun@gmail.com>
2011-11-03 11:30:33 +0800

    fix smdk6410

--- src/arch/arm/mach-smdk6410/mach-smdk6410.c
+++ src/arch/arm/mach-smdk6410/mach-smdk6410.c
@@ -1,5 +1,5 @@
 /*
- * arch/arm/mach-smdk6410/mach-smdk6410.c
+ * mach-smdk6410.c
  *
  * Copyright (c) 2007-2010  jianjun jiang <jerryjianjun@gmail.com>
  * official site: http://xboot.org
@@ -21,14 +21,6 @@
  */
 
 #include <xboot.h>
-#include <types.h>
-#include <sizes.h>
-#include <io.h>
-#include <mode/mode.h>
-#include <xboot/log.h>
-#include <xboot/printk.h>
-#include <xboot/machine.h>
-#include <xboot/initcall.h>
 #include <s3c6410-cp15.h>
 #include <s3c6410/reg-gpio.h>
 #include <s3c6410/reg-wdg.h>
@@ -49,9 +41,6 @@ extern u8_t __heap_end[];
 extern u8_t __stack_start[];
 extern u8_t __stack_end[];
 
-/*
- * system initial, like power lock
- */
 static void mach_init(void)
 {
 	/* GPK13 and GPK15 power lock */
@@ -63,17 +52,16 @@ static void mach_init(void)
 	writel(S3C6410_GPKDAT, (readl(S3C6410_GPKDAT) & ~(0x1<<15)) | (0x1<<15));
 }
 
-/*
- * system halt, shutdown machine.
- */
+static bool_t mach_sleep(void)
+{
+	return FALSE;
+}
+
 static bool_t mach_halt(void)
 {
 	return TRUE;
 }
 
-/*
- * reset the cpu by setting up the watchdog timer and let him time out
- */
 static bool_t mach_reset(void)
 {
 	/* disable watchdog */
@@ -88,18 +76,26 @@ static bool_t mach_reset(void)
 	return TRUE;
 }
 
-/*
- * get system mode
- */
-static enum mode mach_getmode(void)
+static enum mode_t mach_getmode(void)
 {
 	return MODE_MENU;
 }
 
-/*
- * clean up system before running os.
- */
-bool_t mach_cleanup(void)
+static bool_t mach_batinfo(struct battery_info * info)
+{
+	if(!info)
+		return FALSE;
+
+	info->charging = FALSE;
+	info->voltage = 3700;
+	info->current = 300;
+	info->temperature = 200;
+	info->capacity = 100;
+
+	return TRUE;
+}
+
+static bool_t mach_cleanup(void)
 {
 	/* disable irq */
 	irq_disable();
@@ -122,16 +118,13 @@ bool_t mach_cleanup(void)
 	return TRUE;
 }
 
-/*
- * for anti-piracy
- */
-static bool_t mach_genuine(void)
+static bool_t mach_authentication(void)
 {
 	return TRUE;
 }
 
 /*
- * a portable data interface for machine.
+ * A portable machine interface.
  */
 static struct machine smdk6410 = {
 	.info = {
@@ -185,16 +178,16 @@ static struct machine smdk6410 = {
 
 	.pm = {
 		.init 				= mach_init,
-		.suspend			= NULL,
-		.resume				= NULL,
+		.sleep				= mach_sleep,
 		.halt				= mach_halt,
 		.reset				= mach_reset,
 	},
 
 	.misc = {
 		.getmode			= mach_getmode,
+		.batinfo			= mach_batinfo,
 		.cleanup			= mach_cleanup,
-		.genuine			= mach_genuine,
+		.authentication		= mach_authentication,
 	},
 
 	.priv					= NULL,

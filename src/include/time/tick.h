347c974fac4571ae8f517d9f79f01c775e940241
jerryjianjun <jerryjianjun@gmail.com>
2012-09-10 14:01:35 +0800

    add define time_after

--- src/include/time/tick.h
+++ src/include/time/tick.h
@@ -4,6 +4,14 @@
 
 #include <xboot.h>
 
+/*
+ * When HZ = 1000, The Max delay is 24 days
+ */
+#define time_after(a, b)		(((s32_t)(b) - (s32_t)(a) < 0))
+#define time_before(a, b)		time_after(b, a)
+#define time_after_eq(a,b)		(((s32_t)(a) - (s32_t)(b) >= 0))
+#define time_before_eq(a,b)		time_after_eq(b, a)
+
 extern volatile u32_t jiffies;
 
 /*

776ee55a0b3475cb78e91b1d0808b6360deed55a
jerryjianjun <jerryjianjun@gmail.com>
2012-10-23 16:32:49 +0800

    support for exynos4x12

--- src/arch/arm/mach-exynos4412/exynos4x12-irom.c
+++ src/arch/arm/mach-exynos4412/exynos4x12-irom.c
@@ -38,7 +38,40 @@
 #include <exynos4x12/reg-gpio.h>
 #include <exynos4x12/reg-pmu.h>
 
-#if 0
+inline static void reg_write(u32_t addr, u32_t value)
+{
+	( *((volatile u32_t *)(addr)) ) = value;
+}
+
+inline static u32_t reg_read(u32_t addr)
+{
+	return( *((volatile u32_t *)(addr)) );
+}
+
+inline void delay_test(int ms)
+{
+	int i, j;
+
+	for(i = 0; i < ms; i++)
+	for(j = 0; j < 100; j++);
+}
+
+void test_led(int ms)
+{
+	reg_write(EXYNOS4X12_GPD0CON, (reg_read(EXYNOS4X12_GPD0CON) & ~(0xf<<0)) | (0x1<<0));
+	reg_write(EXYNOS4X12_GPD0PUD, (reg_read(EXYNOS4X12_GPD0PUD) & ~(0x3<<0)) | (0x2<<0));
+	reg_write(EXYNOS4X12_GPD0DAT, (reg_read(EXYNOS4X12_GPD0DAT) & ~(0x1<<0)) | (0x0<<0));
+
+	while(1)
+	{
+		reg_write(EXYNOS4X12_GPD0DAT, (reg_read(EXYNOS4X12_GPD0DAT) & ~(0x1<<0)) | (0x0<<0));
+		delay_test(ms);
+
+		reg_write(EXYNOS4X12_GPD0DAT, (reg_read(EXYNOS4X12_GPD0DAT) & ~(0x1<<0)) | (0x1<<0));
+		delay_test(ms);
+	}
+}
+
 extern u8_t	__text_start[];
 extern u8_t __text_end[];
 extern u8_t __data_shadow_start[];
@@ -54,7 +87,7 @@ extern u8_t __stack_end[];
 
 #define irom_sdmmc_to_mem(sector, count, mem)		\
 		(((u32_t(*)(u32_t, u32_t, u32_t *))(*((u32_t *)(0x02020030))))(sector, count, mem))
-
+#if 0
 #define irom_emmc43_to_mem(size, mem)				\
 		(((u32_t(*)(u32_t, u32_t *))(*((u32_t *)(0x0202003c))))(size, mem))
 
@@ -78,6 +111,103 @@ static u32_t reg_read(u32_t addr)
  */
 void irom_copyself(void)
 {
+	u32_t om;
+	u32_t * mem;
+	u32_t size;
+
+	/*
+	 * read om register, om[5..1]
+	 */
+	om = (u32_t)((reg_read(EXYNOS4X12_PMU_OM_STAT) >> 1) & 0x1f);
+
+	/* SDMMC CH2 */
+	if(om == 0x2)
+	{
+		/*
+		 * the xboot's memory base address.
+		 */
+		mem = (u32_t *)__text_start;
+
+		/*
+		 * the size which will be copyed, the 'size' is
+		 * 1 : 256KB, 2 : 512KB, 3 : 768KB, 4 : 1024KB ...
+		 */
+		size = (__data_shadow_end - __text_start + 0x00040000) >> 18;
+
+		/*
+		 * how many blocks the 'size' is , 512 bytes per block.
+		 * size * 256 *1024 / 512 = size * 2^9 = size << 9
+		 */
+		size = size << 9;
+
+		/*
+		 * copy xboot to memory from sdmmc ch2.
+		 */
+		if(irom_sdmmc_to_mem(1, size, mem) == 0)
+		{
+			;//test_led(1000);
+		}
+		else
+		{
+			;//test_led(100);
+		}
+	}
+
+	/* eMMC43 CH0 */
+	else if(om == 0x3)
+	{
+
+	}
+
+	/* eMMC44 CH4 */
+	else if(om == 0x4)
+	{
+
+	}
+
+	/* NAND 512B 8ECC */
+	else if(om == 0x8)
+	{
+
+	}
+
+	/* NAND 2KB OVER */
+	else if(om == 0x9)
+	{
+
+	}
+
+	/*=============*/
+	/* eMMC43 CH0 */
+	else if(om == 0x13)
+	{
+
+	}
+
+	/* eMMC44 CH4 */
+	else if(om == 0x14)
+	{
+
+	}
+
+	/* NAND 512B 8ECC */
+	else if(om == 0x18)
+	{
+
+	}
+
+	/* NAND 2KB OVER */
+	else if(om == 0x19)
+	{
+
+	}
+
+	/* Not support */
+	else
+	{
+		return;
+	}
+
 #if 0
 	u32_t om;
 	u32_t * mem;
@@ -191,40 +321,4 @@ void irom_copyself(void)
 		return;
 	}
 #endif
-}
-
-
-
-inline static void reg_write(u32_t addr, u32_t value)
-{
-	( *((volatile u32_t *)(addr)) ) = value;
-}
-
-inline static u32_t reg_read(u32_t addr)
-{
-	return( *((volatile u32_t *)(addr)) );
-}
-
-inline void delay_test(void)
-{
-	int i, j;
-
-	for(i = 0; i < 1000; i++)
-	for(j = 0; j < 100; j++);
-}
-
-void test_led(void)
-{
-	reg_write(EXYNOS4X12_GPD0CON, (reg_read(EXYNOS4X12_GPD0CON) & ~(0xf<<0)) | (0x1<<0));
-	reg_write(EXYNOS4X12_GPD0PUD, (reg_read(EXYNOS4X12_GPD0PUD) & ~(0x3<<0)) | (0x2<<0));
-	reg_write(EXYNOS4X12_GPD0DAT, (reg_read(EXYNOS4X12_GPD0DAT) & ~(0x1<<0)) | (0x0<<0));
-
-	while(1)
-	{
-		reg_write(EXYNOS4X12_GPD0DAT, (reg_read(EXYNOS4X12_GPD0DAT) & ~(0x1<<0)) | (0x0<<0));
-		delay_test();
-
-		reg_write(EXYNOS4X12_GPD0DAT, (reg_read(EXYNOS4X12_GPD0DAT) & ~(0x1<<0)) | (0x1<<0));
-		delay_test();
-	}
 }

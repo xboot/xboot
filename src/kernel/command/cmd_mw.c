84611fbda532dba9e26b8940c16a7b202cf81ba9
jerryjianjun <jerryjianjun@gmail.com>
2012-11-20 22:56:27 +0800

    change mail to <jerryjianjun@gmail.com>

--- src/kernel/command/cmd_mw.c
+++ src/kernel/command/cmd_mw.c
@@ -1,7 +1,7 @@
 /*
  * kernel/command/cmd_mw.c
  *
- * Copyright (c) 2007-2012  jianjun jiang <jjjstudio@gmail.com>
+ * Copyright (c) 2007-2012  jianjun jiang <jerryjianjun@gmail.com>
  * official site: http://xboot.org
  *
  * This program is free software; you can redistribute it and/or modify

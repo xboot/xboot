2179943363b2b770669147d7c2afe72c8882ad55
jerryjianjun <jerryjianjun@gmail.com>
2012-12-10 17:36:49 +0800

    fix boot.lua

--- src/romdisk/boot.lua
+++ src/romdisk/boot.lua
@@ -1,134 +1,23 @@
---[[
---]]
-
--- Make sure xboot table exists.
+-- Make sure xboot table exists
 if not xboot then xboot = {} end
 
--- Used for setup:
-xboot.path = {}
---xboot.arg = {}
-
--- Unparsed arguments:
---argv = {}
-
--- Replace any \ with /.
-function xboot.path.normalslashes(p)
-	return string.gsub(p, "\\", "/")
-end
-
--- Makes sure there is a slash at the end
--- of a path.
-function xboot.path.endslash(p)
-	if string.sub(p, string.len(p)-1) ~= "/" then
-		return p .. "/"
-	else
-		return p
-	end
-end
-
--- Checks whether a path is absolute or not.
-function xboot.path.abs(p)
-
-	local tmp = xboot.path.normalslashes(p)
-
-	-- Path is absolute if it starts with a "/".
-	if string.find(tmp, "/") == 1 then
-		return true
-	end
-
-	-- Path is absolute if it starts with a
-	-- letter followed by a colon.
-	if string.find(tmp, "%a:") == 1 then
-		return true
-	end
-
-	-- Relative.
-	return false
-
-end
-
--- Converts any path into a full path.
-function xboot.path.getfull(p)
-
-	if xboot.path.abs(p) then
-		return xboot.path.normalslashes(p)
-	end
-
-	local cwd = "/" --xboot.filesystem.getWorkingDirectory()
-	cwd = xboot.path.normalslashes(cwd)
-	cwd = xboot.path.endslash(cwd)
-
-	-- Construct a full path.
-	local full = cwd .. xboot.path.normalslashes(p)
-
-	-- Remove trailing /., if applicable
-	return full:match("(.-)/%.$") or full
-end
-
--- Returns the leaf of a full path.
-function xboot.path.leaf(p)
-	local a = 1
-	local last = p
-
-	while a do
-		a = string.find(p, "/", a+1)
-
-		if a then
-			last = string.sub(p, a+1)
-		end
-	end
-
-	return last
-end
-
+-- Setting package path
 package.path = "/romdisk/system/lib/?.lua;/romdisk/system/lib/?/init.lua;" .. package.path
 package.cpath = "/romdisk/system/lib/?.so;/romdisk/system/lib/loadall.so;" .. package.path
 
-local sample = require "xboot.sample"
-local framerate = require "xboot.framerate"
-local logger = require "logger"
-local dump = require "dump"
-local test = nil
-
-function xboot.boot()
---	dump(package.searchers, 3)
-	test = require "test"
-	test.error("ttt")
-
-	local printr = require "printr"
-	printr(arg)
+-- Main function
+function xboot.main()
+	require("main")
 end
 
-function xboot.init()
-end
-
-function xboot.run()
-	if xboot.load then xboot.load(arg) end
-	local fr = framerate.new()
-	while true do
-		fr:step()
-		if xboot.update then xboot.update(fr:getdelta()) end
-		if xboot.draw then xboot.draw() end
-		fr:sleep(0.1)
-		--fr.sleep(fr, 0.1)
-		--test.error(fr:getfps())
-		--logger.error(fr:getfps())
-		--print(fr:getfps())
-		break
-	end
-end
-
-local debug = debug
-local function error_printer(msg, layer)
-	print((debug.traceback("Error: " .. tostring(msg), 1+(layer or 1)):gsub("\n[^\n]+$", "")))
+-- Message handler
+local function handler(msg, layer)
+	print((debug.traceback("ERROR: " .. tostring(msg), 1 + (layer or 1)):gsub("\n[^\n]+$", "")))
 end
 
 return function()
-	local result = xpcall(xboot.boot, error_printer)
-	if not result then return 1 end
-	local result = xpcall(xboot.init, error_printer)
-	if not result then return 1 end
-	local result, retval = xpcall(xboot.run, error_printer)
-	if not result then return 1 end
-	return tonumber(retval) or 0
+	local res, ret = xpcall(xboot.main, handler)
+	if not res then return -1 end
+	return tonumber(ret) or -1
 end
+

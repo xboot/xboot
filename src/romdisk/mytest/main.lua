6b63f510ba5cf87b712159e996aa74dcd519b154
jerryjianjun <jerryjianjun@gmail.com>
2012-12-11 13:02:07 +0800

    src/romdisk/mytest/main.lua

--- src/romdisk/mytest/main.lua
+++ src/romdisk/mytest/main.lua
@@ -4,7 +4,7 @@ local dump = require "dump"
 local printr = require "printr"
 local log = require "lib.log"
 
-function main()
+local function main()
 	if xboot.load then xboot.load(arg) end
 	local fr = framerate.new()
 	while true do
@@ -13,8 +13,12 @@ function main()
 		if xboot.draw then xboot.draw() end
 		fr:sleep(0.1)
 		log.info(fr:getfps())
+		break
 	end
 end
 
+printr(xboot)
+printr(arg)
 main()
+assert(false)
 

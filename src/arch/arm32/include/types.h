10998981b7351be55f64822423f73e687ccc4b32
jerryjianjun <jerryjianjun@gmail.com>
2013-02-11 23:47:25 +0800

    change arm32's types.h, redefine s32_t and u32_t

--- src/arch/arm32/include/types.h
+++ src/arch/arm32/include/types.h
@@ -11,8 +11,8 @@ typedef unsigned char			u8_t;
 typedef signed short			s16_t;
 typedef unsigned short			u16_t;
 
-typedef long signed int			s32_t;
-typedef long unsigned int		u32_t;
+typedef signed int				s32_t;
+typedef unsigned int			u32_t;
 
 typedef signed long long		s64_t;
 typedef unsigned long long		u64_t;

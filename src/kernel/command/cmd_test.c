97472278bdc8bb44546dea6411d05d634dc1bfc3
jerryjianjun <jerryjianjun@gmail.com>
2013-02-14 19:00:06 +0800

    cairo clock programe

--- src/kernel/command/cmd_test.c
+++ src/kernel/command/cmd_test.c
@@ -49,6 +49,10 @@
 #include "cairo-types-private.h"
 #include "cairo-image-surface-private.h"
 
+#include <ft2build.h>
+#include <freetype/freetype.h>
+#include <freetype/ftglyph.h>
+
 struct fb * fb;
 struct surface_t * screen;
 u32_t c;
@@ -69,7 +73,6 @@ void show_image(pixman_image_t * image)
 
 	width = pixman_image_get_width(image);
 	height = pixman_image_get_height(image);
-
 	format = pixman_image_get_format(image);
 
     /* Three cases:
@@ -84,7 +87,7 @@ void show_image(pixman_image_t * image)
      *  - other: we will convert to a8r8g8b8 under the assumption that
      *    whoever created the image probably did it wrong.
      */
-	switch (format)
+/*	switch (format)
 	{
 	case PIXMAN_a8r8g8b8_sRGB:
 	case PIXMAN_a8r8g8b8:
@@ -98,10 +101,9 @@ void show_image(pixman_image_t * image)
 				0, 0, width, height);
 		break;
 	}
-
-	show_image_init();
-	obj = surface_alloc(pixman_image_get_data(copy), width, height, PIXEL_FORMAT_ARGB_8888);
-
+*/
+//	show_image_init();
+	obj = surface_alloc(pixman_image_get_data(image), width, height, PIXEL_FORMAT_ARGB_8888);
 	fb->swap(fb);
 	surface_fill(screen, &screen->clip, c, BLEND_MODE_REPLACE);
 	surface_blit(screen, NULL, obj, NULL, BLEND_MODE_REPLACE);
@@ -109,12 +111,153 @@ void show_image(pixman_image_t * image)
 	surface_free(obj);
 }
 
+void lcd(cairo_surface_t * surface)
+{
+	static int init = 0;
+
+	if(!init)
+	{
+		show_image_init();
+		init = 1;
+	}
+
+	show_image(to_image_surface(surface)->pixman_image);
+}
+
+/*
+ *
+ *
+ */
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+/* Draws a clock on a normalized Cairo context */
+static void
+draw (cairo_t *cr)
+{
+    static struct tm __tm;
+    struct tm *tm = &__tm;
+    double seconds, minutes, hours;
+
+    tm->tm_sec++;
+    if(tm->tm_sec >= 60)
+    {
+    	tm->tm_sec = 0;
+    	tm->tm_min++;
+    	if(tm->tm_min >= 60)
+    	{
+    		tm->tm_min = 0;
+    		tm->tm_hour++;
+    		if(tm->tm_hour >= 24)
+    			tm->tm_hour = 0;
+    	}
+    }
+
+    /* compute the angles for the indicators of our clock */
+    seconds = tm->tm_sec * M_PI / 30;
+    minutes = tm->tm_min * M_PI / 30;
+    hours = tm->tm_hour * M_PI / 6;
+
+    /* Fill the background with white. */
+    cairo_set_source_rgb (cr, 1, 1, 1);
+    cairo_paint (cr);
+
+    /* who doesn't want all those nice line settings :) */
+    cairo_set_line_cap (cr, CAIRO_LINE_CAP_ROUND);
+    cairo_set_line_width (cr, 0.1);
+
+    /* translate to the center of the rendering context and draw a black */
+    /* clock outline */
+    cairo_set_source_rgb (cr, 0, 0, 0);
+    cairo_translate (cr, 0.5, 0.5);
+    cairo_arc (cr, 0, 0, 0.4, 0, M_PI * 2);
+    cairo_stroke (cr);
+
+    /* draw a white dot on the current second. */
+    cairo_set_source_rgba (cr, 1, 1, 1, 0.6);
+    cairo_arc (cr,
+	       sin (seconds) * 0.4, -cos (seconds) * 0.4,
+	       0.05, 0, M_PI * 2);
+    cairo_fill (cr);
+
+    /* draw the minutes indicator */
+    cairo_set_source_rgba (cr, 0.2, 0.2, 1, 0.6);
+    cairo_move_to (cr, 0, 0);
+    cairo_line_to (cr, sin (minutes) * 0.4, -cos (minutes) * 0.4);
+    cairo_stroke (cr);
+
+    /* draw the hours indicator      */
+    cairo_move_to (cr, 0, 0);
+    cairo_line_to (cr, sin (hours) * 0.2, -cos (hours) * 0.2);
+    cairo_stroke (cr);
+}
+
+void run(void)
+{
+	cairo_surface_t *surface;
+	cairo_t * cr;
+	int width = 200;
+	int height = 200;
+	static int count = 0;
+	static int flag_w = 1;
+	static int flag_h = 1;
+
+	show_image_init();
+
+	while (1)
+	{
+		if(flag_w)
+		{
+			if(++width >=799)
+				flag_w = 0;
+		}
+		else
+		{
+			if(--width < 100)
+				flag_w = 1;
+		}
+
+		if(flag_h)
+		{
+			if(++height >=479)
+				flag_h = 0;
+		}
+		else
+		{
+			if(--height < 100)
+				flag_h = 1;
+		}
+
+		surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
+		cr = cairo_create(surface);
+
+		cairo_scale(cr, width, height);
+		draw(cr);
+
+//		printk("count=%d\r\n", count++);
+		show_image(to_image_surface(surface) ->pixman_image);
+
+		cairo_destroy(cr);
+		cairo_surface_destroy(surface);
+	}
+
+}
+/*
+ *
+ *
+ */
+
 static int test(int argc, char ** argv)
 {
 	cairo_surface_t *surface;
 	cairo_t *cr;
 
-	surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 320, 320);
+	run();
+	return 0 ;
+
+	surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 750, 430);
 	cr = cairo_create(surface);
 
 	cairo_save(cr);
@@ -122,24 +265,45 @@ static int test(int argc, char ** argv)
 	cairo_paint(cr);
 	cairo_restore(cr);
 
-	// "romdisk/system/media/image/battery/battery_6.png"
+	// /romdisk/system/media/image/battery/battery_6.png
 	/////////////////////////////////////
-	double dashes[] = {50.0,  /* ink */
-	                   10.0,  /* skip */
-	                   10.0,  /* ink */
-	                   10.0   /* skip*/
-	                  };
-	int    ndash  = sizeof (dashes)/sizeof(dashes[0]);
-	double offset = -50.0;
 
-	cairo_set_dash (cr, dashes, ndash, offset);
-	cairo_set_line_width (cr, 10.0);
+	FT_Library library;
+	FT_Face face;
+	FT_Init_FreeType( &library );
+	FT_New_Face( library, "/romdisk/DroidSansFallback.ttf", 0, &face );
+	cairo_font_face_t * myfont_face;
+	myfont_face =  cairo_ft_font_face_create_for_ft_face(face, 0);
 
-	cairo_move_to (cr, 128.0, 25.6);
-	cairo_line_to (cr, 230.4, 230.4);
-	cairo_rel_line_to (cr, -102.4, 0.0);
-	cairo_curve_to (cr, 51.2, 230.4, 51.2, 128.0, 128.0, 128.0);
+	cairo_set_font_face(cr, myfont_face);
 
+	//////////////////////////
+	cairo_text_extents_t extents;
+
+	const char *utf8 = "111啊A哦哦o";
+	double x,y;
+
+/*	cairo_select_font_face (cr, "Sans",
+	    CAIRO_FONT_SLANT_NORMAL,
+	    CAIRO_FONT_WEIGHT_NORMAL);*/
+
+	cairo_set_font_size (cr, 152.0);
+	cairo_text_extents (cr, utf8, &extents);
+	x = 128.0-(extents.width/2 + extents.x_bearing);
+	y = 128.0-(extents.height/2 + extents.y_bearing);
+
+	cairo_move_to (cr, x, y);
+	cairo_show_text (cr, utf8);
+
+	/* draw helping lines */
+	cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
+	cairo_set_line_width (cr, 6.0);
+	cairo_arc (cr, x, y, 10.0, 0, 2*M_PI);
+	cairo_fill (cr);
+	cairo_move_to (cr, 128.0, 0);
+	cairo_rel_line_to (cr, 0, 256);
+	cairo_move_to (cr, 0, 128.0);
+	cairo_rel_line_to (cr, 256, 0);
 	cairo_stroke (cr);
 	/////////////////////////////
 
--- src/romdisk/mytest/main.lua
+++ src/romdisk/mytest/main.lua
@@ -3,95 +3,23 @@ local logger = require "logger"
 local dump = require "dump"
 local printr = require "printr"
 local log = require "lib.log"
+--local cairo = require "org.xboot.cairo"
+--local CAIRO = cairo
 
 local function main()
-	if xboot.load then xboot.load(arg) end
-	local fr = framerate.new()
-	while true do
-		fr:step()
-		if xboot.update then xboot.update(fr:getdelta()) end
-		if xboot.draw then xboot.draw() end
-		fr:sleep(0.1)
-		log.info(fr:getfps())
-		break
-	end
+	local w = 320
+	local h = 240
+
+	local cs = cairo.image_surface_create(CAIRO.FORMAT_ARGB32, w, h)
+	local cr = cairo.create (cs)
+
+	cairo.save(cr)
+	cairo.set_source_rgb(cr, 1, 0, 1)
+	cairo.paint(cr)
+	cairo.restore(cr)
+
+	cairo.lcd(cs)
+	while true do end
 end
 
---[[
-local base64 = require "org.xboot.base64"
-printr(base64)
-a = base64.encode("abc")
-b = base64.decode(a)
-print(a)
-print(b)
-]]
-
---[[
-local EVENT = require "org.xboot.event"
-local EVENTLISTENER = require "org.xboot.event.listener"
-local EVENTDISPATCHER = require "org.xboot.event.dispatcher"
-
-function evcall1(e)
-	print("NEW EVENT 1...")
-	print(e)
-end
-
-function evcall2(e)
-	print("NEW EVENT 2...")
-	print(e)
-end
-
-local el1 = EVENTLISTENER.new(1000, evcall1)
-local el2 = EVENTLISTENER.new(1000, evcall2)
-
-EVENTDISPATCHER.add_event_listener(el1);
-EVENTDISPATCHER.add_event_listener(el2);
-
-EVENT.new(1000, 10, 20, 3, 4, -1):send()
-
-
-e1 = EVENT.new(1000, 0, 0, 1, 1, 1)
-e2 = EVENT.new(1001, 0, 0, 1, 1, 2)
-e3 = EVENT.new(1002, 0, 0, 1, 1, 3)
-e4 = EVENT.new(1003, 0, 0, 1, 1, 4)
-e5 = EVENT.new(1004, 0, 0, 1, 1, 5)
-e = { e1, e2, e3, e4, e5 }
-
-printr(e)
-
-print("------")
-print(e1:get("type"))
-print(e1:get("timestamp"))
-printr(e1:get("event"))
-print("------")
-
-
-printr(EVENT)
-print(EVENT.EV_UNKNOWN)
-print(EVENT.EV_MOUSE_RAW)
-print(e1)
-
-while true do
-	EVENTDISPATCHER.run()
-end
-]]
-
-print("===============")
-
-local EVENT = require "org.xboot.event"
-local EVENTLISTENER = require "org.xboot.event.listener"
-local EVENTDISPATCHER = require "org.xboot.event.dispatcher"
-
-local function evcallback(e)
-	print("evcallback", e)
-end
-
-local el = EVENTLISTENER.new(EVENT.EV_MOUSE_RAW, evcallback)
-
-EVENTDISPATCHER.add_event_listener(el);
-
-while true do
-	EVENTDISPATCHER.run()
-end
-
-print("===============")
+--main()

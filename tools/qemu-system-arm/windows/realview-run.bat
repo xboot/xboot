5894e8448414c41d24f59dc7c8f78f8f62c46648
jerryjianjun <jerryjianjun@gmail.com>
2013-02-17 21:31:06 +0800

    add -show-cursor flag for qemu-system-arm

--- tools/qemu-system-arm/windows/realview-run.bat
+++ tools/qemu-system-arm/windows/realview-run.bat
@@ -5,4 +5,4 @@ if exist "%USERPROFILE%\.xboot\sdcard.img" goto RunQemu
     unzip.exe sdcard.zip -d "%USERPROFILE%\.xboot"
 
 :RunQemu
-qemu-system-arm.exe -M realview-eb -name "ARM RealView Emulation Baseboard" -localtime -serial vc -sd "%USERPROFILE%\.xboot\sdcard.img" -kernel ..\..\..\output\xboot.elf
+qemu-system-arm.exe -M realview-eb -name "ARM RealView Emulation Baseboard" -show-cursor -localtime -serial vc -sd "%USERPROFILE%\.xboot\sdcard.img" -kernel ..\..\..\output\xboot.elf

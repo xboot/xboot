b6613ef688d98d6eabf958c81d3017be4caf7d1f
jianjun jiang <jerryjianjun@gmail.com>
2013-06-29 22:16:18 +0800

    delete event framework

--- src/include/framework/framework.h
+++ src/include/framework/framework.h
@@ -8,7 +8,6 @@ extern "C" {
 #include <framework/luahelper.h>
 #include <framework/base64/l_base64.h>
 #include <framework/cairo/l_cairo.h>
-#include <framework/event/l_event.h>
 #include <framework/framerate/l_framerate.h>
 
 int luaopen_boot(lua_State * L);

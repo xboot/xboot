606e30e0905e479a5e6d557279c7c9ae837bd590
jianjun jiang <jerryjianjun@gmail.com>
2013-07-25 21:32:07 +0800

    working on framework...

--- src/romdisk/system/lib/boot.lua
+++ src/romdisk/system/lib/boot.lua
@@ -1,16 +1,12 @@
--- Make sure xboot table exists
-if not xboot then xboot = {} end
-
--- Setting package path
 package.path = "/romdisk/system/lib/?.lua;/romdisk/system/lib/?/init.lua;" .. package.path
 package.cpath = "/romdisk/system/lib/?.so;/romdisk/system/lib/loadall.so;" .. package.path
 
--- Loader function
+print = require("org.xboot.buildin.logger").print
+
 local function loader()
 	require("main")
 end
 
--- Message handler
 local function handler(msg, layer)
 	print((debug.traceback("ERROR: " .. tostring(msg), 1 + (layer or 1)):gsub("\n[^\n]+$", "")))
 end

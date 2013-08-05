d484b088b05b84c2cb8a2f92a26f6b2e29f17629
jerryjianjun <jerryjianjun@gmail.com>
2013-08-05 10:08:57 +0800

    [pixman] porting pixman to version pixman-0.30

---
+++ src/external/pixman-0.30/pixman-conf.h
@@ -0,0 +1,16 @@
+#ifndef __PIXMAN_CONF_H__
+#define __PIXMAN_CONF_H__
+
+#include <xboot.h>
+
+#if defined(__BIG_ENDIAN)
+# define WORDS_BIGENDIAN		(1)
+#endif
+
+#define PIXMAN_NO_TLS 			(1)
+
+#define SIZEOF_LONG				(4)
+
+#endif /* __PIXMAN_CONF_H__ */
+
+

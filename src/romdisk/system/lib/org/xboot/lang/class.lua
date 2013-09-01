ce17f3116c0df93929b86fb5e06cf3a2fab42403
jianjun jiang <jerryjianjun@gmail.com>
2013-09-02 00:20:12 +0800

    fix class.lua

--- src/romdisk/system/lib/org/xboot/lang/class.lua
+++ src/romdisk/system/lib/org/xboot/lang/class.lua
@@ -1,12 +1,12 @@
 local M = {}
 setmetatable(M, M)
 
-local function table_copy(src, dest)
-    dest = dest or {}
+local function table_copy(src, dst)
+    dst = dst or {}
     for i, v in pairs(src) do
-        dest[i] = v
+        dst[i] = v
     end
-    return dest
+    return dst
 end
 
 function M:__call(...)
@@ -15,25 +15,25 @@ function M:__call(...)
 	for i = #b, 1, -1 do
 		table_copy(b[i], c)
 	end
-	
+
 	c.__call = function(self, ...)
 		return self:new(...)
 	end
-	
+
 	return setmetatable(c, c)
 end
 
 function M:new(...)
 	local o = {__index = self}
 	setmetatable(o, o)
-	
+
 	if o.init then
 		o:init(...)
 	end
-	
+
 	o.new = nil
 	o.init = nil
-	
+
 	return o
 end
 

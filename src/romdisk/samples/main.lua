91ef397f4b7fea0a259b2069a8f7247d86a0cf21
jianjun jiang <jerryjianjun@gmail.com>
2013-09-01 22:39:53 +0800

    modify cursor for main.lua

--- src/romdisk/samples/main.lua
+++ src/romdisk/samples/main.lua
@@ -45,6 +45,10 @@ local cursor = display_image:new("/romdisk/samples/images/cursor.png", 0, 0)
 cursor:add_event_listener(event.MOUSE_DOWN, function(d, e) d:setxy(e.info.x, e.info.y) end, cursor)
 cursor:add_event_listener(event.MOUSE_MOVE, function(d, e) d:setxy(e.info.x, e.info.y) end, cursor)
 cursor:add_event_listener(event.MOUSE_UP, function(d, e) d:setxy(e.info.x, e.info.y) end, cursor)
+cursor:add_event_listener(event.TOUCHES_BEGIN, function(d, e) d:setxy(e.info.x, e.info.y) end, cursor)
+cursor:add_event_listener(event.TOUCHES_MOVE, function(d, e) d:setxy(e.info.x, e.info.y) end, cursor)
+cursor:add_event_listener(event.TOUCHES_END, function(d, e) d:setxy(e.info.x, e.info.y) end, cursor)
+cursor:add_event_listener(event.TOUCHES_CANCEL, function(d, e) d:setxy(e.info.x, e.info.y) end, cursor)
 runtime:add_child(cursor)
 
 ------------------- main --------------------------------

37295690a58959ea0c8c4b89bf450865076b8f1f
jerryjianjun <jerryjianjun@gmail.com>
2013-09-11 18:06:15 +0800

    add test ...

---
+++ src/romdisk/test/Animation.lua
@@ -0,0 +1,33 @@
+local M = Class(DisplayObject)
+
+function M:init(list, x, y)
+	DisplayObject.init(self)
+
+	self.x = x or 0
+	self.y = y or 0
+	self.anim = list or {}
+	self.frame = 1
+	self.nframes = #self.anim
+	self.subframe = 0
+	
+	self:addChild(self.anim[self.frame])
+	self:addEventListener(Event.ENTER_FRAME, self.onEnterFrame, self)
+end
+
+function M:onEnterFrame()
+	self.subframe = self.subframe + 1
+	
+	if self.subframe > 2 then
+		self:removeChild(self.anim[self.frame])
+		
+		self.frame = self.frame + 1
+		if self.frame > self.nframes then
+			self.frame = 1
+		end
+
+		self:addChild(self.anim[self.frame])
+		self.subframe = 0
+	end
+end
+
+return M
---
+++ src/romdisk/test/main.lua
@@ -0,0 +1,23 @@
+local Animation = require ("Animation")
+
+local background = DisplayImage:new(Texture:new("background.png"))
+runtime:addChild(background)
+
+local boy = Texture:new("boy.png")
+local boylist = {
+	DisplayImage:new(boy:region(72 * 0, 0, 72, 72)),
+	DisplayImage:new(boy:region(72 * 1, 0, 72, 72)),
+	DisplayImage:new(boy:region(72 * 2, 0, 72, 72)),
+	DisplayImage:new(boy:region(72 * 3, 0, 72, 72)),
+	DisplayImage:new(boy:region(72 * 4, 0, 72, 72)),
+	DisplayImage:new(boy:region(72 * 5, 0, 72, 72)),
+	DisplayImage:new(boy:region(72 * 6, 0, 72, 72)),
+	DisplayImage:new(boy:region(72 * 7, 0, 72, 72)),
+	DisplayImage:new(boy:region(72 * 8, 0, 72, 72)),
+	DisplayImage:new(boy:region(72 * 9, 0, 72, 72)),
+	DisplayImage:new(boy:region(72 * 10, 0, 72, 72)),
+} 
+
+local anmi = Animation:new(boylist, 100 ,100)
+anmi:setXY(100,100)
+runtime:addChild(anmi)

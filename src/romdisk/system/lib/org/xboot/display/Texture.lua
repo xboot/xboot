5550c550a7358dffeefc32503795a38439f19df3
jerryjianjun <jerryjianjun@gmail.com>
2013-09-12 09:27:28 +0800

    fix Texture.lua

--- src/romdisk/system/lib/org/xboot/display/Texture.lua
+++ src/romdisk/system/lib/org/xboot/display/Texture.lua
@@ -1,45 +1,66 @@
 ---
 -- The 'Texture' class lets you create a new texture object
--- to load from an image file and display in screen.
--- 
+-- to load from an image file or others.
+--
 -- @module Texture
 local M = Class()
 
 ---
 -- Creates a new texture object.
--- 
+--
 -- @function [parent=#Texture] new
--- @param filename (string) The name of the texture file to be loaded.
--- @return #Texture 
-function M:init(filename)
-	if type(filename) == "string" then
-		self.surface = buildin_cairo.image_surface_create_from_png(filename);
+-- @param data (string) The name of the image file to be loaded.
+-- @param data (userdata) The surface of the texture.
+-- @return #Texture
+function M:init(data)
+	if type(data) == "string" then
+		self.surface = buildin_cairo.image_surface_create_from_png(data);
+	elseif type(data) == "userdata" then
+		self.surface = data
+	else
+		self.surface = nil
 	end
 end
 
---- 
+---
 -- Returns the width of the texture in pixels.
--- 
+--
 -- @function [parent=#Texture] getWidth
 -- @param self
 -- @return The width of the texture in pixels.
 function M:getWidth()
-	return self.surface:get_width()
+	if self.surface then
+		return self.surface:get_width()
+	end
+	return 0
 end
 
 ---
 -- Returns the height of the texture in pixels.
--- 
+--
 -- @function [parent=#Texture] getHeight
 -- @param self
 -- @return The height of the texture in pixels.
 function M:getHeight()
-	return self.surface:get_height()
+	if self.surface then
+		return self.surface:get_height()
+	end
+	return 0
+end
+
+---
+-- Sets the surface of the texture.
+--
+-- @function [parent=#Texture] setSurface
+-- @param self
+-- @param surface (userdata) The surface of the texture
+function M:setSurface(surface)
+	self.surface = surface or self.surface
 end
 
 ---
 -- Returns the surface of the texture.
--- 
+--
 -- @function [parent=#Texture] getSurface
 -- @param self
 -- @return The surface of the texture.
@@ -49,14 +70,14 @@ end
 
 ---
 -- Creates a new texture object specifies a texture and a rectangular region in it.
--- 
+--
 -- @function [parent=#Texture] region
 -- @param self
 -- @param x (number) left coordinate of the region
 -- @param y (number) top coordinate of the region
 -- @param width (number) width of the region
 -- @param height (number) height of the region
--- @return #Texture 
+-- @return #Texture
 function M:region(x, y, width, height)
 	x = x or 0
 	y = y or 0
@@ -67,9 +88,8 @@ function M:region(x, y, width, height)
 	local cr = buildin_cairo.create(cs)
 	cr:set_source_surface(self.surface, -x, -y)
 	cr:paint()
-	local tex = M:new()
-	tex.surface = cs
-	return tex
+
+	return M:new(cs)
 end
 
 return M

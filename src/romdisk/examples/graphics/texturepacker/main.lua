1ab3066a5dff6bff6c0cb66e3b3540893c43504b
jerryjianjun <jerryjianjun@gmail.com>
2013-09-12 13:54:08 +0800

    add TexturePacker.lua and test ....

---
+++ src/romdisk/examples/graphics/texturepacker/main.lua
@@ -0,0 +1,24 @@
+local background = DisplayImage:new(Texture:new("background.png"))
+runtime:addChild(background)
+
+local packer = TexturePacker:new("cnum")
+
+local img1 = DisplayImage:new(packer:getTexture("1.png"))
+img1:setXY(0,0)
+runtime:addChild(img1)
+
+local img2 = DisplayImage:new(packer:getTexture("2.png"))
+img2:setXY(109,0)
+runtime:addChild(img2)
+
+local img3 = DisplayImage:new(packer:getTexture("3.png"))
+img3:setXY(0,109)
+runtime:addChild(img3)
+
+local img4 = DisplayImage:new(packer:getTexture("4.png"))
+img4:setXY(109,109)
+runtime:addChild(img4)
+
+local img5 = DisplayImage:new(packer:getTexture("5.png"))
+img5:setXY(109+109,0)
+runtime:addChild(img5)

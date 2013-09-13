755580d3053d5c4e5e9a24afac93871b5d75cc11
jianjun jiang <jerryjianjun@gmail.com>
2013-09-13 22:40:21 +0800

    fix initcall.h and module.h

--- src/include/xboot/module.h
+++ src/include/xboot/module.h
@@ -39,7 +39,7 @@ struct module_list
 #define EXPORT_SYMBOL(symbol) \
 	extern typeof(symbol) symbol; \
 	static const char __ksym_name_##symbol[] = #symbol; \
-	const struct symbol_t __ksymtab_##symbol \
+	static const struct symbol_t __ksymtab_##symbol \
 	__attribute__((__used__, section(".ksymtab.text"))) = { (void *)&symbol, __ksym_name_##symbol }
 
 /*

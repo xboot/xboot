606e8ec19a9ddf09e5d5cf97c4ff3e9ce71a7441
jerryjianjun <jerryjianjun@gmail.com>
2013-09-29 11:52:19 +0800

    add widget button test

---
+++ src/romdisk/examples/widget/button/main.lua
@@ -0,0 +1,19 @@
+-- Create background
+local bg = DisplayImage:new(Texture.new("images/bg.png"))
+runtime:addChild(bg)
+
+-- Create up and down display object for the button
+local up = DisplayImage:new(Texture.new("images/up.png"))
+local down = DisplayImage:new(Texture.new("images/down.png"))
+
+-- Create the button
+local btn = Widget.Button:new(up, down)
+btn:setPosition(100, 100)
+runtime:addChild(btn)
+
+-- Register to "Click" event
+local count = 0
+btn:addEventListener("Click", function(d, e)
+	count = count + 1
+	print("Button clicked " .. count .. " times")
+end, btn)

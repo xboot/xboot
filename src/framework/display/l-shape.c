4c0fcdea03992ab023491737fb9fb028fbbc52dc
jerryjianjun <jerryjianjun@gmail.com>
2013-10-24 16:42:34 +0800

    work on shape...

--- src/framework/display/l-shape.c
+++ src/framework/display/l-shape.c
@@ -51,6 +51,145 @@ static int m_shape_gc(lua_State * L)
 	return 0;
 }
 
+static int m_shape_save(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_save(*cr);
+	return 0;
+}
+
+static int m_shape_restore(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_restore(*cr);
+	return 0;
+}
+
+static int m_shape_push_group(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_push_group(*cr);
+	return 0;
+}
+
+static int m_shape_pop_group(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_pop_group(*cr);
+	return 0;
+}
+
+static int m_shape_pop_group_to_source(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_pop_group_to_source(*cr);
+	return 0;
+}
+
+static int m_shape_new_path(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_new_path(*cr);
+	return 0;
+}
+
+static int m_shape_new_sub_path(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_new_sub_path(*cr);
+	return 0;
+}
+
+static int m_shape_close_path(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_close_path(*cr);
+	return 0;
+}
+
+static int m_shape_set_operator(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_operator_t op = (cairo_operator_t)luaL_checkinteger(L, 2);
+	cairo_set_operator(*cr, op);
+	return 0;
+}
+
+static int m_shape_set_source(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_pattern_t ** pattern = luaL_checkudata(L, 2, MT_NAME_PARTTERN);
+	cairo_set_source(*cr, *pattern);
+	return 0;
+}
+
+static int m_shape_set_source_color(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	double red = luaL_checknumber(L, 2);
+	double green = luaL_checknumber(L, 3);
+	double blue = luaL_checknumber(L, 4);
+	double alpha = luaL_optnumber(L, 5, 1);
+	cairo_set_source_rgba(*cr, red, green, blue, alpha);
+	return 0;
+}
+
+static int m_shape_set_tolerance(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	double tolerance = luaL_checknumber(L, 2);
+	cairo_set_tolerance(*cr, tolerance);
+	return 0;
+}
+
+static int m_shape_set_miter_limit(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	double limit = luaL_checknumber(L, 2);
+	cairo_set_miter_limit(*cr, limit);
+	return 0;
+}
+
+static int m_shape_set_antialias(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_antialias_t antialias = (cairo_antialias_t)luaL_checkinteger(L, 2);
+	cairo_set_antialias(*cr, antialias);
+	return 0;
+}
+
+static int m_shape_set_fill_rule(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_fill_rule_t rule = (cairo_fill_rule_t)luaL_checkinteger(L, 2);
+	cairo_set_fill_rule(*cr, rule);
+	return 0;
+}
+
+static int m_shape_set_line_width(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	double width = luaL_checknumber(L, 2);
+	cairo_set_line_width(*cr, width);
+	return 0;
+}
+
+static int m_shape_set_line_cap(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_line_cap_t cap = (cairo_line_cap_t)luaL_checkinteger(L, 2);
+	cairo_set_line_cap(*cr, cap);
+	return 0;
+}
+
+static int m_shape_set_line_join(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	cairo_line_join_t join = (cairo_line_join_t)luaL_checkinteger(L, 2);
+	cairo_set_line_join(*cr, join);
+	return 0;
+}
+
 static int m_shape_move_to(lua_State * L)
 {
 	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
@@ -87,17 +226,6 @@ static int m_shape_rel_line_to(lua_State * L)
 	return 0;
 }
 
-static int m_shape_rectangle(lua_State * L)
-{
-	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	double x = luaL_checknumber(L, 2);
-	double y = luaL_checknumber(L, 3);
-	double width = luaL_checknumber(L, 4);
-	double height = luaL_checknumber(L, 5);
-	cairo_rectangle(*cr, x, y, width, height);
-	return 0;
-}
-
 static int m_shape_curve_to(lua_State * L)
 {
 	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
@@ -124,6 +252,17 @@ static int m_shape_rel_curve_to(lua_State * L)
 	return 0;
 }
 
+static int m_shape_rectangle(lua_State * L)
+{
+	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
+	double x = luaL_checknumber(L, 2);
+	double y = luaL_checknumber(L, 3);
+	double width = luaL_checknumber(L, 4);
+	double height = luaL_checknumber(L, 5);
+	cairo_rectangle(*cr, x, y, width, height);
+	return 0;
+}
+
 static int m_shape_arc(lua_State * L)
 {
 	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
@@ -145,33 +284,6 @@ static int m_shape_arc_negative(lua_State * L)
 	double angle1 = luaL_checknumber(L, 5);
 	double angle2 = luaL_checknumber(L, 6);
 	cairo_arc_negative(*cr, xc, yc, radius, angle1, angle2);
-	return 0;
-}
-
-static int m_shape_set_line_width(lua_State * L)
-{
-	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	double width = luaL_checknumber(L, 2);
-	cairo_set_line_width(*cr, width);
-	return 0;
-}
-
-static int m_shape_set_source(lua_State * L)
-{
-	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	cairo_pattern_t ** pattern = luaL_checkudata(L, 2, MT_NAME_PARTTERN);
-	cairo_set_source(*cr, *pattern);
-	return 0;
-}
-
-static int m_shape_set_source_rgba(lua_State * L)
-{
-	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	double red = luaL_checknumber(L, 2);
-	double green = luaL_checknumber(L, 3);
-	double blue = luaL_checknumber(L, 4);
-	double alpha = luaL_optnumber(L, 5, 1);
-	cairo_set_source_rgba(*cr, red, green, blue, alpha);
 	return 0;
 }
 
@@ -220,50 +332,8 @@ static int m_shape_clip_preserve(lua_State * L)
 static int m_shape_paint(lua_State * L)
 {
 	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	cairo_paint(*cr);
-	return 0;
-}
-
-static int m_shape_paint_with_alpha(lua_State * L)
-{
-	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	double alpha = luaL_checknumber(L, 2);
+	double alpha = luaL_optnumber(L, 2, 1.0);
 	cairo_paint_with_alpha(*cr, alpha);
-	return 0;
-}
-
-static int m_shape_save(lua_State * L)
-{
-	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	cairo_save(*cr);
-	return 0;
-}
-
-static int m_shape_restore(lua_State * L)
-{
-	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	cairo_restore(*cr);
-	return 0;
-}
-
-static int m_shape_new_path(lua_State * L)
-{
-	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	cairo_new_path(*cr);
-	return 0;
-}
-
-static int m_shape_new_sub_path(lua_State * L)
-{
-	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	cairo_new_sub_path(*cr);
-	return 0;
-}
-
-static int m_shape_close_path(lua_State * L)
-{
-	cairo_t ** cr = luaL_checkudata(L, 1, MT_NAME_SHAPE);
-	cairo_close_path(*cr);
 	return 0;
 }
 
@@ -287,31 +357,40 @@ static int m_shape_size(lua_State * L)
 
 static const luaL_Reg m_shape[] = {
 	{"__gc",				m_shape_gc},
-	{"arc",					m_shape_arc},
-	{"arc_negative",		m_shape_arc_negative},
-	{"clip",				m_shape_clip},
-	{"clip_preserve",		m_shape_clip_preserve},
-	{"close_path",			m_shape_close_path},
-	{"curve_to",			m_shape_curve_to},
-	{"fill",				m_shape_fill},
-	{"fill_preserve",		m_shape_fill_preserve},
-	{"line_to",				m_shape_line_to},
-	{"move_to",				m_shape_move_to},
-	{"new_path",			m_shape_new_path},
-	{"new_sub_path",		m_shape_new_sub_path},
-	{"paint",				m_shape_paint},
-	{"paint_with_alpha",	m_shape_paint_with_alpha},
-	{"rectangle",			m_shape_rectangle},
-	{"rel_curve_to",		m_shape_rel_curve_to},
-	{"rel_line_to",			m_shape_rel_line_to},
-	{"rel_move_to",			m_shape_rel_move_to},
-	{"restore",				m_shape_restore},
 	{"save",				m_shape_save},
-	{"set_line_width",		m_shape_set_line_width},
-	{"set_source_rgba",		m_shape_set_source_rgba},
-	{"set_source",			m_shape_set_source},
+	{"restore",				m_shape_restore},
+	{"pushGroup",			m_shape_push_group},
+	{"popGroup",			m_shape_pop_group},
+	{"popGroupToSource",	m_shape_pop_group_to_source},
+	{"newPath",				m_shape_new_path},
+	{"newSubPath",			m_shape_new_sub_path},
+	{"closePath",			m_shape_close_path},
+	{"setOperator",			m_shape_set_operator},
+	{"setSource",			m_shape_set_source},
+	{"setSourceColor",		m_shape_set_source_color},
+	{"setTolerance",		m_shape_set_tolerance},
+	{"setMiterLimit",		m_shape_set_miter_limit},
+	{"setAntialias",		m_shape_set_antialias},
+	{"setFillRule",			m_shape_set_fill_rule},
+	{"setLineWidth",		m_shape_set_line_width},
+	{"setLineCap",			m_shape_set_line_cap},
+	{"setLineJoin",			m_shape_set_line_join},
+	{"moveTo",				m_shape_move_to},
+	{"relMoveTo",			m_shape_rel_move_to},
+	{"lineTo",				m_shape_line_to},
+	{"relLineTo",			m_shape_rel_line_to},
+	{"curveTo",				m_shape_curve_to},
+	{"relCurveTo",			m_shape_rel_curve_to},
+	{"rectangle",			m_shape_rectangle},
+	{"arc",					m_shape_arc},
+	{"arcNegative",			m_shape_arc_negative},
 	{"stroke",				m_shape_stroke},
-	{"stroke_preserve",		m_shape_stroke_preserve},
+	{"strokePreserve",		m_shape_stroke_preserve},
+	{"fill",				m_shape_fill},
+	{"fillPreserve",		m_shape_fill_preserve},
+	{"clip",				m_shape_clip},
+	{"clipPreserve",		m_shape_clip_preserve},
+	{"paint",				m_shape_paint},
 	{"size",				m_shape_size},
 	{NULL,					NULL}
 };
@@ -319,6 +398,55 @@ static const luaL_Reg m_shape[] = {
 int luaopen_shape(lua_State * L)
 {
 	luaL_newlib(L, l_shape);
+	/* cairo_operator_t */
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_CLEAR",			CAIRO_OPERATOR_CLEAR);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_SOURCE",			CAIRO_OPERATOR_SOURCE);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_OVER",			CAIRO_OPERATOR_OVER);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_IN",				CAIRO_OPERATOR_IN);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_OUT",				CAIRO_OPERATOR_OUT);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_ATOP",			CAIRO_OPERATOR_ATOP);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_DEST",			CAIRO_OPERATOR_DEST);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_DEST_OVER",		CAIRO_OPERATOR_DEST_OVER);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_DEST_IN",			CAIRO_OPERATOR_DEST_IN);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_DEST_OUT",		CAIRO_OPERATOR_DEST_OUT);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_DEST_ATOP",		CAIRO_OPERATOR_DEST_ATOP);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_XOR",				CAIRO_OPERATOR_XOR);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_ADD",				CAIRO_OPERATOR_ADD);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_SATURATE",		CAIRO_OPERATOR_SATURATE);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_MULTIPLY",		CAIRO_OPERATOR_MULTIPLY);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_SCREEN",			CAIRO_OPERATOR_SCREEN);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_OVERLAY",			CAIRO_OPERATOR_OVERLAY);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_DARKEN",			CAIRO_OPERATOR_DARKEN);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_LIGHTEN",			CAIRO_OPERATOR_LIGHTEN);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_COLOR_DODGE",		CAIRO_OPERATOR_COLOR_DODGE);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_COLOR_BURN",		CAIRO_OPERATOR_COLOR_BURN);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_HARD_LIGHT", 		CAIRO_OPERATOR_HARD_LIGHT);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_SOFT_LIGHT",		CAIRO_OPERATOR_SOFT_LIGHT);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_DIFFERENCE",		CAIRO_OPERATOR_DIFFERENCE);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_EXCLUSION", 		CAIRO_OPERATOR_EXCLUSION);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_HSL_HUE",			CAIRO_OPERATOR_HSL_HUE);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_HSL_SATURATION",	CAIRO_OPERATOR_HSL_SATURATION);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_HSL_COLOR",		CAIRO_OPERATOR_HSL_COLOR);
+	luahelper_set_intfield(L, "CAIRO_OPERATOR_HSL_LUMINOSITY",	CAIRO_OPERATOR_HSL_LUMINOSITY);
+	/* cairo_antialias_t */
+	luahelper_set_intfield(L, "ANTIALIAS_DEFAULT",				CAIRO_ANTIALIAS_DEFAULT);
+	luahelper_set_intfield(L, "ANTIALIAS_NONE",					CAIRO_ANTIALIAS_NONE);
+	luahelper_set_intfield(L, "ANTIALIAS_GRAY",					CAIRO_ANTIALIAS_GRAY);
+	luahelper_set_intfield(L, "ANTIALIAS_SUBPIXEL",				CAIRO_ANTIALIAS_SUBPIXEL);
+	luahelper_set_intfield(L, "ANTIALIAS_FAST",					CAIRO_ANTIALIAS_FAST);
+	luahelper_set_intfield(L, "ANTIALIAS_GOOD",					CAIRO_ANTIALIAS_GOOD);
+	luahelper_set_intfield(L, "ANTIALIAS_BEST",					CAIRO_ANTIALIAS_BEST);
+    /* cairo_fill_rule_t */
+	luahelper_set_intfield(L, "FILL_RULE_WINDING",				CAIRO_FILL_RULE_WINDING);
+	luahelper_set_intfield(L, "FILL_RULE_EVEN_ODD",				CAIRO_FILL_RULE_EVEN_ODD);
+	/* cairo_line_cap_t */
+	luahelper_set_intfield(L, "LINE_CAP_BUTT",					CAIRO_LINE_CAP_BUTT);
+	luahelper_set_intfield(L, "LINE_CAP_ROUND",					CAIRO_LINE_CAP_ROUND);
+	luahelper_set_intfield(L, "LINE_CAP_SQUARE",				CAIRO_LINE_CAP_SQUARE);
+	/* cairo_line_join_t */
+	luahelper_set_intfield(L, "LINE_JOIN_MITER", 				CAIRO_LINE_JOIN_MITER);
+	luahelper_set_intfield(L, "LINE_JOIN_ROUND", 				CAIRO_LINE_JOIN_ROUND);
+	luahelper_set_intfield(L, "LINE_JOIN_BEVEL", 				CAIRO_LINE_JOIN_BEVEL);
 	luahelper_create_metatable(L, MT_NAME_SHAPE, m_shape);
 	return 1;
 }
--- src/romdisk/framework/org/xboot/display/DisplayShape.lua
+++ src/romdisk/framework/org/xboot/display/DisplayShape.lua
@@ -12,7 +12,7 @@ local M = Class(DisplayObject)
 -- @param width (number) The width of drawing area in pixels.
 -- @param height (number) The height of drawing area in pixels.
 -- @param x (optional) The x coordinate of the display shape.
--- @param y (optional) The y coordinate of the display shape. 
+-- @param y (optional) The y coordinate of the display shape.
 -- @return #DisplayShape
 function M:init(width, height, x, y)
 	self.super:init()
@@ -51,6 +51,134 @@ end
 
 function M:restore()
 	self.shape:restore()
+end
+
+function M:pushGroup()
+	self.shape:pushGroup()
+end
+
+function M:popGroup()
+	self.shape:popGroup()
+end
+
+function M:popGroupToSource()
+	self.shape:popGroupToSource()
+end
+
+function M:newPath()
+	self.shape:newPath()
+end
+
+function M:newSubPath()
+	self.shape:newSubPath()
+end
+
+function M:closePath()
+	self.shape:closePath()
+end
+
+function M:setOperator(op)
+	self.shape:setOperator(op)
+end
+
+function M:setSource(pattern)
+	self.shape:setSource(pattern)
+end
+
+function M:setSourceColor(r, g, b, a)
+	self.shape:setSourceColor(r, g, b, a)
+end
+
+function M:setTolerance(tolerance)
+	self.shape:setTolerance(tolerance)
+end
+
+function M:setMiterLimit(limit)
+	self.shape:setMiterLimit(limit)
+end
+
+function M:setAntialias(antialias)
+	self.shape:setAntialias(antialias)
+end
+
+function M:setFillRule(rule)
+	self.shape:setFillRule(rule)
+end
+
+function M:setLineWidth(width)
+	self.shape:setLineWidth(width)
+end
+
+function M:setLineCap(cap)
+	self.shape:setLineCap(cap)
+end
+
+function M:setLineJoin(join)
+	self.shape:setLineJoin(join)
+end
+
+function M:moveTo(x, y)
+	self.shape:moveTo(x, y)
+end
+
+function M:relMoveTo(dx, dy)
+	self.shape:relMoveTo(dx, dy)
+end
+
+function M:lineTo(x, y)
+	self.shape:lineTo(x, y)
+end
+
+function M:relLineTo(dx, dy)
+	self.shape:relLineTo(dx, dy)
+end
+
+function M:curveTo(x1, y1, x2, y2, x3, y3)
+	self.shape:curveTo(x1, y1, x2, y2, x3, y3)
+end
+
+function M:relCurveTo(dx1, dy1, dx2, dy2, dx3, dy3)
+	self.shape:relCurveTo(dx1, dy1, dx2, dy2, dx3, dy3)
+end
+
+function M:rectangle(x, y, width, height)
+	self.shape:rectangle(x, y, width, height)
+end
+
+function M:arc(xc, yc, radius, angle1, angle2)
+	self.shape:arc(xc, yc, radius, angle1, angle2)
+end
+
+function M:arcNegative(xc, yc, radius, angle1, angle2)
+	self.shape:arcNegative(xc, yc, radius, angle1, angle2)
+end
+
+function M:stroke()
+	self.shape:stroke()
+end
+
+function M:strokePreserve()
+	self.shape:strokePreserve()
+end
+
+function M:fill()
+	self.shape:fill()
+end
+
+function M:fillPreserve()
+	self.shape:fillPreserve()
+end
+
+function M:clip()
+	self.shape:clip()
+end
+
+function M:clipPreserve()
+	self.shape:clipPreserve()
+end
+
+function M:paint(alpha)
+	self.shape:paint(alpha)
 end
 
 ---

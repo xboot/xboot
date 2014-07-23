5bc75f363425d7c174e00fd494079ad2040a6237
jerryjianjun <jerryjianjun@gmail.com>
2014-07-23 16:28:09 +0800

    temp fix 4412 for arm-2013.05 toolchains

--- src/arch/arm32/mach-exynos4412/include/configs.h
+++ src/arch/arm32/mach-exynos4412/include/configs.h
@@ -1,7 +1,7 @@
 #ifndef __OVERRIDE_CONFIGS_H__
 #define __OVERRIDE_CONFIGS_H__
 
-#define	CONFIG_MMU_ON			(1)
+#define	CONFIG_MMU_ON			(0)
 #define CONFIG_HEAP_SIZE		(SZ_128M)
 
 #endif /* __OVERRIDE_CONFIGS_H__ */

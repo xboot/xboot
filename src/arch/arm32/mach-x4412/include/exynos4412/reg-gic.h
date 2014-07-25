1c1cfd3b997e3d29392a5416041d4a8cc19d22b4
jerryjianjun <jerryjianjun@gmail.com>
2014-07-25 18:19:07 +0800

    add exynos gic and combiner head files

---
+++ src/arch/arm32/mach-x4412/include/exynos4412/reg-gic.h
@@ -0,0 +1,28 @@
+#ifndef __EXYNOS4412_REG_GIC_H__
+#define __EXYNOS4412_REG_GIC_H__
+
+#define EXYNOS4412_GIC_CPU_BASE		(0x10480000)
+#define EXYNOS4412_GIC_DIST_BASE	(0x10490000)
+
+#define GIC_CPU_CTRL				(0x00)
+#define GIC_CPU_PRIMASK				(0x04)
+#define GIC_CPU_BINPOINT			(0x08)
+#define GIC_CPU_INTACK				(0x0c)
+#define GIC_CPU_EOI					(0x10)
+#define GIC_CPU_RUNNINGPRI			(0x14)
+#define GIC_CPU_HIGHPRI				(0x18)
+
+#define GIC_DIST_CTRL				(0x000)
+#define GIC_DIST_CTR				(0x004)
+#define GIC_DIST_ENABLE_SET			(0x100)
+#define GIC_DIST_ENABLE_CLEAR		(0x180)
+#define GIC_DIST_PENDING_SET		(0x200)
+#define GIC_DIST_PENDING_CLEAR		(0x280)
+#define GIC_DIST_ACTIVE_BIT			(0x300)
+#define GIC_DIST_PRI				(0x400)
+#define GIC_DIST_TARGET				(0x800)
+#define GIC_DIST_CONFIG				(0xc00)
+#define GIC_DIST_SOFTINT			(0xf00)
+
+
+#endif /* __EXYNOS4412_REG_GIC_H__ */

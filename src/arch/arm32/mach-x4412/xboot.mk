35d6ad2bd7a64bbf252a9f9525e306df5bc80e9c
jerryjianjun <jerryjianjun@gmail.com>
2014-08-10 18:05:23 +0800

    FIX 4412 fpu, remove -mfloat-abi=softfp flags

--- src/arch/arm32/mach-x4412/xboot.mk
+++ src/arch/arm32/mach-x4412/xboot.mk
@@ -4,14 +4,14 @@
 
 DEFINES		+= -D__ARM_ARCH__=7 -D__CORTEX_A9__ -D__ARM_NEON__
 
-ASFLAGS		:= -g -ggdb -Wall
-CFLAGS		:= -g -ggdb -Wall
-CXXFLAGS	:= -g -ggdb -Wall
+ASFLAGS		:= -g -ggdb -Wall -O3
+CFLAGS		:= -g -ggdb -Wall -O3
+CXXFLAGS	:= -g -ggdb -Wall -O3
 LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
 ARFLAGS		:= -rcs
 OCFLAGS		:= -v -O binary
 ODFLAGS		:= -d
-MCFLAGS		:= -mcpu=cortex-a8 -mtune=cortex-a8 -march=armv7-a -mfpu=neon -ftree-vectorize -ffast-math -mfloat-abi=softfp
+MCFLAGS		:= -mcpu=cortex-a9 -mtune=cortex-a9 -march=armv7-a -mfpu=neon -ftree-vectorize -ffast-math
 
 LIBDIRS		:=
 LIBS 		:=

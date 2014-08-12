50a205b9ff07a9eb7cc3e3437ae76917d74f294d
jerryjianjun <jerryjianjun@gmail.com>
2014-08-12 15:05:56 +0800

    add clocksource, not complete

---
+++ src/include/clocksource/clocksource.h
@@ -0,0 +1,15 @@
+#ifndef __CLOCKSOURCE_H__
+#define __CLOCKSOURCE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <xboot.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CLOCKSOURCE_H__ */
+

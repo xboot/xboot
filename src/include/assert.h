ae29beebc465e05ba492026f36469c8bf0c2cda3
jerryjianjun <jerryjianjun@gmail.com>
2014-09-29 00:12:38 +0800

    [libc]fix assert, change DEBUG to NDEBUG

--- src/include/assert.h
+++ src/include/assert.h
@@ -5,10 +5,10 @@
 extern "C" {
 #endif
 
-#ifdef DEBUG
-#define assert(x)	((void)((x) || (__assert_fail(#x, __FILE__, __LINE__, __func__), 0)))
-#else
+#ifdef NDEBUG
 #define	assert(x)	((void)0)
+#else
+#define assert(x)	((void)((x) || (__assert_fail(#x, __FILE__, __LINE__, __func__), 0)))
 #endif
 
 void __assert_fail(const char * expr, const char * file, int line, const char * func);

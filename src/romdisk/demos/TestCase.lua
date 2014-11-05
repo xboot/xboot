d53c6e648a3ae82f229f1f847560be05f5b0ef6e
jerryjianjun <jerryjianjun@gmail.com>
2014-11-06 00:45:39 +0800

    [TestCase]modify TestCase

--- src/romdisk/demos/TestCase.lua
+++ src/romdisk/demos/TestCase.lua
@@ -1,60 +1,144 @@
 local M = Class(DisplayObject)
 
-function M:init(cases, w, h)
+function M:init(cases, width, height)
 	self.super:init()
 
-	local width, height = application:getScreenSize()
-	self.w = w or width
-	self.h = h or height
+	local w, h = application:getScreenSize()
+	self.width = width or w
+	self.height = height or h
 	self.cases = cases or {}
+	self.index = 1
 	if #self.cases > 0 then
-		self.index = 1
-		self.case = self.cases[self.index].new()
-		self:addChild(self.case)
---		self.oldcase = self.case
---		self.newcase = self.case
+		self.case1 = self.cases[self.index].new()
+		self.case2 = nil
+		self:addChild(self.case1)
 	end
+	self.tweening = false
+	
+	self:addEventListener(Event.ENTER_FRAME, self.onEnterFrame, self)
+end
+
+function M:onEnterFrame(e)
+	if not self.tweening then
+		return
+	end
+
+	local elapsed = self.watch:elapsed()
+	if elapsed < self.duration then
+		self.transition(elapsed)
+	else
+		self.transition(self.duration)
+		self:removeChild(self.case1)
+		self.case1 = self.case2
+		self.case2 = nil
+		self.transition = nil
+		self.ease = nil
+		self.watch = nil
+		self.tweening = false
+
+		collectgarbage()
+	end
+end
+
+function M:select(index, duration, transition, ease)
+	if self.tweening then
+		return
+	end
+
+	if index < 1 then
+		index = 1;
+	elseif index > #self.cases then
+		index = #self.cases
+	end
+	
+	if index == self.index then
+		return
+	end
+	self.index = index
+	
+	if self.case1 == nil then
+		self.case1 = self.cases[self.index].new()
+		self:addChild(self.case)
+		return
+	end
+	self.case2 = self.cases[self.index].new()
+	self:addChild(self.case2)
+
+	self.duration = duration or 1
+	local transition = transition or "moveFromLeft"
+	local ease = ease or "outBounce"
+
+	if transition == "moveFromLeft" then
+		self.transition = function(t)
+			local x = self.ease:easing(t)
+			self.case1:setX(x)
+			self.case2:setX(x - self.width)
+		end
+		self.ease = Easing.new(0, self.width, self.duration, ease)
+
+	elseif transition == "moveFromRight" then
+		self.transition = function(t)
+			local x = self.ease:easing(t)
+			self.case1:setX(-x)
+			self.case2:setX(self.width - x)
+		end
+		self.ease = Easing.new(0, self.width, self.duration, ease)
+
+	elseif transition == "moveFromTop" then
+		self.transition = function(t)
+			local y = self.ease:easing(t)
+			self.case1:setY(y)
+			self.case2:setY(y - self.height)
+		end
+		self.ease = Easing.new(0, self.height, self.duration, ease)
+	
+	elseif transition == "moveFromBottom" then
+		self.transition = function(t)
+			local y = self.ease:easing(t)
+			self.case1:setY(-y)
+			self.case2:setY(self.height - y)
+		end
+		self.ease = Easing.new(0, self.height, self.duration, ease)
+	
+	elseif transition == "overFromLeft" then
+		self.transition = function(t)
+			local x = self.ease:easing(t)
+			self.case2:setX(x - self.width)
+		end
+		self.ease = Easing.new(0, self.width, self.duration, ease)
+		
+	elseif transition == "overFromRight" then
+		self.transition = function(t)
+			local x = self.ease:easing(t)
+			self.case2:setX(self.width - x)
+		end
+		self.ease = Easing.new(0, self.width, self.duration, ease)
+
+	elseif transition == "overFromTop" then
+		self.transition = function(t)
+			local y = self.ease:easing(t)
+			self.case2:setY(y - self.height)
+		end
+		self.ease = Easing.new(0, self.height, self.duration, ease)
+	
+	elseif transition == "overFromBottom" then
+		self.transition = function(t)
+			local y = self.ease:easing(t)
+			self.case2:setY(self.height - y)
+		end
+		self.ease = Easing.new(0, self.height, self.duration, ease)
+	end
+
+	self.watch = Stopwatch.new()
+	self.tweening = true
 end
 
 function M:prev()
-	self.index = self.index - 1
-	if self.index < 1 then
-		self.index = 1
-		self.case:setX(self.w / 8):animate({x = 0}, 0.5, "outBounce")
-		return
-	end
-	
-	local oldcase = self.case
-	local newcase = self.cases[self.index].new()
-	newcase:setPosition(-self.w, 0)
-	newcase:animate({x = 0}, 1, "outBounce")
-	newcase:addEventListener(Event.ANIMATE_COMPLETE, function(d, e)
-		if oldcase then oldcase:removeSelf() end
-		self.case = newcase
-	end)
-	self:addChild(newcase)
-	if oldcase then oldcase:animate({x = self.w}, 1, "outBounce") end
+	self:select(self.index - 1, 0.6, "moveFromLeft", "outBounce")
 end
 
 function M:next()
-	self.index = self.index + 1
-	if self.index > #self.cases then
-		self.index = #self.cases
-		self.case:setX(-self.w / 8):animate({x = 0}, 0.5, "outBounce")
-		return
-	end
-	
-	local newcase = self.cases[self.index].new()
-	local oldcase = self.case
-
-	newcase:setPosition(self.w, 0)
-	newcase:addEventListener(Event.ANIMATE_COMPLETE, function(d, e)
-		if oldcase then oldcase:removeSelf() end
-		self.case = newcase
-	end)
-	newcase:animate({x = 0}, 1, "outBounce")
-	if oldcase then oldcase:animate({x = -self.w}, 1, "outBounce") end
-	self:addChild(newcase)
+	self:select(self.index + 1, 0.6, "moveFromRight", "outBounce")
 end
 
 return M

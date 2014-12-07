b81551c30b01fca7aa1614224696aa90295f8fe6
jerryjianjun <jerryjianjun@gmail.com>
2014-12-07 12:31:05 +0800

    working on 2048

---
+++ src/romdisk/examples/games/2048/Tile.lua
@@ -0,0 +1,30 @@
+local M = Class(DisplayObject)
+
+function M:init(x, y, n)
+	self.super:init()
+
+	local assets = application:getAssets()
+	self.gx = x
+	self.gy = y
+	self.n = n
+	self.o = assets:loadDisplay("games/2048/tile" .. self.n .. ".png")
+		:setPosition((self.gx - 1) * 70 + 55, (self.gy - 1) * 70 + 171)
+		:setAnchor(0.5, 0.5)
+		:setScale(0.1, 0.1)
+		:animate({scalex = 1, scaley = 1}, 0.3, "inOutQuint")
+	self:addChild(self.o)
+end
+
+function M:move(x, y)
+	if self.gx ~= x or self.gy ~= y then
+		self.gx = x
+		self.gy = y
+		self.o:animate({x = (x - 1) * 70 + 55, y = (y - 1) * 70 + 171}, 0.3, "inOutQuint")
+	end
+end
+
+function M:number()
+	return self.n
+end
+
+return M

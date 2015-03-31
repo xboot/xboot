2f44763f712349f162b77670b9e343f76b2071ef
jianjun jiang <8192542@qq.com>
2015-03-31 16:32:15 +0800

    [device]add buzzer and watchdog type

--- src/kernel/core/device.c
+++ src/kernel/core/device.c
@@ -60,11 +60,17 @@ static struct kobj_t * search_device_kobj(struct device_t * dev)
 	case DEVICE_TYPE_LEDTRIG:
 		name = "ledtrig";
 		break;
+	case DEVICE_TYPE_BUZZER:
+		name = "buzzer";
+		break;
 	case DEVICE_TYPE_FRAMEBUFFER:
 		name = "framebuffer";
 		break;
 	case DEVICE_TYPE_INPUT:
 		name = "input";
+		break;
+	case DEVICE_TYPE_WATCHDOG:
+		name = "watchdog";
 		break;
 	case DEVICE_TYPE_BLOCK:
 		name = "block";

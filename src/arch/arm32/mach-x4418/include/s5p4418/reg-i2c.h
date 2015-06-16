f47b59f234be6a665eac5b4dbb58638f83acf881
jianjun jiang <8192542@qq.com>
2015-06-16 10:55:49 +0800

    [x4418]add i2c head file

---
+++ src/arch/arm32/mach-x4418/include/s5p4418/reg-i2c.h
@@ -0,0 +1,14 @@
+#ifndef __S5P4418_REG_I2C_H__
+#define __S5P4418_REG_I2C_H__
+
+#define S5P4418_I2C0_BASE		(0xC00A4000)
+#define S5P4418_I2C1_BASE		(0xC00A5000)
+#define S5P4418_I2C2_BASE		(0xC00A6000)
+
+#define I2C_ICCR				(0x00)
+#define I2C_ICSR				(0x04)
+#define I2C_IAR					(0x08)
+#define I2C_IDSR				(0x0C)
+#define I2C_STOPCON				(0x10)
+
+#endif /* __S5P4418_REG_I2C_H__ */

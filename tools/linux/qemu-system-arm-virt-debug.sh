3d0bdd7ebf52ec058e245a74f72371946abfc055
jianjun jiang <8192542@qq.com>
2015-11-24 11:43:35 +0800

    [arm32][mach-virt]remove mach-virt package

--- tools/linux/qemu-system-arm-virt-debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-
-# The script root directory.
-export QEMU_DIR=$(cd `dirname $0` ; pwd)
-
-# running ...
-exec qemu-system-arm -machine virt -cpu cortex-a15 -m 256 -smp 1 -name "ARM Virtual Machine For Cortex-A15" -show-cursor -localtime -serial stdio -kernel ${QEMU_DIR}/../../output/xboot -gdb tcp::10000 -S

3705c2ef0b589f1f5fb5afee1f82d6eeced678e9
jianjun jiang <8192542@qq.com>
2015-11-25 15:58:33 +0800

    [tools]update qemu-system to qemu-w64-setup-20151115.exe from http://qemu.weilnetz.de/

---
+++ tools/qemu-system/linux/realview-debug.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+
+# The qemu's root directory.
+export QEMU_DIR=$(cd `dirname $0` ; pwd)
+
+if [ ! -e ~/.xboot/sdcard.img ]; then
+	mkdir -p ~/.xboot;
+	unzip ${QEMU_DIR}/sdcard.zip -d ~/.xboot > /dev/null;
+fi
+
+# Run qemu
+exec qemu-system-arm -M realview-pb-a8 -m 256M -name "ARM RealView Platform Baseboard for Cortex-A8" -S -gdb tcp::10000,ipv4 -show-cursor -localtime -serial stdio -sd ~/.xboot/sdcard.img -net nic,vlan=0,macaddr=88:88:88:11:22:33,model=lan9118 -net user -kernel ${QEMU_DIR}/../../../output/xboot

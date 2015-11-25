803cf110340026d47ab580c7ec54a8e2127d654d
jianjun jiang <8192542@qq.com>
2015-11-25 16:50:26 +0800

    [arm64]fix for x6818 platform

--- src/arch/arm64/mach-x6818/xboot.mk
+++ src/arch/arm64/mach-x6818/xboot.mk
@@ -2,13 +2,13 @@
 # Machine makefile
 #
 
-DEFINES		+= -D__ARM_ARCH__=8 -D__CORTEX_A53__ -D__ARM_NEON__
+DEFINES		+= -D__ARM_ARCH__=8 -D__CORTEX_A53__
 
-ASFLAGS		:= -g -ggdb -Wall -O3
-CFLAGS		:= -g -ggdb -Wall -O3
-CXXFLAGS	:= -g -ggdb -Wall -O3
+ASFLAGS		:= -g -ggdb -Wall -O0
+CFLAGS		:= -g -ggdb -Wall -O0
+CXXFLAGS	:= -g -ggdb -Wall -O0
 LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
-MCFLAGS		:= -mcpu=cortex-a53 -mtune=cortex-a53 -march=armv8-a
+MCFLAGS		:= -march=armv8-a -mcpu=cortex-a53 -mtune=cortex-a53
 
 LIBDIRS		:=
 LIBS 		:=

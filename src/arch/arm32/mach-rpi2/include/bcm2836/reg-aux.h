a66bd7f2a70476aa014420fa06e10428a731c153
jiangjianjun <8192542@qq.com>
2016-02-02 21:46:29 +0800

    [arm32-rpi2]remove bcm2836-aux support

--- src/arch/arm32/mach-rpi2/include/bcm2836/reg-aux.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BCM2836_REG_AUX_H__
-#define __BCM2836_REG_AUX_H__
-
-#define BCM2836_AUX_BASE	(0x3f215000)
-
-#define AUX_IRQ				(0x00)
-#define AUX_ENB				(0x04)
-
-#endif /* __BCM2836_REG_AUX_H__ */

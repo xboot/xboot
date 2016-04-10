b73ecbdc9e20be257f7f659ef24c62fa9cbb105e
jiangjianjun <8192542@qq.com>
2016-04-10 14:04:09 +0800

    [arm64-virt]remove arm64-virt support

--- src/arch/arm64/mach-virt/include/virt/reg-uart.h
+++
@@ -1,6 +0,0 @@
-#ifndef __VIRT_REG_UART_H__
-#define __VIRT_REG_UART_H__
-
-#define VIRT_UART0_BASE		(0x09000000)
-
-#endif /* __VIRT_REG_UART_H__ */
--- tools/qemu-system/windows/virt-run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-if exist "%USERPROFILE%\.xboot\sdcard.img" goto RunQemu
-    mkdir "%USERPROFILE%\.xboot"
-    unzip.exe sdcard.zip -d "%USERPROFILE%\.xboot"
-
-:RunQemu
-qemu-system-aarch64.exe -machine virt -cpu cortex-a57 -m 256M -name "ARM Virtual Machine For Cortex-A57" -show-cursor -localtime -serial vc -sd "%USERPROFILE%\.xboot\sdcard.img" -kernel ..\..\..\output\xboot

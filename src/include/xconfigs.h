02b102542f780adeb0da12da68ea6885a0ce595b
jiangjianjun <8192542@qq.com>
2016-05-25 20:30:34 +0800

    [xconfigs.h]default CONFIG_AUTO_BOOT_COMMAND to ""

--- src/include/xconfigs.h
+++ src/include/xconfigs.h
@@ -33,7 +33,7 @@ extern "C" {
 #endif
 
 #if !defined(CONFIG_AUTO_BOOT_COMMAND)
-#define CONFIG_AUTO_BOOT_COMMAND			"exec /romdisk/examples/"
+#define CONFIG_AUTO_BOOT_COMMAND			""
 #endif
 
 #if !defined(CONFIG_MAX_BRIGHTNESS)

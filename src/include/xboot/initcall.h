ddb250361be7aa4b3f827e92aedbf0d6a89423f9
jiangjianjun <8192542@qq.com>
2016-09-05 00:05:34 +0800

    [initcall]change initcall.h

--- src/include/xboot/initcall.h
+++ src/include/xboot/initcall.h
@@ -25,9 +25,9 @@ typedef void (*exitcall_t)(void);
 #define postcore_initcall(fn)	__define_initcall("3", fn, 3)
 #define driver_initcall(fn)		__define_initcall("4", fn, 4)
 #define subsys_initcall(fn)		__define_initcall("5", fn, 5)
-#define device_initcall(fn)		__define_initcall("6", fn, 6)
-#define postdevice_initcall(fn)	__define_initcall("7", fn, 7)
-#define command_initcall(fn)	__define_initcall("8", fn, 8)
+#define command_initcall(fn)	__define_initcall("6", fn, 6)
+#define server_initcall(fn)		__define_initcall("7", fn, 7)
+#define reserver_initcall(fn)	__define_initcall("8", fn, 8)
 #define late_initcall(fn)		__define_initcall("9", fn, 9)
 
 #define pure_exitcall(fn)		__define_exitcall("0", fn, 0)
@@ -36,9 +36,9 @@ typedef void (*exitcall_t)(void);
 #define postcore_exitcall(fn)	__define_exitcall("3", fn, 3)
 #define driver_exitcall(fn)		__define_exitcall("4", fn, 4)
 #define subsys_exitcall(fn)		__define_exitcall("5", fn, 5)
-#define device_exitcall(fn)		__define_exitcall("6", fn, 6)
-#define postdevice_exitcall(fn)	__define_exitcall("7", fn, 7)
-#define command_exitcall(fn)	__define_exitcall("8", fn, 8)
+#define command_exitcall(fn)	__define_exitcall("6", fn, 6)
+#define server_exitcall(fn)		__define_exitcall("7", fn, 7)
+#define reserver_exitcall(fn)	__define_exitcall("8", fn, 8)
 #define late_exitcall(fn)		__define_exitcall("9", fn, 9)
 
 void do_initcalls(void);

d16bffe9005d4846a078ab6af16959d5f4effd3a
jianjun jiang <8192542@qq.com>
2016-09-07 19:24:08 +0800

    [battery]modify some comment

--- src/include/battery/battery.h
+++ src/include/battery/battery.h
@@ -36,7 +36,7 @@ struct battery_info_t {
 	int design_voltage;		/* mV */
 	int voltage;			/* mV */
 	int current;			/* mA */
-	int temperature;		/* 0.1 Centigrade */
+	int temperature;		/* 0.1°C */
 	int cycle;				/* cycle count */
 	int level;				/* Percentage */
 };

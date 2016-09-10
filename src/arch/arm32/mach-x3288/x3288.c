8134a0e3e1c254218db088457576de8322d7ac41
jianjun jiang <8192542@qq.com>
2016-09-10 17:53:56 +0800

    [x3288]working on x3288 platform

---
+++ src/arch/arm32/mach-x3288/x3288.c
@@ -0,0 +1,96 @@
+/*
+ * x3288.c
+ *
+ * Copyright(c) 2007-2016 Jianjun Jiang <8192542@qq.com>
+ * Official site: http://xboot.org
+ * Mobile phone: +86-18665388956
+ * QQ: 8192542
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <xboot.h>
+
+static const struct mmap_t mach_map[] = {
+	{ 0 },
+};
+
+static int mach_detect(struct machine_t * mach)
+{
+	return 1;
+}
+
+static void mach_memmap(struct machine_t * mach)
+{
+}
+
+static void mach_shutdown(struct machine_t * mach)
+{
+}
+
+static void mach_reboot(struct machine_t * mach)
+{
+}
+
+static void mach_sleep(struct machine_t * mach)
+{
+}
+
+static void mach_cleanup(struct machine_t * mach)
+{
+}
+
+static void mach_logger(struct machine_t * mach, const char * buf, int count)
+{
+}
+
+static const char * mach_uniqueid(struct machine_t * mach)
+{
+	return NULL;
+}
+
+static int mach_keygen(struct machine_t * mach, const char * msg, void * key)
+{
+	return 0;
+}
+
+static struct machine_t x3288 = {
+	.name 		= "x3288",
+	.desc 		= "X3288 Based On RK3288 SOC",
+	.map		= mach_map,
+	.detect 	= mach_detect,
+	.memmap		= mach_memmap,
+	.shutdown	= mach_shutdown,
+	.reboot		= mach_reboot,
+	.sleep		= mach_sleep,
+	.cleanup	= mach_cleanup,
+	.logger		= mach_logger,
+	.uniqueid	= mach_uniqueid,
+	.keygen		= mach_keygen,
+};
+
+static __init void x3288_machine_init(void)
+{
+	register_machine(&x3288);
+}
+
+static __exit void x3288_machine_exit(void)
+{
+	unregister_machine(&x3288);
+}
+
+machine_initcall(x3288_machine_init);
+machine_exitcall(x3288_machine_exit);
---
+++ src/arch/arm32/mach-x3288/xboot.mk
@@ -0,0 +1,28 @@
+#
+# Machine makefile
+#
+
+DEFINES		+= -D__ARM_ARCH__=7 -D__CORTEX_A17__ -D__ARM32_NEON__
+
+ASFLAGS		:= -g -ggdb -Wall -O0
+CFLAGS		:= -g -ggdb -Wall -O0
+CXXFLAGS	:= -g -ggdb -Wall -O0
+LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
+MCFLAGS		:= -march=armv7-a -mtune=cortex-a17 -mfpu=vfpv3-d16 -mfloat-abi=hard
+
+LIBDIRS		:=
+LIBS 		:=
+INCDIRS		:=
+SRCDIRS		:=
+
+ifeq ($(strip $(HOSTOS)), linux)
+MK3288		:= arch/$(ARCH)/$(MACH)/tools/linux/mk3288
+endif
+ifeq ($(strip $(HOSTOS)), windows)
+MK3288		:= arch/$(ARCH)/$(MACH)/tools/windows/mk3288
+endif
+LOADER		:= arch/$(ARCH)/$(MACH)/tools/images/loader
+
+xend:
+	@echo Make header information for irom booting
+	@$(MK3288) $(X_NAME)pak.bin $(LOADER) $(X_NAME).bin

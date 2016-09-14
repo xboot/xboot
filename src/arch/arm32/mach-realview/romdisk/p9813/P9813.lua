b438c444b19113bffd5d54bfc17e749c05b48009
jiangjianjun <8192542@qq.com>
2016-09-14 23:36:54 +0800

    [P9813]fix P9813 examples

--- src/arch/arm32/mach-realview/romdisk/p9813/P9813.lua
+++ src/arch/arm32/mach-realview/romdisk/p9813/P9813.lua
@@ -1,46 +1,64 @@
-local Gpio = require("hardware.gpio")
+local GPIO = require("hardware.gpio")
 local M = Class()
 
-function M:init(cin, din)
-	self.clk = Gpio.new(cin.name, cin.offset)
-	self.dat = Gpio.new(din.name, din.offset)
-	self.clk:setDir(Gpio.DIR_OUTPUT)
-	self.dat:setDir(Gpio.DIR_OUTPUT)
+function M:init(cpin, dpin, n)
+	self.clk = GPIO.new(cpin)
+	self.dat = GPIO.new(dpin)
+	self.num = n or 1
+
+	self.buf = {}
+	for i = 1, self.num, 1 do
+		self.buf[i] = {r = 0, g = 0, b = 0}
+	end
+
+	self.clk:setDir(GPIO.DIR_OUTPUT)
+	self.dat:setDir(GPIO.DIR_OUTPUT)
 	self.clk:setValue(1)
 	self.dat:setValue(1)
 end
 
-function M:sendByte(val)
-	for i = 0, 7, 1 do
-		if val & 0x80 == 0x80 then
+function M:sendByte(byte)
+	for i = 1, 8, 1 do
+		if byte & 0x80 == 0x80 then
 			self.dat:setValue(1)
 		else
 			self.dat:setValue(0)
 		end
 		self.clk:setValue(0)
 		self.clk:setValue(1)
-		val = val << 1
+		byte = byte << 1
 	end
 end
 
-function M:setColor(r, g, b)
-	local prefix = 0xc0;
-	if b & 0x80 == 0 then prefix = prefix | 0x20 end
-	if b & 0x40 == 0 then prefix = prefix | 0x10 end
-	if g & 0x80 == 0 then prefix = prefix | 0x08 end
-	if g & 0x40 == 0 then prefix = prefix | 0x04 end
-	if r & 0x80 == 0 then prefix = prefix | 0x02 end
-	if r & 0x40 == 0 then prefix = prefix | 0x01 end
+function M:setColor(i, r, g, b)
+	self.buf[i].r = r or self.buf[i].r
+	self.buf[i].g = g or self.buf[i].g
+	self.buf[i].b = b or self.buf[i].b
+end
 
+function M:refresh()
 	self:sendByte(0)
 	self:sendByte(0)
 	self:sendByte(0)
 	self:sendByte(0)
 
-	self:sendByte(prefix)
-	self:sendByte(b)
-	self:sendByte(g)
-	self:sendByte(r)
+	for i = 1, self.num, 1 do
+		local r = self.buf[i].r
+		local g = self.buf[i].g
+		local b = self.buf[i].b
+		local prefix = 0xc0;
+		if b & 0x80 == 0 then prefix = prefix | 0x20 end
+		if b & 0x40 == 0 then prefix = prefix | 0x10 end
+		if g & 0x80 == 0 then prefix = prefix | 0x08 end
+		if g & 0x40 == 0 then prefix = prefix | 0x04 end
+		if r & 0x80 == 0 then prefix = prefix | 0x02 end
+		if r & 0x40 == 0 then prefix = prefix | 0x01 end
+
+		self:sendByte(prefix)
+		self:sendByte(b)
+		self:sendByte(g)
+		self:sendByte(r)
+	end
 
 	self:sendByte(0)
 	self:sendByte(0)

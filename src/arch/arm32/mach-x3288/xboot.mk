211580d1b6b088da550405bde4c84828e857aff1
jianjun jiang <8192542@qq.com>
2017-01-09 18:05:12 +0800

    [gdb-stub]gdb stub for x3288

--- src/arch/arm32/mach-x3288/xboot.mk
+++ src/arch/arm32/mach-x3288/xboot.mk
@@ -8,7 +8,7 @@ ASFLAGS		:= -g -ggdb -Wall -O3
 CFLAGS		:= -g -ggdb -Wall -O3
 CXXFLAGS	:= -g -ggdb -Wall -O3
 LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
-MCFLAGS		:= -march=armv7-a -mtune=cortex-a17 -mfpu=vfpv3-d16 -mfloat-abi=hard
+MCFLAGS		:= -march=armv7-a -mtune=cortex-a17 -mfpu=vfpv3-d16 -mfloat-abi=hard -marm -mno-thumb-interwork
 
 LIBDIRS		:=
 LIBS 		:=

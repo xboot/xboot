746db724d4099324698dc6b198f40543eb44a9f4
Jiang Jianjun <8192542@qq.com>
2017-09-06 04:25:13 +0800

    [n3ds]add n3ds support

---
+++ src/arch/arm32/mach-n3ds/sandbox.c
@@ -0,0 +1,98 @@
+/*
+ * sandbox.c
+ *
+ * Copyright(c) 2007-2017 Jianjun Jiang <8192542@qq.com>
+ * Official site: http://xboot.org
+ * Mobile phone: +86-18665388956
+ * QQ: 8192542
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <sandbox.h>
+#include <xboot.h>
+
+static const struct mmap_t mach_map[] = {
+	{ 0 },
+};
+
+static int mach_detect(struct machine_t * mach)
+{
+	return 1;
+}
+
+static void mach_memmap(struct machine_t * mach)
+{
+}
+
+static void mach_shutdown(struct machine_t * mach)
+{
+}
+
+static void mach_reboot(struct machine_t * mach)
+{
+}
+
+static void mach_sleep(struct machine_t * mach)
+{
+}
+
+static void mach_cleanup(struct machine_t * mach)
+{
+}
+
+static void mach_logger(struct machine_t * mach, const char * buf, int count)
+{
+	//sandbox_stdio_write((void *)buf, count);
+}
+
+static const char * mach_uniqueid(struct machine_t * mach)
+{
+	return NULL;
+}
+
+static int mach_keygen(struct machine_t * mach, const char * msg, void * key)
+{
+	return 0;
+}
+
+static struct machine_t sandbox = {
+	.name 		= "sandbox",
+	.desc 		= "Xboot Sandbox Runtime Enverionment",
+	.map		= mach_map,
+	.detect 	= mach_detect,
+	.memmap		= mach_memmap,
+	.shutdown	= mach_shutdown,
+	.reboot		= mach_reboot,
+	.sleep		= mach_sleep,
+	.cleanup	= mach_cleanup,
+	.logger		= mach_logger,
+	.uniqueid	= mach_uniqueid,
+	.keygen		= mach_keygen,
+};
+
+static __init void sandbox_machine_init(void)
+{
+	register_machine(&sandbox);
+}
+
+static __exit void sandbox_machine_exit(void)
+{
+	unregister_machine(&sandbox);
+}
+
+machine_initcall(sandbox_machine_init);
+machine_exitcall(sandbox_machine_exit);

72dec1652095a555bbf2a267ea960c725879c35a
jianjun jiang <8192542@qq.com>
2017-09-12 19:30:03 +0800

    [freetype]update freetype from version 2.4.11 to 2.8

---
+++ src/external/freetype-2.8/src/pfr/pfr.c
@@ -0,0 +1,30 @@
+/***************************************************************************/
+/*                                                                         */
+/*  pfr.c                                                                  */
+/*                                                                         */
+/*    FreeType PFR driver component.                                       */
+/*                                                                         */
+/*  Copyright 2002-2017 by                                                 */
+/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
+/*                                                                         */
+/*  This file is part of the FreeType project, and may only be used,       */
+/*  modified, and distributed under the terms of the FreeType project      */
+/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
+
+
+#define FT_MAKE_OPTION_SINGLE_OBJECT
+#include <ft2build.h>
+
+#include "pfrcmap.c.h"
+#include "pfrdrivr.c.h"
+#include "pfrgload.c.h"
+#include "pfrload.c.h"
+#include "pfrobjs.c.h"
+#include "pfrsbit.c.h"
+
+
+/* END */
---
+++ src/external/freetype-2.8/src/psaux/psaux.c
@@ -0,0 +1,30 @@
+/***************************************************************************/
+/*                                                                         */
+/*  psaux.c                                                                */
+/*                                                                         */
+/*    FreeType auxiliary PostScript driver component (body only).          */
+/*                                                                         */
+/*  Copyright 1996-2017 by                                                 */
+/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
+/*                                                                         */
+/*  This file is part of the FreeType project, and may only be used,       */
+/*  modified, and distributed under the terms of the FreeType project      */
+/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
+
+
+#define FT_MAKE_OPTION_SINGLE_OBJECT
+#include <ft2build.h>
+
+#include "afmparse.c.h"
+#include "psauxmod.c.h"
+#include "psconv.c.h"
+#include "psobjs.c.h"
+#include "t1cmap.c.h"
+#include "t1decode.c.h"
+
+
+/* END */

d4660e659989c4bc1aae149bb3ed14c316ce1fd1
Jiang Jianjun <8192542@qq.com>
2017-12-05 22:10:35 +0800

    [s5pv210]modify s5pv210's clock and ddr initial code

--- src/arch/arm32/mach-s5pv210/sys-clock.c
+++ src/arch/arm32/mach-s5pv210/sys-clock.c
@@ -23,7 +23,38 @@
  */
 
 #include <xboot.h>
+#include <s5pv210/reg-clk.h>
+
+#define DIV0_VAL	((0 << 0) | (4 << 4) | (4 << 8) | (1 << 12) | (3 << 16) | (1 << 20) | (4 << 24) | (1 << 28))
+#define DIV1_VAL	((1 << 16) | (1 << 12) | (1 << 8) | (1 << 4))
+#define DIV2_VAL	((1 << 0))
+
+#define APLL_VAL	((1 << 31) | (0x7d << 16) | (0x3 << 8) | (0x1))
+#define MPLL_VAL	((1 << 31) | (0x29b << 16) | (0xc << 8) | (0x1))
+#define EPLL_VAL	((1 << 31) | (0x60 << 16) | (0x6 << 8) | (0x2))
+#define VPLL_VAL	((1 << 31) | (0x6c << 16) | (0x6 << 8) | (0x3))
+
+static inline void sdelay(int loops)
+{
+	__asm__ __volatile__ ("1:\n" "subs %0, %1, #1\n"
+		"bne 1b":"=r" (loops):"0"(loops));
+}
 
 void sys_clock_init(void)
 {
+	write32(S5PV210_CLK_BASE + CLK_APLL_LOCK, 0xfff);
+	write32(S5PV210_CLK_BASE + CLK_MPLL_LOCK, 0xfff);
+	write32(S5PV210_CLK_BASE + CLK_EPLL_LOCK, 0xfff);
+	write32(S5PV210_CLK_BASE + CLK_VPLL_LOCK, 0xfff);
+
+	write32(S5PV210_CLK_BASE + CLK_DIV0, DIV0_VAL);
+	write32(S5PV210_CLK_BASE + CLK_DIV1, DIV1_VAL);
+	write32(S5PV210_CLK_BASE + CLK_DIV2, DIV2_VAL);
+
+	write32(S5PV210_CLK_BASE + CLK_APLL_CON0, APLL_VAL);
+	write32(S5PV210_CLK_BASE + CLK_MPLL_CON, MPLL_VAL);
+	write32(S5PV210_CLK_BASE + CLK_EPLL_CON0, EPLL_VAL);
+	write32(S5PV210_CLK_BASE + CLK_VPLL_CON, VPLL_VAL);
+
+	sdelay(50000);
 }

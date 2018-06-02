54c8e1c3e7895bd7f714edc92ed9d00bc709ef33
JianJun Jiang <8192542@qq.com>
2018-06-02 21:52:09 +0800

    test for coroutine

---
+++ src/include/xboot/coroutine.h
@@ -0,0 +1,17 @@
+#ifndef __COROUTINE_H__
+#define __COROUTINE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <types.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <list.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __COROUTINE_H__ */

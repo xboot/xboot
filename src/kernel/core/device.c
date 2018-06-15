8520c7d85b10529e90e59c347037b275bdaa328b
jiangjianjun <8192542@qq.com>
2018-06-15 11:28:54 +0800

    [orientation]remove orientation device type

--- src/kernel/core/device.c
+++ src/kernel/core/device.c
@@ -136,9 +136,6 @@ static struct kobj_t * search_device_kobj(struct device_t * dev)
 	case DEVICE_TYPE_NVMEM:
 		name = "nvmem";
 		break;
-	case DEVICE_TYPE_ORIENTATION:
-		name = "orientation";
-		break;
 	case DEVICE_TYPE_PRESSURE:
 		name = "pressure";
 		break;

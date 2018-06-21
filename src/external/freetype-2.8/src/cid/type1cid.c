206ee9c7d2275cc425639d9199515064b16a9ffa
jiangjianjun <8192542@qq.com>
2018-06-21 19:19:14 +0800

    Revert "Revert "[freetype]update freetype from version 2.4.11 to 2.8""
    This reverts commit 6bfa2d7d50c8d5fcc3961f99fc653845305ad88a.

---
+++ src/external/freetype-2.8/src/cid/type1cid.c
@@ -0,0 +1,29 @@
+/***************************************************************************/
+/*                                                                         */
+/*  type1cid.c                                                             */
+/*                                                                         */
+/*    FreeType OpenType driver component (body only).                      */
+/*                                                                         */
+/*  Copyright 1996-2017 by                                                 */
+/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
+/*                                                                         */
+/*  This file is part of the FreeType project, and may only be used,       */
+/*  modified, and distributed under the terms of the FreeType project      */
+/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
+
+
+#define FT_MAKE_OPTION_SINGLE_OBJECT
+#include <ft2build.h>
+
+#include "cidgload.c.h"
+#include "cidload.c.h"
+#include "cidobjs.c.h"
+#include "cidparse.c.h"
+#include "cidriver.c.h"
+
+
+/* END */

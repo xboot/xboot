8950ebb225e9400e6ad80000b1ff927ffd97ee96
jiangjianjun <8192542@qq.com>
2018-07-20 14:53:43 +0800

    [raspi3]change raspberry script

--- tools/qemu-system/linux/raspberry-pi-3-dbg.sh
+++ tools/qemu-system/linux/raspberry-pi-3-dbg.sh
@@ -5,4 +5,4 @@
 export QEMU_DIR=$(cd `dirname $0` ; pwd)
 
 # Run qemu
-exec qemu-system-aarch64 -M raspi3 -m 1024M -name "Raspberry Pi 3" -S -gdb tcp::10000,ipv4 -show-cursor -rtc base=localtime -serial stdio -kernel ${QEMU_DIR}/../../../output/xboot
+exec qemu-system-aarch64 -M raspi3 -m 1024M -global bcm2835-fb.pixo=0 -name "Raspberry Pi 3" -S -gdb tcp::10000,ipv4 -show-cursor -rtc base=localtime -serial stdio -kernel ${QEMU_DIR}/../../../output/xboot

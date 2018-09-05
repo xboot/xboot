c70846bb118190a21fea3d611ea20a47f3e1dfa5
jiangjianjun <8192542@qq.com>
2018-09-05 17:47:43 +0800

    [x3399]add cold reboot support

--- src/arch/arm64/mach-x3399/x3399.c
+++ src/arch/arm64/mach-x3399/x3399.c
@@ -27,6 +27,7 @@
  */
 
 #include <xboot.h>
+#include <rk3399/reg-cru.h>
 
 static const struct mmap_t mach_map[] = {
 	{ 0 },
@@ -47,6 +48,7 @@ static void mach_shutdown(struct machine_t * mach)
 
 static void mach_reboot(struct machine_t * mach)
 {
+	write32(phys_to_virt(RK3399_CRU_BASE) + CRU_GLB_SRST_FST_VALUE, 0xfdb9);
 }
 
 static void mach_sleep(struct machine_t * mach)

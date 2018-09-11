0c1a7d14fd38961fb55dfb43890641c2a500f115
jiangjianjun <8192542@qq.com>
2018-09-11 14:11:26 +0800

    [tools]remove vlan for qemu realview

--- tools/qemu-system/windows/realview-dbg.bat
+++ tools/qemu-system/windows/realview-dbg.bat
@@ -6,4 +6,4 @@ if exist "%USERPROFILE%\.xboot\sdcard.img" goto RunQemu
     unzip.exe sdcard.zip -d "%USERPROFILE%\.xboot"
 
 :RunQemu
-qemu-system-arm.exe -M realview-pb-a8 -m 256M -name "ARM RealView Platform Baseboard for Cortex-A8" -S -gdb tcp::10000,ipv4 -show-cursor -rtc base=localtime -serial vc -sd "%USERPROFILE%\.xboot\sdcard.img" -net nic,vlan=0,macaddr=88:88:88:11:22:33,model=lan9118 -net user -kernel ..\..\..\output\xboot
+qemu-system-arm.exe -M realview-pb-a8 -m 256M -name "ARM RealView Platform Baseboard for Cortex-A8" -S -gdb tcp::10000,ipv4 -show-cursor -rtc base=localtime -serial vc -sd "%USERPROFILE%\.xboot\sdcard.img" -net nic,macaddr=88:88:88:11:22:33,model=lan9118 -net user -kernel ..\..\..\output\xboot

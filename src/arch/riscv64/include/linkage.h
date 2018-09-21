7a7aea67df07eb69b3c50d581bfd6d9088784bec
jiangjianjun <8192542@qq.com>
2018-09-21 21:50:02 +0800

    [riscv]workong on riscv

--- src/arch/riscv64/include/linkage.h
+++ src/arch/riscv64/include/linkage.h
@@ -8,15 +8,11 @@ extern "C" {
 #if __riscv_xlen == 64
 #define LREG	ld
 #define SREG	sd
-#define LFREG	fld
-#define SFREG	fsd
 #define REGSZ	8
 #define RVPTR	.dword
 #elif __riscv_xlen == 32
 #define LREG	lw
 #define SREG	sw
-#define LFREG	flw
-#define SFREG	fsw
 #define REGSZ	4
 #define RVPTR	.word
 #endif

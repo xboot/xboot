3d09546768fd4fa0c96ea48e6ee92fec62e4b74b
jianjian jiang <8192542@qq.com>
2018-09-23 15:05:36 +0800

    [riscv]modify smp 2 cores

--- tools/qemu-system/windows/riscv64-virt-dbg.bat
+++ tools/qemu-system/windows/riscv64-virt-dbg.bat
@@ -2,4 +2,4 @@
 @set SDL_STDIO_REDIRECT=0
 
 :RunQemu
-@qemu-system-riscv64.exe -M virt -m 512M -name "RiscV64 Virtual Machine" -S -gdb tcp::10000,ipv4 -show-cursor -rtc base=localtime -serial vc -kernel ..\..\..\output\xboot
+@qemu-system-riscv64.exe -M virt -m 512M -smp 2 -name "RiscV64 Virtual Machine" -S -gdb tcp::10000,ipv4 -show-cursor -rtc base=localtime -serial vc -kernel ..\..\..\output\xboot

994b2c487ba0e1c12835cc3e3acd77f6207057c5
jianjian jiang <8192542@qq.com>
2018-10-14 21:18:14 +0800

    [libc]add \r for __assert_fail

--- src/lib/libc/exit/assert.c
+++ src/lib/libc/exit/assert.c
@@ -7,6 +7,6 @@
 
 void __assert_fail(const char * expr, const char * file, int line, const char * func)
 {
-	fprintf(stderr, "Assertion failed: %s (%s: %s: %d)\n", expr, file, func, line);
+	fprintf(stderr, "Assertion failed: %s (%s: %s: %d)\r\n", expr, file, func, line);
 }
 EXPORT_SYMBOL(__assert_fail);

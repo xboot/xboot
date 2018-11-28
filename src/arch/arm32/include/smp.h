ec87ec420c17f6c0d86dda9e353e89234f5b9b3f
jiangjianjun <8192542@qq.com>
2018-11-28 11:58:49 +0800

    [arm32]fix arm32 smp.h

--- src/arch/arm32/include/smp.h
+++ src/arch/arm32/include/smp.h
@@ -12,8 +12,8 @@ static inline int smp_processor_id(void)
 	int tmp;
 
 	__asm__ __volatile__(
-		"mrc p15,0,%0,c0,c0,5\n"
-		"and %0,%0,#0x3\n"
+		"mrc p15, 0, %0, c0, c0, 5\n"
+		"and %0, %0, #0xf\n"
 		: "=r" (tmp)
 		:
 		: "memory");

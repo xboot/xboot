084169bb2fa485f33132c2cb312a6a73f2ce0e0c
jiangjianjun <8192542@qq.com>
2018-12-08 13:56:36 +0800

    [machine]using ktime_get for record time

--- src/kernel/core/machine.c
+++ src/kernel/core/machine.c
@@ -264,7 +264,7 @@ void machine_cleanup(void)
 int machine_logger(const char * fmt, ...)
 {
 	struct machine_t * mach = get_machine();
-	struct timeval tv;
+	uint64_t us;
 	char buf[SZ_4K];
 	int len = 0;
 	va_list ap;
@@ -272,8 +272,8 @@ int machine_logger(const char * fmt, ...)
 	if(mach && mach->logger)
 	{
 		va_start(ap, fmt);
-		gettimeofday(&tv, 0);
-		len += sprintf((char *)(buf + len), "[%5u.%06u]", tv.tv_sec, tv.tv_usec);
+		us = ktime_to_us(ktime_get());
+		len += sprintf((char *)(buf + len), "[%5u.%06u]", (unsigned long)(us / 1000000), (unsigned long)((us % 1000000)));
 		len += vsnprintf((char *)(buf + len), (SZ_4K - len), fmt, ap);
 		va_end(ap);
 		mach->logger(mach, (const char *)buf, len);

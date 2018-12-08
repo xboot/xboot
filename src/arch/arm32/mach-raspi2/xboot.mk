a1ae20673d8ae07f84c6982a2a3c84bc9f2429f6
jiangjianjun <8192542@qq.com>
2018-12-08 14:26:24 +0800

    [arm32]add -marm -mno-thumb-interwork MCFLAGS

--- src/arch/arm32/mach-raspi2/xboot.mk
+++ src/arch/arm32/mach-raspi2/xboot.mk
@@ -7,7 +7,7 @@ DEFINES		+= -D__ARM32_ARCH__=7 -D__CORTEX_A7__ -D__ARM32_NEON__
 ASFLAGS		:= -g -ggdb -Wall -O3
 CFLAGS		:= -g -ggdb -Wall -O3
 LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
-MCFLAGS		:= -march=armv7-a -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard
+MCFLAGS		:= -march=armv7-a -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -marm -mno-thumb-interwork
 
 LIBDIRS		:=
 LIBS 		:=

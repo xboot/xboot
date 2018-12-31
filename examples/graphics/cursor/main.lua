34ec6c536aba8fe9ea64c7f17b92aa33ddd2ecb5
jiangjianjun <8192542@qq.com>
2018-12-31 16:35:43 +0800

    [cursor]add cursor examples

---
+++ examples/graphics/cursor/main.lua
@@ -0,0 +1,12 @@
+local Pattern = require "graphic.pattern"
+
+local cursor = assets:loadDisplay("cursor.png")
+	:addEventListener(Event.MOUSE_DOWN, function(d, e) d:setPosition(e.x, e.y) end)
+	:addEventListener(Event.MOUSE_MOVE, function(d, e) d:setPosition(e.x, e.y) end)
+	:addEventListener(Event.MOUSE_UP, function(d, e) d:setPosition(e.x, e.y) end)
+	:addEventListener(Event.TOUCH_BEGIN, function(d, e) d:setPosition(e.x, e.y) end)
+	:addEventListener(Event.TOUCH_MOVE, function(d, e) d:setPosition(e.x, e.y) end)
+	:addEventListener(Event.TOUCH_END, function(d, e) d:setPosition(e.x, e.y) end)
+
+stage:addChild(cursor)
+stage:showfps(true)

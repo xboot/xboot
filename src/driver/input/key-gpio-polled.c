15d4e8f4a6747635e8fefb9e106618a1c3e493cc
jiangjianjun <8192542@qq.com>
2019-01-05 21:54:24 +0800

    [key-gpio-polled]fix key-gpio-polled bug

--- src/driver/input/key-gpio-polled.c
+++ src/driver/input/key-gpio-polled.c
@@ -128,7 +128,7 @@ static struct device_t * key_gpio_polled_probe(struct driver_t * drv, struct dtn
 	timer_init(&pdat->timer, key_gpio_polled_timer_function, input);
 	pdat->keys = keys;
 	pdat->nkeys = nkeys;
-	pdat->interval = dt_read_int(&o, "poll-interval-ms", 100);
+	pdat->interval = dt_read_int(n, "poll-interval-ms", 100);
 
 	input->name = alloc_device_name(dt_read_name(n), dt_read_id(n));
 	input->type = INPUT_TYPE_KEYBOARD;

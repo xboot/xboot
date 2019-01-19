bdb2735957680854ba389a81ce8f703054e9b551
jiangjianjun <8192542@qq.com>
2019-01-19 17:51:37 +0800

    [f1c500s]update fb-f1c500s, thanks Steward

--- src/arch/arm32/mach-f1c500s/driver/fb-f1c500s.c
+++ src/arch/arm32/mach-f1c500s/driver/fb-f1c500s.c
@@ -37,43 +37,45 @@
 #include <f1c500s/reg-debe.h>
 #include <f1c500s/reg-defe.h>
 #include <f1c500s/reg-tcon.h>
+#include <interrupt/interrupt.h>
+
+#define TCON_USES_DEBE        1
+//#define TCON_USES_GPIO        1
 
 #define F1C500S_GPIO_BASE     0x01c20800
+#define TCON0_CPU_IF_REG      0x060                 //TCON0 CPU Interface Control Register
+#define TCON0_CPU_WR_REG      0x064                 //TCON0 CPU Mode Write Register
 
-#define PD_CFG0     ((3*0x24)+0x00)       //Port n Configure Register 0 (n=0~5)
-#define PD_CFG1     ((3*0x24)+0x04)       //Port n Configure Register 1 (n=0~5)
-#define PD_CFG2     ((3*0x24)+0x08)       //Port n Configure Register 2 (n=0~5)
-#define PD_CFG3     ((3*0x24)+0x0C)       //Port n Configure Register 3 (n=0~5)
-#define PD_DATA     ((3*0x24)+0x10)       //Port n Data Register (n=0~5)
-#define PD_DRV0     ((3*0x24)+0x14)       //Port n Multi-Driving Register 0 (n=0~5)
-#define PD_DRV1     ((3*0x24)+0x18)       //Port n Multi-Driving Register 1 (n=0~5)
-#define PD_PUL0     ((3*0x24)+0x1C)       //Port n Pull Register 0 (n=0~5)
-#define PD_PUL1     ((3*0x24)+0x20)       //Port n Pull Register 1 (n=0~5)
+#define PD_CFG0               ((3*0x24)+0x00)       //Port n Configure Register 0 (n=0~5)
+#define PD_CFG1               ((3*0x24)+0x04)       //Port n Configure Register 1 (n=0~5)
+#define PD_CFG2               ((3*0x24)+0x08)       //Port n Configure Register 2 (n=0~5)
+#define PD_CFG3               ((3*0x24)+0x0C)       //Port n Configure Register 3 (n=0~5)
+#define PD_DATA               ((3*0x24)+0x10)       //Port n Data Register (n=0~5)
+#define PD_DRV0               ((3*0x24)+0x14)       //Port n Multi-Driving Register 0 (n=0~5)
+#define PD_DRV1               ((3*0x24)+0x18)       //Port n Multi-Driving Register 1 (n=0~5)
+#define PD_PUL0               ((3*0x24)+0x1C)       //Port n Pull Register 0 (n=0~5)
+#define PD_PUL1               ((3*0x24)+0x20)       //Port n Pull Register 1 (n=0~5)
 
-#define PD_INT_CFG0 (0x200+(n*0x20)+0x0)  //PIO Interrupt Configure Register 0 (n=0~2)
-#define PD_INT_CFG1 (0x200+(n*0x20)+0x4)  //PIO Interrupt Configure Register 1 (n=0~2)
-#define PD_INT_CFG2 (0x200+(n*0x20)+0x8)  //PIO Interrupt Configure Register 2 (n=0~2)
-#define PD_INT_CFG3 (0x200+(n*0x20)+0xC)  //PIO Interrupt Configure Register 3 (n=0~2)
-#define PD_INT_CTRL (0x200+(n*0x20)+0x10) //PIO Interrupt Control Register (n=0~2)
-#define PD_INT_STA  (0x200+(n*0x20)+0x14) //PIO Interrupt Status Register (n=0~2)
-#define PD_INT_DEB  (0x200+(n*0x20)+0x18) //PIO Interrupt Debounce Register (n=0~2)
+#define PD_INT_CFG0           (0x200+(n*0x20)+0x0)  //PIO Interrupt Configure Register 0 (n=0~2)
+#define PD_INT_CFG1           (0x200+(n*0x20)+0x4)  //PIO Interrupt Configure Register 1 (n=0~2)
+#define PD_INT_CFG2           (0x200+(n*0x20)+0x8)  //PIO Interrupt Configure Register 2 (n=0~2)
+#define PD_INT_CFG3           (0x200+(n*0x20)+0xC)  //PIO Interrupt Configure Register 3 (n=0~2)
+#define PD_INT_CTRL           (0x200+(n*0x20)+0x10) //PIO Interrupt Control Register (n=0~2)
+#define PD_INT_STA            (0x200+(n*0x20)+0x14) //PIO Interrupt Status Register (n=0~2)
+#define PD_INT_DEB            (0x200+(n*0x20)+0x18) //PIO Interrupt Debounce Register (n=0~2)
 
-#define PE_CFG0     ((3*0x24)+0x00)       //Port n Configure Register 0 (n=0~5)
-#define PE_CFG1     ((3*0x24)+0x04)       //Port n Configure Register 1 (n=0~5)
-#define PE_CFG2     ((3*0x24)+0x08)       //Port n Configure Register 2 (n=0~5)
-#define PE_CFG3     ((3*0x24)+0x0C)       //Port n Configure Register 3 (n=0~5)
-#define PE_DATA     ((3*0x24)+0x10)       //Port n Data Register (n=0~5)
-#define PE_DRV0     ((3*0x24)+0x14)       //Port n Multi-Driving Register 0 (n=0~5)
-#define PE_DRV1     ((3*0x24)+0x18)       //Port n Multi-Driving Register 1 (n=0~5)
-#define PE_PUL0     ((3*0x24)+0x1C)       //Port n Pull Register 0 (n=0~5)
-#define PE_PUL1     ((3*0x24)+0x20)       //Port n Pull Register 1 (n=0~5)
+#define PE_CFG0               ((3*0x24)+0x00)       //Port n Configure Register 0 (n=0~5)
+#define PE_CFG1               ((3*0x24)+0x04)       //Port n Configure Register 1 (n=0~5)
+#define PE_CFG2               ((3*0x24)+0x08)       //Port n Configure Register 2 (n=0~5)
+#define PE_CFG3               ((3*0x24)+0x0C)       //Port n Configure Register 3 (n=0~5)
+#define PE_DATA               ((3*0x24)+0x10)       //Port n Data Register (n=0~5)
+#define PE_DRV0               ((3*0x24)+0x14)       //Port n Multi-Driving Register 0 (n=0~5)
+#define PE_DRV1               ((3*0x24)+0x18)       //Port n Multi-Driving Register 1 (n=0~5)
+#define PE_PUL0               ((3*0x24)+0x1C)       //Port n Pull Register 0 (n=0~5)
+#define PE_PUL1               ((3*0x24)+0x20)       //Port n Pull Register 1 (n=0~5)
 
-#define SDR_PAD_DRV 0x2C0                 //SDRAM Pad Multi-Driving Register
-#define SDR_PAD_PUL 0x2C4                 //SDRAM Pad Pull Register
-
-#define F1C500S_TCON_BASE      	(0x01c0c000)
-#define TCON0_CPU_IF_REG          0x060 //TCON0 CPU Interface Control Register
-#define TCON0_CPU_WR_REG          0x064 //TCON0 CPU Mode Write Register
+#define SDR_PAD_DRV           0x2C0                 //SDRAM Pad Multi-Driving Register
+#define SDR_PAD_PUL           0x2C4                 //SDRAM Pad Pull Register
 
 struct fb_f1c500s_pdata_t
 {
@@ -112,6 +114,7 @@ struct fb_f1c500s_pdata_t
 		int clk_active;
 	} timing;
 
+  volatile int fb_swap;
 	struct led_t * backlight;
 	int brightness;
 };
@@ -172,10 +175,10 @@ static inline void f1c500s_debe_set_mode(struct fb_f1c500s_pdata_t * pdat)
 
 	write32((virtual_addr_t)&debe->disp_size, (((pdat->height) - 1) << 16) | (((pdat->width) - 1) << 0));
 	write32((virtual_addr_t)&debe->layer0_size, (((pdat->height) - 1) << 16) | (((pdat->width) - 1) << 0));
-	write32((virtual_addr_t)&debe->layer0_stride, ((pdat->width) << 5));
+	write32((virtual_addr_t)&debe->layer0_stride, ((pdat->width) << 5)); // 5:32bits
 	write32((virtual_addr_t)&debe->layer0_addr_low32b, (u32_t)(pdat->vram[pdat->index]) << 3);
 	write32((virtual_addr_t)&debe->layer0_addr_high4b, (u32_t)(pdat->vram[pdat->index]) >> 29);
-	write32((virtual_addr_t)&debe->layer0_attr1_ctrl, 0x09 << 8);
+	write32((virtual_addr_t)&debe->layer0_attr1_ctrl, 0x09 << 8); // 9:rgb888
 
 	val = read32((virtual_addr_t)&debe->mode);
 	val |= (1 << 8);
@@ -224,8 +227,75 @@ static inline void f1c500s_tcon_disable(struct fb_f1c500s_pdata_t * pdat)
 	write32((virtual_addr_t)&tcon->tcon1_io_tristate, 0xffffffff);
 }
 
+#if defined(TCON_USES_DEBE)
+static void f1c500s_tcon_vsync_irq_handler(void * data)
+{
+  u32_t reg_val;
+  struct f1c500s_tcon_reg_t * tcon = (struct f1c500s_tcon_reg_t *)phys_to_virt(F1C500S_TCON_BASE);
+  struct fb_f1c500s_pdata_t * pdat = (struct fb_f1c500s_pdata_t *)data;
+  
+	//clear interrupt flag
+  reg_val = read32((virtual_addr_t)&tcon->int0);
+  reg_val &= ~(1 << 15);
+  write32((virtual_addr_t)&tcon->int0, reg_val);
+
+  if(pdat->fb_swap == 1)
+  {
+    pdat->index = (pdat->index + 1) & 0x1;
+    f1c500s_debe_set_address(pdat, pdat->vram[pdat->index]);
+    pdat->fb_swap = 0;
+  }
+}
+#endif
+
 static inline void f1c500s_tcon_set_mode(struct fb_f1c500s_pdata_t * pdat)
 {
+#if defined(TCON_USES_DEBE)
+  u32_t val;
+  int bp, total;
+  struct f1c500s_tcon_reg_t * tcon = (struct f1c500s_tcon_reg_t *)pdat->virttcon;
+  
+  // IO_MAP_SEL tcon0  +0x00
+  val = read32((virtual_addr_t)&tcon->ctrl);
+  val &= ~(0x1 << 0); 
+  write32((virtual_addr_t)&tcon->ctrl, val);
+  
+  // enable tcon0, sta delay == val, using i80 interface + 0x44    
+  val = (pdat->timing.v_front_porch + pdat->timing.v_back_porch + pdat->timing.v_sync_len);
+  write32((virtual_addr_t)&tcon->tcon0_ctrl, (1 << 31) | ((val & 0x1f) << 4) | (0x01 << 24));
+  
+  // enable all clock , clock divisor == val
+  val = clk_get_rate(pdat->clktcon) / pdat->timing.pixel_clock_hz;
+  write32((virtual_addr_t)&tcon->tcon0_dclk, (0xf << 28) | (val << 0));
+  
+  // screen size
+  write32((virtual_addr_t)&tcon->tcon0_timing_active, ((pdat->width - 1) << 16) | ((pdat->height - 1) << 0));
+
+  // horizontal back porch and horizontal total time ( in DCLK ).
+  bp = pdat->timing.h_sync_len + pdat->timing.h_back_porch;
+
+  // note: in serial i80 mode, width *3 + h_front_porch + bp.
+  total = pdat->width * 3 + pdat->timing.h_front_porch + bp; 
+  write32((virtual_addr_t)&tcon->tcon0_timing_h, ((total - 1) << 16) | ((bp - 1) << 0));
+
+  // vertical back porch and vertical total time (in DCLK).
+  bp = pdat->timing.v_sync_len + pdat->timing.v_back_porch;
+  total = pdat->height + pdat->timing.v_front_porch + bp;
+  write32((virtual_addr_t)&tcon->tcon0_timing_v, ((total * 2) << 16) | ((bp - 1) << 0));
+
+  // horizontal and vertical pulse width (in DCLK).
+  write32((virtual_addr_t)&tcon->tcon0_timing_sync, 
+    ((pdat->timing.h_sync_len - 1) << 16) | ((pdat->timing.v_sync_len - 1) << 0));
+
+  // ignore
+  write32((virtual_addr_t)&tcon->tcon0_hv_intf, 0);
+  
+  // 8bit 65k mode  rgb565    ???(pin A1 value in 8080 mode auto/flash states)???
+  write32((virtual_addr_t)&tcon->tcon0_cpu_intf, (0x4 << 29) | (1 << 26));
+  write32((virtual_addr_t)&tcon->tcon0_io_polarity, (1 << 28));
+  write32((virtual_addr_t)&tcon->tcon0_io_tristate, 0);
+  request_irq(29, f1c500s_tcon_vsync_irq_handler, IRQ_TYPE_NONE, pdat);
+#else
 	struct f1c500s_tcon_reg_t * tcon = (struct f1c500s_tcon_reg_t *)pdat->virttcon;
 	int bp, total;
 	u32_t val;
@@ -246,11 +316,11 @@ static inline void f1c500s_tcon_set_mode(struct fb_f1c500s_pdata_t * pdat)
 	bp = pdat->timing.v_sync_len + pdat->timing.v_back_porch;
 	total = pdat->height + pdat->timing.v_front_porch + bp;
 	write32((virtual_addr_t)&tcon->tcon0_timing_v, ((total * 2) << 16) | ((bp - 1) << 0));
-	write32((virtual_addr_t)&tcon->tcon0_timing_sync, ((pdat->timing.h_sync_len - 1) << 16) | ((pdat->timing.v_sync_len - 1) << 0));
+	write32((virtual_addr_t)&tcon->tcon0_timing_sync, 
+    ((pdat->timing.h_sync_len - 1) << 16) | ((pdat->timing.v_sync_len - 1) << 0));
 
 	write32((virtual_addr_t)&tcon->tcon0_hv_intf, 0);
 	write32((virtual_addr_t)&tcon->tcon0_cpu_intf, 0xe4000000); // 8080 mode
-
 	if(pdat->bits_per_pixel == 18 || pdat->bits_per_pixel == 16)
 	{
 		write32((virtual_addr_t)&tcon->tcon0_frm_seed[0], 0x11111111);
@@ -263,20 +333,30 @@ static inline void f1c500s_tcon_set_mode(struct fb_f1c500s_pdata_t * pdat)
 		write32((virtual_addr_t)&tcon->tcon0_frm_table[1], 0x15151111);
 		write32((virtual_addr_t)&tcon->tcon0_frm_table[2], 0x57575555);
 		write32((virtual_addr_t)&tcon->tcon0_frm_table[3], 0x7f7f7777);
-		write32((virtual_addr_t)&tcon->tcon0_frm_ctrl, (pdat->bits_per_pixel == 18) ? ((1 << 31) | (0 << 4)) : ((1 << 31) | (5 << 4)));
+		write32((virtual_addr_t)&tcon->tcon0_frm_ctrl, 
+      (pdat->bits_per_pixel == 18) ? ((1 << 31) | (0 << 4)) : ((1 << 31) | (5 << 4)));
 	}
 
 	val = (1 << 28);
 	if(!pdat->timing.h_sync_active)
+  {
 		val |= (1 << 25);
+  }
 	if(!pdat->timing.v_sync_active)
+  {
 		val |= (1 << 24);
+  }
 	if(!pdat->timing.den_active)
+  {
 		val |= (1 << 27);
+  }
 	if(!pdat->timing.clk_active)
+  {
 		val |= (1 << 26);
+  }
 	write32((virtual_addr_t)&tcon->tcon0_io_polarity, val);
 	write32((virtual_addr_t)&tcon->tcon0_io_tristate, 0);
+#endif
 }
 
 static inline void fb_f1c500s_cfg_gpios(int base, int n, int cfg, enum gpio_pull_t pull, enum gpio_drv_t drv)
@@ -289,14 +369,28 @@ static inline void fb_f1c500s_cfg_gpios(int base, int n, int cfg, enum gpio_pull
 	}
 }
 
+static inline void f1c500s_lcd_init()
+{
+  u32_t reg_val;
+  struct f1c500s_tcon_reg_t * tcon = (struct f1c500s_tcon_reg_t *)phys_to_virt(F1C500S_TCON_BASE);
+  
+	// enable vertical blanking irq.
+  write32((virtual_addr_t)&tcon->int0, (1 << 31));
+
+  // start auto transfer 
+  reg_val = read32((virtual_addr_t)&tcon->tcon0_cpu_intf);
+  reg_val |= (1 << 28);
+  write32((virtual_addr_t)&tcon->tcon0_cpu_intf, reg_val);
+}
+
 static inline void fb_f1c500s_init(struct fb_f1c500s_pdata_t * pdat)
 {
 	fb_f1c500s_cfg_gpios(F1C500S_GPIOD0, 22, 0x2, GPIO_PULL_NONE, GPIO_DRV_STRONG);
-
 	f1c500s_tcon_disable(pdat);
 	f1c500s_debe_set_mode(pdat);
 	f1c500s_tcon_set_mode(pdat);
 	f1c500s_tcon_enable(pdat);
+	f1c500s_lcd_init();
 }
 
 static void fb_setbl(struct framebuffer_t * fb, int brightness)
@@ -337,7 +431,6 @@ static struct render_t * fb_create(struct framebuffer_t * fb)
 	render->pixels = pixels;
 	render->pixlen = pixlen;
 	render->priv = NULL;
-
 	return render;
 }
 
@@ -352,29 +445,38 @@ static void fb_destroy(struct framebuffer_t * fb, struct render_t * render)
 
 static void fb_present(struct framebuffer_t * fb, struct render_t * render, struct dirty_rect_t * rect, int nrect)
 {
+  size_t pixlen=0;
 	struct fb_f1c500s_pdata_t * pdat = (struct fb_f1c500s_pdata_t *)fb->priv;
 
-	if(render && render->pixels)
-	{
-		pdat->index = (pdat->index + 1) & 0x1;
-		memcpy(pdat->vram[pdat->index], render->pixels, render->pixlen);
-		dma_cache_sync(pdat->vram[pdat->index], render->pixlen, DMA_TO_DEVICE);
-		f1c500s_debe_set_address(pdat, pdat->vram[pdat->index]);
-		{
-			uint32_t x, y;
-			uint32_t val, *p = render->pixels;
+#if defined(TCON_USES_DEBE)
+	while(pdat->fb_swap == 1);
 
-			r61520_lcd_cmd(pdat, 0x2c);
-			for(y = 0; y < 240; y++)
-			{
-				for(x = 0; x < 320; x++)
-				{
-					val = *p++;
-					r61520_lcd_dat(pdat, ((val & 0xf80000) >> 8) | ((val & 0x00fc00) >> 5) | ((val & 0xf8) >> 3));
-				}
+	if(render && render->pixels){
+		memcpy(pdat->vram[(pdat->index + 1) & 0x1], render->pixels, render->pixlen);
+		pixlen = render->pixlen;
+    pdat->fb_swap = 1;
+	}
+#else
+	if(render && render->pixels) {
+		uint32_t x, y;
+		uint32_t val, *p = render->pixels;
+		
+		//pdat->index = (pdat->index + 1) & 0x1;
+		//memcpy(pdat->vram[pdat->index], render->pixels, render->pixlen);
+		//dma_cache_sync(pdat->vram[pdat->index], render->pixlen, DMA_TO_DEVICE);
+		//f1c500s_debe_set_address(pdat, pdat->vram[pdat->index]);
+		
+		r61520_lcd_cmd(pdat, 0x2c);
+		for(y = 0; y < 240; y++) 
+    {
+			for(x = 0; x < 320; x++) 
+      {
+				val = *p++;
+				r61520_lcd_dat(pdat, ((val & 0xf80000) >> 8) | ((val & 0x00fc00) >> 5) | ((val & 0xf8) >> 3));
 			}
 		}
-	}
+  }
+#endif
 }
 
 static void sunxi_lcdc_gpio_config(struct fb_f1c500s_pdata_t * pdat, uint32_t use_gpio)
@@ -674,7 +776,6 @@ static struct device_t * fb_f1c500s_probe(struct driver_t * drv, struct dtnode_t
 		reset_deassert(pdat->rsttcon);
 	for(i = 0x0800; i < 0x1000; i += 4)
 		write32(pdat->virtdebe + i, 0);
-	fb_f1c500s_init(pdat);
 
 	if(!register_framebuffer(&dev, fb))
 	{
@@ -696,6 +797,10 @@ static struct device_t * fb_f1c500s_probe(struct driver_t * drv, struct dtnode_t
 
 	sunxi_lcdc_gpio_config(pdat, 1);
 	r61520_lcd_init(pdat);
+#if defined(TCON_USES_DEBE)
+	sunxi_lcdc_gpio_config(pdat, 0);
+#endif
+	fb_f1c500s_init(pdat);
 
 	return dev;
 }

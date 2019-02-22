4a3555dddebab17dea4faabec333cc108d9f56da
jiangjianjun <8192542@qq.com>
2019-02-22 17:45:58 +0800

    [theme]add ios theme

--- src/romdisk/framework/assets/themes/ios/init.lua
+++ src/romdisk/framework/assets/themes/ios/init.lua
@@ -1,15 +1,15 @@
 local Pattern = Pattern
 
 local function D(name)
-	return ("assets/themes/default/" .. name)
+	return ("assets/themes/ios/" .. name)
 end
 
 local common = {
 	fontFamily = "assets/fonts/Roboto-Regular.ttf",
 	fontSize = 24,
 	textPatternNormal = Pattern.color(1, 1, 1),
-	textPatternPressed = Pattern.color(0.5, 0.5, 0.1),
-	textPatternDisabled = Pattern.color(0.4, 0.4, 0.4),
+	textPatternPressed = Pattern.color(1, 1, 1),
+	textPatternDisabled = Pattern.color(1, 1, 1),
 }
 
 return {

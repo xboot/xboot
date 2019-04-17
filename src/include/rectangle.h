f5cf828808434abb82ba405f8e96cac30a5c30fb
jiangjianjun <8192542@qq.com>
2019-04-17 17:39:34 +0800

    [rectangle]update rectangle library

--- src/include/rectangle.h
+++ src/include/rectangle.h
@@ -11,6 +11,10 @@ struct rectangle_t {
 };
 
 void rectangle_init(struct rectangle_t * r, double x, double y, double w, double h);
+int rectangle_isempty(struct rectangle_t * r);
+int rectangle_contains(struct rectangle_t * r, struct rectangle_t * o);
+int rectangle_intersect(struct rectangle_t * r, struct rectangle_t * a, struct rectangle_t * b);
+int rectangle_union(struct rectangle_t * r, struct rectangle_t * a, struct rectangle_t * b);
 
 #ifdef __cplusplus
 }

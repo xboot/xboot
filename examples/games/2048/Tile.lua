42fc36a9b4aba7ef60bccc7783a927054af5e797
jiangjianjun <8192542@qq.com>
2019-05-06 20:11:15 +0800

    [2048]fix 2048

--- examples/games/2048/Tile.lua
+++ examples/games/2048/Tile.lua
@@ -6,18 +6,18 @@ function M:init(x, y, n)
 	self.gy = y
 	self.n = n
 	self.o = assets:loadDisplay("assets/images/tile" .. self.n .. ".png")
-		:setPosition((self.gx - 1) * 70 + 55, (self.gy - 1) * 70 + 171)
+	self:addChild(self.o)
+	self:setPosition((self.gx - 1) * 70 + 55, (self.gy - 1) * 70 + 171)
 		:setAnchor(0.5, 0.5)
 		:setScale(0.1, 0.1)
 		:animate({scalex = 1, scaley = 1}, 0.3, "quint-in-out")
-	self:addChild(self.o)
 end
 
 function M:move(x, y)
 	if self.gx ~= x or self.gy ~= y then
 		self.gx = x
 		self.gy = y
-		self.o:animate({x = (x - 1) * 70 + 55, y = (y - 1) * 70 + 171}, 0.3, "quint-in-out")
+		self:animate({x = (x - 1) * 70 + 55, y = (y - 1) * 70 + 171}, 0.3, "quint-in-out")
 	end
 end
 
--- src/romdisk/application/launcher/games/2048/Tile.lua
+++ src/romdisk/application/launcher/games/2048/Tile.lua
@@ -2,24 +2,22 @@ local M = Class(DisplayObject)
 
 function M:init(x, y, n)
 	self.super:init(60, 60)
-
-	local assets = assets
 	self.gx = x
 	self.gy = y
 	self.n = n
 	self.o = assets:loadDisplay("games/2048/images/tile" .. self.n .. ".png")
-		:setPosition((self.gx - 1) * 70 + 55, (self.gy - 1) * 70 + 171)
+	self:addChild(self.o)
+	self:setPosition((self.gx - 1) * 70 + 55, (self.gy - 1) * 70 + 171)
 		:setAnchor(0.5, 0.5)
 		:setScale(0.1, 0.1)
 		:animate({scalex = 1, scaley = 1}, 0.3, "quint-in-out")
-	self:addChild(self.o)
 end
 
 function M:move(x, y)
 	if self.gx ~= x or self.gy ~= y then
 		self.gx = x
 		self.gy = y
-		self.o:animate({x = (x - 1) * 70 + 55, y = (y - 1) * 70 + 171}, 0.3, "quint-in-out")
+		self:animate({x = (x - 1) * 70 + 55, y = (y - 1) * 70 + 171}, 0.3, "quint-in-out")
 	end
 end
 

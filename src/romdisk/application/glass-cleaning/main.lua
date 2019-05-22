87a8513fac3aa7b0a2b5cd662ec57f2c3b4c5829
jianjian jiang <8192542@qq.com>
2019-05-22 22:59:43 +0800

    [glass-cleaning]optimie glass cleaning

--- src/romdisk/application/glass-cleaning/main.lua
+++ src/romdisk/application/glass-cleaning/main.lua
@@ -3,45 +3,26 @@ local sw, sh = stage:getSize()
 local bgimg = Image.new("assets/images/background.png")
 stage:addChild(DisplayImage.new(bgimg))
 
-local maskimg = DisplayShape.new(sw, sh)
-	:setSource(Pattern.image(bgimg:clone():blur(200)))
+local mask = DisplayShape.new(sw, sh)
+	:setSource(Pattern.image(bgimg:clone():blur(120)))
 	:paint()
 	:setSource(Pattern.image(Image.new("assets/images/water.png")))
 	:paint()
-	:snapshot()
-local mask = DisplayImage.new(maskimg)
+	:setOperator("clear")
 stage:addChild(mask)
 
 local function onMouseDown(self, e)
 	if self:hitTestPoint(e.x, e.y) then
 		self.touchid = -1
 		e.stop = true
-		local m = DisplayShape.new(sw, sh)
-			:setSource(Pattern.image(maskimg))
-			:paint()
-			:arc(e.x, e.y, 30, 0, 2 * math.pi)
-			:setOperator("clear")
-			:fill()
-			:snapshot()
-		maskimg:clear():mask(m)
-		mask:markDirty()
-		collectgarbage("collect")
 	end
 end
 
 local function onMouseMove(self, e)
 	if self.touchid == -1 then
-		e.stop = true
-		local m = DisplayShape.new(sw, sh)
-			:setSource(Pattern.image(maskimg))
-			:paint()
-			:arc(e.x, e.y, 30, 0, 2 * math.pi)
-			:setOperator("clear")
-			:fill()
-			:snapshot()
-		maskimg:clear():mask(m)
+		mask:arc(e.x, e.y, 30, 0, 2 * math.pi):fill()
 		mask:markDirty()
-		collectgarbage("collect")
+		e.stop = true
 	end
 end
 
@@ -56,32 +37,14 @@ local function onTouchBegin(self, e)
 	if self:hitTestPoint(e.x, e.y) then
 		self.touchid = e.id
 		e.stop = true
-		local m = DisplayShape.new(sw, sh)
-			:setSource(Pattern.image(maskimg))
-			:paint()
-			:arc(e.x, e.y, 30, 0, 2 * math.pi)
-			:setOperator("clear")
-			:fill()
-			:snapshot()
-		maskimg:clear():mask(m)
-		mask:markDirty()
-		collectgarbage("collect")
 	end
 end
 
 local function onTouchMove(self, e)
 	if self.touchid == e.id then
-		e.stop = true
-		local m = DisplayShape.new(sw, sh)
-			:setSource(Pattern.image(maskimg))
-			:paint()
-			:arc(e.x, e.y, 30, 0, 2 * math.pi)
-			:setOperator("clear")
-			:fill()
-			:snapshot()
-		maskimg:clear():mask(m)
+		mask:arc(e.x, e.y, 30, 0, 2 * math.pi):fill()
 		mask:markDirty()
-		collectgarbage("collect")
+		e.stop = true
 	end
 end
 
@@ -99,5 +62,5 @@ mask:addEventListener("touch-begin", onTouchBegin)
 mask:addEventListener("touch-move", onTouchMove)
 mask:addEventListener("touch-end", onTouchEnd)
 
-stage:showfps(false)
+stage:showfps(true)
 stage:showobj(false)

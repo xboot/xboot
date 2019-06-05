0db3e9ab65a0fc2f582a7f6e3b1c0bab166fa407
jiangjianjun <8192542@qq.com>
2019-06-05 19:50:04 +0800

    [launcher]update launcher

--- src/romdisk/application/launcher/main.lua
+++ src/romdisk/application/launcher/main.lua
@@ -10,35 +10,39 @@ local bbg = nil
 local scroll = nil
 local showtask = false
 
+local homekey = nil
 stage:addEventListener("key-down", function(d, e)
 	if e.key == 10 then
-		if showtask then
-		    stage:removeChild(bbg)
-			stage:removeChild(scroll)
-		    stage:addChild(bg)
-			stage:addChild(pager)
-			bbg = nil
-			scroll = nil
-			showtask = false
-		else
-			bbg = DisplayImage.new(stage:snapshot():blur(32))
-			scroll = TaskScroll.new(sw, sh / 2):setPosition(0, sh / 4)
-			stage:removeChild(bg)
-			stage:removeChild(pager)
-			stage:addChild(bbg)
-			stage:addChild(scroll)
-			showtask = true
+		if homekey then
+			local delta = e.time - homekey
+			if delta < 300000000 then
+				homekey = nil
+				if not showtask then
+					bbg = DisplayImage.new(stage:snapshot():blur(32))
+					scroll = TaskScroll.new(sw, sh / 2):setPosition(0, sh / 4)
+					stage:removeChild(bg)
+					stage:removeChild(pager)
+					stage:addChild(bbg)
+					stage:addChild(scroll)
+					showtask = true
+				end
+				return
+			end
 		end
-	elseif e.key == 12 then
-		if showtask then
-			stage:removeChild(bbg)
-			stage:removeChild(scroll)
-			stage:addChild(bg)
-			stage:addChild(pager)
-			bbg = nil
-			scroll = nil
-			showtask = false
-		end
+		homekey = e.time
+		stage:addTimer(Timer.new(0.3, 1, function(t)
+			if homekey then
+				if showtask then
+					stage:removeChild(bbg)
+					stage:removeChild(scroll)
+					stage:addChild(bg)
+					stage:addChild(pager)
+					bbg = nil
+					scroll = nil
+					showtask = false
+				end
+			end
+		end))
 	end
 end)
 

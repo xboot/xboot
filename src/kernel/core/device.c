c8949aa3f838205178273340234569af37512d74
jiangjianjun <8192542@qq.com>
2019-06-27 12:10:49 +0800

    [laserscan]remove laserscan driver model

--- src/kernel/core/device.c
+++ src/kernel/core/device.c
@@ -114,9 +114,6 @@ static struct kobj_t * search_device_kobj(struct device_t * dev)
 	case DEVICE_TYPE_IRQCHIP:
 		name = "irqchip";
 		break;
-	case DEVICE_TYPE_LASERSCAN:
-		name = "laserscan";
-		break;
 	case DEVICE_TYPE_LED:
 		name = "led";
 		break;

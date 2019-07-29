bbb0b53bad35a42696e88d03665737eeccd99080
jiangjianjun <8192542@qq.com>
2019-07-29 13:41:13 +0800

    [examples]add analogclock2 examples

---
+++ examples/graphics/analogclock2/AnalogClock.lua
@@ -0,0 +1,49 @@
+local M = Class(DisplayObject)
+local os = os
+
+function M:init()
+	self.super:init()
+
+	local face = assets:loadDisplay("clock-face.png")
+	local width, height = face:getSize()
+	
+	local hour = assets:loadDisplay("clock-hour-hand.png")
+	hour:setPosition(width / 2, height /2)
+		:setAnchor(0.5, 0.5)
+		:setRotation(0)
+
+	local minute = assets:loadDisplay("clock-minute-hand.png")
+	minute:setPosition(width / 2, height /2)
+		:setAnchor(0.5, 0.5)
+		:setRotation(0)
+
+	local second = assets:loadDisplay("clock-second-hand.png")
+	second:setPosition(width / 2, height /2)
+		:setAnchor(0.5, 0.5)
+		:setRotation(0)
+
+	self:setSize(width, height)
+	self:addChild(face)
+	self:addChild(hour)
+	self:addChild(minute)
+	self:addChild(second)
+
+	local time = os.date("*t");
+	hour:animate({rotation = time.hour * 360 / 12 + time.min * 360 / (12 * 60) + time.sec * 360 / (12 * 60 * 60)}, 0.5, "cubic-in-out")
+	minute:animate({rotation = time.min * 360 / 60 + time.sec * 360 / (60 * 60)}, 0.5, "cubic-in-out")
+	second:animate({rotation = time.sec * 360 / 60}, 0.5, "cubic-in-out")
+
+	stage:addTimer(Timer.new(1, 0, function(t)
+		local time = os.date("*t");
+		hour:setRotation(time.hour * 360 / 12 + time.min * 360 / (12 * 60) + time.sec * 360 / (12 * 60 * 60))
+		minute:setRotation(time.min * 360 / 60 + time.sec * 360 / (60 * 60))
+		second:setRotation(time.sec * 360 / 60)
+	end))
+end
+
+function M:setSize(width, height)
+	self.super:setSize(width, height)
+	return self
+end
+
+return M
---
+++ examples/graphics/analogclock2/main.lua
@@ -0,0 +1,4 @@
+local AnalogClock = require("AnalogClock")
+
+stage:addChild(AnalogClock.new():setLayoutEnable(true))
+stage:setLayoutDirection("row"):setLayoutJustify("center"):setLayoutAlign("center")

c6e2e8db3a7c6fa3540a36a15f1b0ea334bcd861
jiangjianjun <8192542@qq.com>
2019-07-30 18:41:14 +0800

    [px30]add clk-px30-pll driver

---
+++ src/arch/arm64/mach-px30/driver/clk-px30-pll.c
@@ -0,0 +1,225 @@
+/*
+ * driver/clk-px30-pll.c
+ *
+ * Copyright(c) 2007-2019 Jianjun Jiang <8192542@qq.com>
+ * Official site: http://xboot.org
+ * Mobile phone: +86-18665388956
+ * QQ: 8192542
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <clk/clk.h>
+#include <px30/reg-cru.h>
+#include <px30/reg-pmu-cru.h>
+
+struct clk_px30_pll_pdata_t {
+	virtual_addr_t cru;
+	virtual_addr_t pmucru;
+	char * parent;
+	int channel;
+};
+
+static void clk_px30_pll_set_parent(struct clk_t * clk, const char * pname)
+{
+}
+
+static const char * clk_px30_pll_get_parent(struct clk_t * clk)
+{
+	struct clk_px30_pll_pdata_t * pdat = (struct clk_px30_pll_pdata_t *)clk->priv;
+	return pdat->parent;
+}
+
+static void clk_px30_pll_set_enable(struct clk_t * clk, bool_t enable)
+{
+}
+
+static bool_t clk_px30_pll_get_enable(struct clk_t * clk)
+{
+	return TRUE;
+}
+
+static void clk_px30_pll_set_rate(struct clk_t * clk, u64_t prate, u64_t rate)
+{
+}
+
+static u64_t clk_px30_pll_get_rate(struct clk_t * clk, u64_t prate)
+{
+	struct clk_px30_pll_pdata_t * pdat = (struct clk_px30_pll_pdata_t *)clk->priv;
+	u64_t rate = 0;
+	u32_t con, mode;
+	u32_t postdiv1, postdiv2, fbdiv, refdiv;
+
+	switch(pdat->channel)
+	{
+	case 0 ... 3:
+		con = read32(pdat->cru + CRU_PLL_CON(pdat->channel, 0));
+		postdiv1 = (con >> 12) & 0x7;
+		fbdiv = (con >> 0) & 0xfff;
+		con = read32(pdat->cru + CRU_PLL_CON(pdat->channel, 1));
+		postdiv2 = (con >> 6) & 0x7;
+		refdiv = (con >> 0) & 0x3f;
+		mode = (read32(pdat->cru + CRU_MODE) >> (pdat->channel * 2)) & 0x3;
+		break;
+
+	case 4:
+		return 0;
+
+	default:
+		return 0;
+	}
+
+	if(mode == 0x1)
+	{
+		rate = prate * fbdiv / (u64_t)(refdiv * postdiv1 * postdiv2);
+	}
+	else if(mode == 0x2)
+	{
+		rate = 32768;
+	}
+	else
+	{
+		rate = prate;
+	}
+
+	return rate;
+}
+
+static struct device_t * clk_px30_pll_probe(struct driver_t * drv, struct dtnode_t * n)
+{
+	struct clk_px30_pll_pdata_t * pdat;
+	struct clk_t * clk;
+	struct device_t * dev;
+	struct dtnode_t o;
+	char * parent = dt_read_string(n, "parent", NULL);
+	char * name = dt_read_string(n, "name", NULL);
+	int channel = dt_read_int(n, "channel", -1);
+
+	if(channel < 0 || channel > 7)
+		return NULL;
+
+	if(!parent || !name)
+		return NULL;
+
+	if(!search_clk(parent) || search_clk(name))
+		return NULL;
+
+	pdat = malloc(sizeof(struct clk_px30_pll_pdata_t));
+	if(!pdat)
+		return NULL;
+
+	clk = malloc(sizeof(struct clk_t));
+	if(!clk)
+	{
+		free(pdat);
+		return NULL;
+	}
+
+	pdat->cru = phys_to_virt(PX30_CRU_BASE);
+	pdat->pmucru = phys_to_virt(PX30_PMU_CRU_BASE);
+	pdat->parent = strdup(parent);
+	pdat->channel = channel;
+
+	clk->name = strdup(name);
+	clk->count = 0;
+	clk->set_parent = clk_px30_pll_set_parent;
+	clk->get_parent = clk_px30_pll_get_parent;
+	clk->set_enable = clk_px30_pll_set_enable;
+	clk->get_enable = clk_px30_pll_get_enable;
+	clk->set_rate = clk_px30_pll_set_rate;
+	clk->get_rate = clk_px30_pll_get_rate;
+	clk->priv = pdat;
+
+	if(!register_clk(&dev, clk))
+	{
+		free(pdat->parent);
+
+		free(clk->name);
+		free(clk->priv);
+		free(clk);
+		return NULL;
+	}
+	dev->driver = drv;
+
+	if(dt_read_object(n, "default", &o))
+	{
+		char * c = clk->name;
+		char * p;
+		u64_t r;
+		int e;
+
+		if((p = dt_read_string(&o, "parent", NULL)) && search_clk(p))
+			clk_set_parent(c, p);
+		if((r = (u64_t)dt_read_long(&o, "rate", 0)) > 0)
+			clk_set_rate(c, r);
+		if((e = dt_read_bool(&o, "enable", -1)) != -1)
+		{
+			if(e > 0)
+				clk_enable(c);
+			else
+				clk_disable(c);
+		}
+	}
+	return dev;
+}
+
+static void clk_px30_pll_remove(struct device_t * dev)
+{
+	struct clk_t * clk = (struct clk_t *)dev->priv;
+	struct clk_px30_pll_pdata_t * pdat = (struct clk_px30_pll_pdata_t *)clk->priv;
+
+	if(clk && unregister_clk(clk))
+	{
+		free(pdat->parent);
+
+		free(clk->name);
+		free(clk->priv);
+		free(clk);
+	}
+}
+
+static void clk_px30_pll_suspend(struct device_t * dev)
+{
+}
+
+static void clk_px30_pll_resume(struct device_t * dev)
+{
+}
+
+static struct driver_t clk_px30_pll = {
+	.name		= "clk-px30-pll",
+	.probe		= clk_px30_pll_probe,
+	.remove		= clk_px30_pll_remove,
+	.suspend	= clk_px30_pll_suspend,
+	.resume		= clk_px30_pll_resume,
+};
+
+static __init void clk_px30_pll_driver_init(void)
+{
+	register_driver(&clk_px30_pll);
+}
+
+static __exit void clk_px30_pll_driver_exit(void)
+{
+	unregister_driver(&clk_px30_pll);
+}
+
+driver_initcall(clk_px30_pll_driver_init);
+driver_exitcall(clk_px30_pll_driver_exit);
--- src/arch/arm64/mach-px30/include/px30/reg-cru.h
+++ src/arch/arm64/mach-px30/include/px30/reg-cru.h
@@ -5,9 +5,11 @@
 
 #define CRU_PLL_CON(id, i)		((id) * 0x20 + ((i) * 4))
 #define CRU_CLKSELS_CON(i)		(0x100 + ((i) * 4))
-#define CRU_CLKGATES_CON(i)		(0x300 + ((i) * 4))
-#define CRU_SOFTRSTS_CON(i)		(0x400 + ((i) * 4))
+#define CRU_CLKGATES_CON(i)		(0x200 + ((i) * 4))
+#define CRU_SOFTRSTS_CON(i)		(0x300 + ((i) * 4))
 
+#define CRU_MODE				(0x0a0)
+#define CRU_MISC				(0x0a4)
 #define CRU_GLB_CNT_TH			(0x0b0)
 #define CRU_GLB_RST_ST			(0x0b4)
 #define CRU_GLB_SRST_FST_VALUE	(0x0b8)

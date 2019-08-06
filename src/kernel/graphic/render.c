cdedd917621957848cc98c358a1b2a6fd3b9499f
jiangjianjun <8192542@qq.com>
2019-08-06 21:09:03 +0800

    [render]add render_default_fill

--- src/kernel/graphic/render.c
+++ src/kernel/graphic/render.c
@@ -35,6 +35,51 @@ void render_default_blit(struct surface_t * s, struct region_t * clip, struct ma
 
 void render_default_fill(struct surface_t * s, struct region_t * clip, struct matrix_t * m, int w, int h, struct color_t * c)
 {
+	struct region_t r, region;
+	struct matrix_t t;
+	uint32_t * p, v;
+	int ds = surface_get_stride(s) >> 2;
+	int x1, y1, x2, y2, stride;
+	int x, y, ox, oy;
+	double fx, fy, ofx, ofy;
+
+	region_init(&r, 0, 0, surface_get_width(s), surface_get_height(s));
+	if(clip)
+	{
+		if(!region_intersect(&r, &r, clip))
+			return;
+	}
+	matrix_transform_region(m, w, h, &region);
+	if(!region_intersect(&r, &r, &region))
+		return;
+
+	color_get_pma(c, &v);
+	x1 = r.x;
+	y1 = r.y;
+	x2 = r.x + r.w;
+	y2 = r.y + r.h;
+	stride = ds - r.w;
+	p = (uint32_t *)surface_get_pixels(s) + y1 * ds + x1;
+	fx = x1;
+	fy = y1;
+	memcpy(&t, m, sizeof(struct matrix_t));
+	matrix_invert(&t);
+	matrix_transform_point(&t, &fx, &fy);
+
+	for(y = y1; y < y2; ++y, fx += t.c, fy += t.d)
+	{
+		ofx = fx;
+		ofy = fy;
+		for(x = x1; x < x2; ++x, ofx += t.a, ofy += t.b)
+		{
+			ox = (int)ofx;
+			oy = (int)ofy;
+			if(ox >= 0 && ox < w && oy >= 0 && oy < h)
+				*p = v;
+			p++;
+		}
+		p += stride;
+	}
 }
 
 void render_default_filter_haldclut(struct surface_t * s, struct surface_t * clut, const char * type)

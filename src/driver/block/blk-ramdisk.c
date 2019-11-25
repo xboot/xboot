b823554ab44864b221701375b36679ea67c1b3db
jiangjianjun <8192542@qq.com>
2019-11-25 09:52:02 +0800

    [ramdisk]rename ramdisk to blk-ramdisk

--- src/driver/block/blk-ramdisk.c
+++ src/driver/block/blk-ramdisk.c
@@ -1,5 +1,5 @@
 /*
- * driver/block/ramdisk.c
+ * driver/block/blk-ramdisk.c
  *
  * Copyright(c) 2007-2019 Jianjun Jiang <8192542@qq.com>
  * Official site: http://xboot.org
@@ -29,37 +29,37 @@
 #include <xboot.h>
 #include <block/block.h>
 
-struct ramdisk_pdata_t
+struct blk_ramdisk_pdata_t
 {
 	virtual_addr_t addr;
 	virtual_size_t size;
 };
 
-static u64_t ramdisk_read(struct block_t * blk, u8_t * buf, u64_t blkno, u64_t blkcnt)
+static u64_t blk_ramdisk_read(struct block_t * blk, u8_t * buf, u64_t blkno, u64_t blkcnt)
 {
-	struct ramdisk_pdata_t * pdat = (struct ramdisk_pdata_t *)(blk->priv);
+	struct blk_ramdisk_pdata_t * pdat = (struct blk_ramdisk_pdata_t *)(blk->priv);
 	virtual_addr_t offset = pdat->addr + block_offset(blk, blkno);
 	u64_t length = block_size(blk) * blkcnt;
 	memcpy((void *)buf, (const void *)(offset), length);
 	return blkcnt;
 }
 
-static u64_t ramdisk_write(struct block_t * blk, u8_t * buf, u64_t blkno, u64_t blkcnt)
+static u64_t blk_ramdisk_write(struct block_t * blk, u8_t * buf, u64_t blkno, u64_t blkcnt)
 {
-	struct ramdisk_pdata_t * pdat = (struct ramdisk_pdata_t *)(blk->priv);
+	struct blk_ramdisk_pdata_t * pdat = (struct blk_ramdisk_pdata_t *)(blk->priv);
 	virtual_addr_t offset = pdat->addr + block_offset(blk, blkno);
 	u64_t length = block_size(blk) * blkcnt;
 	memcpy((void *)(offset), (const void *)buf, length);
 	return blkcnt;
 }
 
-static void ramdisk_sync(struct block_t * blk)
+static void blk_ramdisk_sync(struct block_t * blk)
 {
 }
 
-static struct device_t * ramdisk_probe(struct driver_t * drv, struct dtnode_t * n)
+static struct device_t * blk_ramdisk_probe(struct driver_t * drv, struct dtnode_t * n)
 {
-	struct ramdisk_pdata_t * pdat;
+	struct blk_ramdisk_pdata_t * pdat;
 	struct block_t * blk;
 	struct device_t * dev;
 	u64_t blkcnt, blksz = SZ_512;
@@ -70,7 +70,7 @@ static struct device_t * ramdisk_probe(struct driver_t * drv, struct dtnode_t * 
 		return NULL;
 	blkcnt = size / blksz;
 
-	pdat = malloc(sizeof(struct ramdisk_pdata_t));
+	pdat = malloc(sizeof(struct blk_ramdisk_pdata_t));
 	if(!pdat)
 		return NULL;
 
@@ -87,9 +87,9 @@ static struct device_t * ramdisk_probe(struct driver_t * drv, struct dtnode_t * 
 	blk->name = alloc_device_name(dt_read_name(n), dt_read_id(n));
 	blk->blksz	= blksz;
 	blk->blkcnt	= blkcnt;
-	blk->read = ramdisk_read;
-	blk->write = ramdisk_write;
-	blk->sync = ramdisk_sync;
+	blk->read = blk_ramdisk_read;
+	blk->write = blk_ramdisk_write;
+	blk->sync = blk_ramdisk_sync;
 	blk->priv = pdat;
 
 	if(!(dev = register_block(blk, drv)))
@@ -102,7 +102,7 @@ static struct device_t * ramdisk_probe(struct driver_t * drv, struct dtnode_t * 
 	return dev;
 }
 
-static void ramdisk_remove(struct device_t * dev)
+static void blk_ramdisk_remove(struct device_t * dev)
 {
 	struct block_t * blk = (struct block_t *)dev->priv;
 
@@ -115,31 +115,31 @@ static void ramdisk_remove(struct device_t * dev)
 	}
 }
 
-static void ramdisk_suspend(struct device_t * dev)
+static void blk_ramdisk_suspend(struct device_t * dev)
 {
 }
 
-static void ramdisk_resume(struct device_t * dev)
+static void blk_ramdisk_resume(struct device_t * dev)
 {
 }
 
-static struct driver_t ramdisk = {
-	.name		= "ramdisk",
-	.probe		= ramdisk_probe,
-	.remove		= ramdisk_remove,
-	.suspend	= ramdisk_suspend,
-	.resume		= ramdisk_resume,
+static struct driver_t blk_ramdisk = {
+	.name		= "blk-ramdisk",
+	.probe		= blk_ramdisk_probe,
+	.remove		= blk_ramdisk_remove,
+	.suspend	= blk_ramdisk_suspend,
+	.resume		= blk_ramdisk_resume,
 };
 
-static __init void ramdisk_driver_init(void)
+static __init void blk_ramdisk_driver_init(void)
 {
-	register_driver(&ramdisk);
+	register_driver(&blk_ramdisk);
 }
 
-static __exit void ramdisk_driver_exit(void)
+static __exit void blk_ramdisk_driver_exit(void)
 {
-	unregister_driver(&ramdisk);
+	unregister_driver(&blk_ramdisk);
 }
 
-driver_initcall(ramdisk_driver_init);
-driver_exitcall(ramdisk_driver_exit);
+driver_initcall(blk_ramdisk_driver_init);
+driver_exitcall(blk_ramdisk_driver_exit);

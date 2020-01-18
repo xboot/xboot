5420aa4bcfd6e855c44fec32dd53c73521f16e0c
jiangjianjun <8192542@qq.com>
2020-01-18 11:42:00 +0800

    [s3]add s3 dma support

---
+++ src/arch/arm32/mach-s3/include/s3-dma.h
@@ -0,0 +1,41 @@
+#ifndef __S3_DMA_H__
+#define __S3_DMA_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define S3_DMA_CHANNEL0			(0)
+#define S3_DMA_CHANNEL1			(1)
+#define S3_DMA_CHANNEL2			(2)
+#define S3_DMA_CHANNEL3			(3)
+#define S3_DMA_CHANNEL4			(4)
+#define S3_DMA_CHANNEL5			(5)
+#define S3_DMA_CHANNEL6			(6)
+#define S3_DMA_CHANNEL7			(7)
+
+#define S3_DMA_PORT_SRAM		(0)
+#define S3_DMA_PORT_SDRAM		(1)
+#define S3_DMA_PORT_I2S_PCM_RX	(3)
+#define S3_DMA_PORT_I2S_PCM_TX	(3)
+#define S3_DMA_PORT_UART0_RX	(6)
+#define S3_DMA_PORT_UART0_TX	(6)
+#define S3_DMA_PORT_UART1_RX	(7)
+#define S3_DMA_PORT_UART1_TX	(7)
+#define S3_DMA_PORT_UART2_RX	(8)
+#define S3_DMA_PORT_UART2_TX	(8)
+#define S3_DMA_PORT_CODEC		(15)
+#define S3_DMA_PORT_CE_RX		(16)
+#define S3_DMA_PORT_CE_TX		(16)
+#define S3_DMA_PORT_OTG_EP1		(17)
+#define S3_DMA_PORT_OTG_EP2		(18)
+#define S3_DMA_PORT_OTG_EP3		(19)
+#define S3_DMA_PORT_OTG_EP4		(20)
+#define S3_DMA_PORT_SPI_RX		(23)
+#define S3_DMA_PORT_SPI_TX		(23)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __S3_DMA_H__ */

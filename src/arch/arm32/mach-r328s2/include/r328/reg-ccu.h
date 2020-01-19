2de08940f6252a77ae592768bbfa071ebcff676c
jiangjianjun <8192542@qq.com>
2020-01-19 11:26:28 +0800

    [r328]temp add pll clk

---
+++ src/arch/arm32/mach-r328s2/include/r328/reg-ccu.h
@@ -0,0 +1,13 @@
+#ifndef __R328_REG_CCU_H__
+#define __R328_REG_CCU_H__
+
+#define R328_CCU_BASE			(0x03001000)
+
+#define CCU_PLL_CPU				(0x0000)
+#define CCU_PLL_DDR				(0x0010)
+#define CCU_PLL_PERIPH0			(0x0020)
+#define CCU_PLL_PERIPH1			(0x0028)
+#define CCU_PLL_AUDIO			(0x0078)
+#define CCU_PLL_32K				(0x00d8)
+
+#endif /* __R328_REG_CCU_H__ */

da3aa732f893e709da20c8188dffa7e1dd817cf3
jiangjianjun <8192542@qq.com>
2020-04-15 17:22:56 +0800

    [r328]add r328 gpio driver

---
+++ src/arch/arm32/mach-r328s2/include/r328-gpio.h
@@ -0,0 +1,71 @@
+#ifndef __R328_GPIO_H__
+#define __R328_GPIO_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R328_GPIOB0			(32)
+#define R328_GPIOB1			(33)
+#define R328_GPIOB2			(34)
+#define R328_GPIOB3			(35)
+#define R328_GPIOB4			(36)
+#define R328_GPIOB5			(37)
+#define R328_GPIOB6			(38)
+#define R328_GPIOB7			(39)
+#define R328_GPIOB8			(40)
+#define R328_GPIOB9			(41)
+#define R328_GPIOB10		(42)
+#define R328_GPIOB11		(43)
+#define R328_GPIOB12		(44)
+#define R328_GPIOB13		(45)
+
+#define R328_GPIOC0			(64)
+#define R328_GPIOC1			(65)
+#define R328_GPIOC2			(66)
+#define R328_GPIOC3			(67)
+#define R328_GPIOC4			(68)
+#define R328_GPIOC5			(69)
+#define R328_GPIOC6			(70)
+
+#define R328_GPIOE0			(128)
+#define R328_GPIOE1			(129)
+#define R328_GPIOE2			(130)
+#define R328_GPIOE3			(131)
+#define R328_GPIOE4			(132)
+#define R328_GPIOE5			(133)
+#define R328_GPIOE6			(134)
+
+#define R328_GPIOG0			(192)
+#define R328_GPIOG1			(193)
+#define R328_GPIOG2			(194)
+#define R328_GPIOG3			(195)
+#define R328_GPIOG4			(196)
+#define R328_GPIOG5			(197)
+#define R328_GPIOG6			(198)
+#define R328_GPIOG7			(199)
+#define R328_GPIOG8			(200)
+#define R328_GPIOG9			(201)
+#define R328_GPIOG10		(202)
+#define R328_GPIOG11		(203)
+#define R328_GPIOG12		(204)
+#define R328_GPIOG13		(205)
+#define R328_GPIOG14		(206)
+#define R328_GPIOG15		(207)
+
+#define R328_GPIOH0			(224)
+#define R328_GPIOH1			(225)
+#define R328_GPIOH2			(226)
+#define R328_GPIOH3			(227)
+#define R328_GPIOH4			(228)
+#define R328_GPIOH5			(229)
+#define R328_GPIOH6			(230)
+#define R328_GPIOH7			(231)
+#define R328_GPIOH8			(232)
+#define R328_GPIOH9			(233)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __R328_GPIO_H__ */

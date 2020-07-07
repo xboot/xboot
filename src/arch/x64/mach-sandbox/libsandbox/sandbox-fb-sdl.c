73bf36ee691af2d18a8430e347d1e940134edf75
JianjunJiang <8192542@qq.com>
2020-07-07 10:43:17 +0800

    [sandbox]using SDL_UpdateWindowSurfaceRects for present framebuffer

--- src/arch/x64/mach-sandbox/libsandbox/sandbox-fb-sdl.c
+++ src/arch/x64/mach-sandbox/libsandbox/sandbox-fb-sdl.c
@@ -123,26 +123,22 @@ int sandbox_fb_sdl_surface_present(void * context, struct sandbox_fb_surface_t *
 {
 	struct sandbox_fb_sdl_context_t * ctx = (struct sandbox_fb_sdl_context_t *)context;
 	struct sandbox_fb_region_t * r;
-	SDL_Rect rect;
-	int i;
+	int i, n;
 
-	if(rl && (rl->count > 0))
+	if(rl && (n = rl->count > 0))
 	{
-		for(i = 0; i < rl->count; i++)
+		for(i = 0; i < n; i++)
 		{
 			r = &rl->region[i];
-			rect.x = r->x;
-			rect.y = r->y;
-			rect.w = r->w;
-			rect.h = r->h;
-			SDL_BlitSurface(surface->priv, &rect, ctx->screen, &rect);
+			SDL_BlitSurface(surface->priv, (SDL_Rect*)r, ctx->screen, (SDL_Rect*)r);
 		}
+		SDL_UpdateWindowSurfaceRects(ctx->window, (SDL_Rect*)&rl->region[0], n);
 	}
 	else
 	{
 		SDL_BlitSurface(surface->priv, NULL, ctx->screen, NULL);
+		SDL_UpdateWindowSurface(ctx->window);
 	}
-	SDL_UpdateWindowSurface(ctx->window);
 	return 1;
 }
 

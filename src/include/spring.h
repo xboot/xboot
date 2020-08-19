1fa40452e671a605f326bdc1d937f03078c439ee
JianjunJiang <8192542@qq.com>
2020-08-19 17:16:38 +0800

    [spring]update spring function, using semi-implicit euler method

--- src/include/spring.h
+++ src/include/spring.h
@@ -6,18 +6,12 @@ extern "C" {
 #endif
 
 struct spring_t {
-	double tension;
-	double friction;
-
 	double start;
 	double stop;
-	double position;
 	double velocity;
-	double pposition;
-	double pvelocity;
-	double tposition;
-	double tvelocity;
-	double accumulator;
+	double tension;
+	double friction;
+	double acc;
 };
 
 void spring_init(struct spring_t * s, double start, double stop, double velocity, double tension, double friction);
@@ -25,7 +19,7 @@ int spring_step(struct spring_t * s, double dt);
 
 static inline double spring_position(struct spring_t * s)
 {
-	return s->position;
+	return s->start;
 }
 
 static inline double spring_velocity(struct spring_t * s)

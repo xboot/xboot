9cda06e8f15ef49a931d1892a5e42ef44ae9c0f1
JianjunJiang <8192542@qq.com>
2020-09-25 18:30:42 +0800

    [stdint]add some macro for stdint

--- src/include/stdint.h
+++ src/include/stdint.h
@@ -7,17 +7,22 @@ extern "C" {
 
 #include <types.h>
 
-typedef s8_t					int8_t;
-typedef u8_t					uint8_t;
+typedef s8_t			int8_t;
+typedef u8_t			uint8_t;
 
-typedef s16_t					int16_t;
-typedef u16_t					uint16_t;
+typedef s16_t			int16_t;
+typedef u16_t			uint16_t;
 
-typedef s32_t					int32_t;
-typedef u32_t					uint32_t;
+typedef s32_t			int32_t;
+typedef u32_t			uint32_t;
 
-typedef s64_t					int64_t;
-typedef u64_t					uint64_t;
+typedef s64_t			int64_t;
+typedef u64_t			uint64_t;
+
+#define UINT8_MAX		(0xff)
+#define UINT16_MAX		(0xffff)
+#define UINT32_MAX		(0xffffffff)
+#define UINT64_MAX		(0xffffffffffffffffULL)
 
 #ifdef __cplusplus
 }

1b443a1db017c6c1df3a91c11172115a83970d9b
chenjun <675816156@qq.com>
2020-10-07 01:36:11 -0700

    remove D extension
    see details: https://github.com/kendryte/kendryte-gnu-toolchain/commit/289ea18741ac1f54e1752e1e7a8f16f1fa880ede:
    
    (cherry picked from commit 00033e03c71dda97c440e206410f14091f0f71bd)

--- src/arch/riscv64/mach-k210/xboot.mk
+++ src/arch/riscv64/mach-k210/xboot.mk
@@ -7,7 +7,7 @@ DEFINES		+=
 ASFLAGS		:= -g -ggdb -Wall -O3
 CFLAGS		:= -g -ggdb -Wall -O3
 LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
-MCFLAGS		:= -march=rv64imafdc -mabi=lp64d -mcmodel=medany
+MCFLAGS		:= -march=rv64imafc -mabi=lp64f -mcmodel=medany
 
 LIBDIRS		:=
 LIBS 		:=

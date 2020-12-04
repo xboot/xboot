a224327e6d683f67110195fd6b1a0a8120404d8f
JianjunJiang <8192542@qq.com>
2020-12-04 13:45:59 +0800

    [v831]add v831 platform

---
+++ src/arch/arm32/mach-v831/maix-ii.c
@@ -0,0 +1,117 @@
+/*
+ * maix-ii.c
+ *
+ * Copyright(c) 2007-2020 Jianjun Jiang <8192542@qq.com>
+ * Official site: http://xboot.org
+ * Mobile phone: +86-18665388956
+ * QQ: 8192542
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <xboot.h>
+
+static int mach_detect(struct machine_t * mach)
+{
+	return 1;
+}
+
+static void mach_smpinit(struct machine_t * mach)
+{
+}
+
+static void mach_smpboot(struct machine_t * mach, void (*func)(void))
+{
+}
+
+static void mach_shutdown(struct machine_t * mach)
+{
+}
+
+static void mach_reboot(struct machine_t * mach)
+{
+}
+
+static void mach_sleep(struct machine_t * mach)
+{
+}
+
+static void mach_cleanup(struct machine_t * mach)
+{
+}
+
+static void mach_logger(struct machine_t * mach, const char * buf, int count)
+{
+	virtual_addr_t virt = phys_to_virt(0x05000000);
+	int i;
+
+	for(i = 0; i < count; i++)
+	{
+		while((read32(virt + 0x14) & (0x1 << 6)) == 0);
+		write32(virt + 0x00, buf[i]);
+	}
+}
+
+static const char * mach_uniqueid(struct machine_t * mach)
+{
+	static char uniqueid[32 + 3 + 1] = { 0 };
+	virtual_addr_t virt = phys_to_virt(0x03006200);
+	u32_t sid0, sid1, sid2, sid3;
+
+	sid0 = read32(virt + 0 * 4);
+	sid1 = read32(virt + 1 * 4);
+	sid2 = read32(virt + 2 * 4);
+	sid3 = read32(virt + 3 * 4);
+	snprintf(uniqueid, sizeof(uniqueid), "%08x:%08x:%08x:%08x",sid0, sid1, sid2, sid3);
+	return uniqueid;
+}
+
+static int mach_keygen(struct machine_t * mach, const char * msg, void * key)
+{
+	return 0;
+}
+
+static struct machine_t maix_ii = {
+	.name 		= "maix-ii",
+	.desc 		= "Sipeed Maix-II Based On Allwinner V831 SOC",
+	.detect 	= mach_detect,
+	.smpinit	= mach_smpinit,
+	.smpboot	= mach_smpboot,
+	.shutdown	= mach_shutdown,
+	.reboot		= mach_reboot,
+	.sleep		= mach_sleep,
+	.cleanup	= mach_cleanup,
+	.logger		= mach_logger,
+	.uniqueid	= mach_uniqueid,
+	.keygen		= mach_keygen,
+};
+
+static __init void maix_ii_machine_init(void)
+{
+	register_machine(&maix_ii);
+}
+
+static __exit void maix_ii_machine_exit(void)
+{
+	unregister_machine(&maix_ii);
+}
+
+machine_initcall(maix_ii_machine_init);
+machine_exitcall(maix_ii_machine_exit);

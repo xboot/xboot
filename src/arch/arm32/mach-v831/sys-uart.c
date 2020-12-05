ea56f8f0208c2cc774aab0e28cd5d649d6864d72
JianjunJiang <8192542@qq.com>
2020-12-05 14:08:04 +0800

    [v831]add uart0 gpio config, and modify sys_uart_init

--- src/arch/arm32/mach-v831/sys-uart.c
+++ src/arch/arm32/mach-v831/sys-uart.c
@@ -30,36 +30,35 @@
 
 void sys_uart_init(void)
 {
-#if 0
 	virtual_addr_t addr;
 	u32_t val;
 
-	/* Config GPIOB8 and GPIOB9 to txd0 and rxd0 */
-	addr = 0x01c20824 + 0x04;
+	/* Config GPIOH9 and GPIOH10 to txd0 and rxd0 */
+	addr = 0x0300b0fc + 0x04;
 	val = read32(addr);
-	val &= ~(0xf << ((8 & 0x7) << 2));
-	val |= ((0x3 & 0x7) << ((8 & 0x7) << 2));
+	val &= ~(0xf << ((9 & 0x7) << 2));
+	val |= ((0x5 & 0x7) << ((9 & 0x7) << 2));
 	write32(addr, val);
 
 	val = read32(addr);
-	val &= ~(0xf << ((9 & 0x7) << 2));
-	val |= ((0x3 & 0x7) << ((9 & 0x7) << 2));
+	val &= ~(0xf << ((10 & 0x7) << 2));
+	val |= ((0x5 & 0x7) << ((10 & 0x7) << 2));
 	write32(addr, val);
 
 	/* Open the clock gate for uart0 */
-	addr = 0x01c2006c;
+	addr = 0x0300190c;
 	val = read32(addr);
-	val |= 1 << 16;
+	val |= 1 << 0;
 	write32(addr, val);
 
 	/* Deassert uart0 reset */
-	addr = 0x01c202d8;
+	addr = 0x0300190c;
 	val = read32(addr);
 	val |= 1 << 16;
 	write32(addr, val);
 
 	/* Config uart0 to 115200-8-1-0 */
-	addr = 0x01c28000;
+	addr = 0x05000000;
 	write32(addr + 0x04, 0x0);
 	write32(addr + 0x08, 0xf7);
 	write32(addr + 0x10, 0x0);
@@ -75,7 +74,6 @@ void sys_uart_init(void)
 	val &= ~0x1f;
 	val |= (0x3 << 0) | (0 << 2) | (0x0 << 3);
 	write32(addr + 0x0c, val);
-#endif
 }
 
 void sys_uart_putc(char c)

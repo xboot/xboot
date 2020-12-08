5e2fe4379dbee11a46663f879c07aa35fc055439
JianjunJiang <8192542@qq.com>
2020-12-08 15:33:00 +0800

    [v831]fix return_to_fel

--- src/arch/arm32/mach-v831/sys-copyself.c
+++ src/arch/arm32/mach-v831/sys-copyself.c
@@ -32,34 +32,72 @@ extern unsigned char __image_start;
 extern unsigned char __image_end;
 extern void return_to_fel(void);
 extern void sys_uart_putc(char c);
-extern void sys_spinor_init(void);
-extern void sys_spinor_exit(void);
-extern void sys_spinor_read(int addr, void * buf, int count);
 
 enum {
 	BOOT_DEVICE_FEL		= 0,
-	BOOT_DEVICE_MMC0	= 1,
-	BOOT_DEVICE_MMC2	= 2,
-	BOOT_DEVICE_SPI		= 3,
+	BOOT_DEVICE_SDCARD	= 1,
+	BOOT_DEVICE_SPINOR	= 2,
+	BOOT_DEVICE_SPINAND	= 3,
+	BOOT_DEVICE_EMMC	= 4,
 };
 
 static int get_boot_device(void)
 {
 	u32_t * sig = (void *)0x4;
-	u32_t d = sig[9] & 0xf;
 
 	if((sig[0] == 0x4e4f4765) && (sig[1] == 0x3054422e))
 	{
-		if(d == 0)
-			return BOOT_DEVICE_MMC0;
-		else if(d == 2)
-			return BOOT_DEVICE_MMC2;
-		else if(d == 3)
-			return BOOT_DEVICE_SPI;
+		return BOOT_DEVICE_SPINOR;
 	}
 	return BOOT_DEVICE_FEL;
 }
 
 void sys_copyself(void)
 {
+	int d = get_boot_device();
+	void * mem;
+	u32_t size;
+
+	if(d == BOOT_DEVICE_FEL)
+	{
+		sys_uart_putc('B');
+		sys_uart_putc('o');
+		sys_uart_putc('o');
+		sys_uart_putc('t');
+		sys_uart_putc(' ');
+		sys_uart_putc('t');
+		sys_uart_putc('o');
+		sys_uart_putc(' ');
+		sys_uart_putc('F');
+		sys_uart_putc('E');
+		sys_uart_putc('L');
+		sys_uart_putc(' ');
+		sys_uart_putc('m');
+		sys_uart_putc('o');
+		sys_uart_putc('d');
+		sys_uart_putc('e');
+		sys_uart_putc('\r');
+		sys_uart_putc('\n');
+		return_to_fel();
+	}
+	else if(d == BOOT_DEVICE_SDCARD)
+	{
+		mem = (void *)&__image_start;
+		size = (&__image_end - &__image_start + 512) >> 9;
+	}
+	else if(d == BOOT_DEVICE_SPINOR)
+	{
+		mem = (void *)&__image_start;
+		size = &__image_end - &__image_start;
+	}
+	else if(d == BOOT_DEVICE_SPINAND)
+	{
+		mem = (void *)&__image_start;
+		size = (&__image_end - &__image_start + 512) >> 9;
+	}
+	else if(d == BOOT_DEVICE_EMMC)
+	{
+		mem = (void *)&__image_start;
+		size = (&__image_end - &__image_start + 512) >> 9;
+	}
 }

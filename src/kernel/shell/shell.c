70a0137cdf3d73181237cd4c38ab2e54ca7c5ce4
JianjunJiang <8192542@qq.com>
2020-12-09 14:15:16 +0800

    [shell]remove parser.c and parser.h

--- src/kernel/shell/shell.c
+++ src/kernel/shell/shell.c
@@ -30,7 +30,6 @@
 #include <framework/vm.h>
 #include <command/command.h>
 #include <shell/readline.h>
-#include <shell/parser.h>
 #include <shell/shell.h>
 
 static char shell_cwd[VFS_MAX_PATH] = { '/', '\0', };
@@ -146,6 +145,203 @@ int shell_setcwd(const char * path)
 	return -1;
 }
 
+enum paser_state_t {
+	PARSER_STATE_TEXT 		= 1,
+	PARSER_STATE_ESC		= 2,
+	PARSER_STATE_QUOTE		= 3,
+	PARSER_STATE_DQUOTE		= 4,
+	PARSER_STATE_VAR		= 5,
+	PARSER_STATE_VARNAME	= 6,
+	PARSER_STATE_VARNAME2	= 7,
+	PARSER_STATE_QVAR		= 8,
+	PARSER_STATE_QVARNAME	= 9,
+	PARSER_STATE_QVARNAME2	= 10,
+};
+
+struct parser_state_transition_t {
+	enum paser_state_t from_state;
+	enum paser_state_t to_state;
+	char input;
+	int keep_value;
+};
+
+static struct parser_state_transition_t state_transitions[] = {
+	{ PARSER_STATE_TEXT, PARSER_STATE_QUOTE, '\'', 0},
+	{ PARSER_STATE_TEXT, PARSER_STATE_DQUOTE, '\"', 0},
+	{ PARSER_STATE_TEXT, PARSER_STATE_VAR, '$', 0},
+	{ PARSER_STATE_TEXT, PARSER_STATE_ESC, '\\', 0},
+	{ PARSER_STATE_ESC, PARSER_STATE_TEXT, 0, 1},
+	{ PARSER_STATE_QUOTE, PARSER_STATE_TEXT, '\'', 0},
+	{ PARSER_STATE_DQUOTE, PARSER_STATE_TEXT, '\"', 0},
+	{ PARSER_STATE_DQUOTE,PARSER_STATE_QVAR, '$', 0},
+	{ PARSER_STATE_VAR, PARSER_STATE_VARNAME2, '{', 0},
+	{ PARSER_STATE_VAR, PARSER_STATE_VARNAME, 0, 1},
+	{ PARSER_STATE_VARNAME, PARSER_STATE_TEXT, ' ', 1},
+	{ PARSER_STATE_VARNAME2, PARSER_STATE_TEXT, '}', 0},
+	{ PARSER_STATE_QVAR, PARSER_STATE_QVARNAME2, '{', 0},
+	{ PARSER_STATE_QVAR, PARSER_STATE_QVARNAME, 0, 1},
+	{ PARSER_STATE_QVARNAME, PARSER_STATE_DQUOTE, ' ', 1},
+	{ PARSER_STATE_QVARNAME, PARSER_STATE_TEXT, '\"', 0},
+	{ PARSER_STATE_QVARNAME2, PARSER_STATE_DQUOTE, '}', 0},
+	{ 0, 0, 0, 0}
+};
+
+static enum paser_state_t shell_parser_state(enum paser_state_t state, char c, char * result)
+{
+	struct parser_state_transition_t * transition;
+	struct parser_state_transition_t * next_match = 0;
+	struct parser_state_transition_t default_transition;
+	int found = 0;
+
+	default_transition.to_state = state;
+	default_transition.keep_value = 1;
+	for(transition = state_transitions; transition->from_state; transition++)
+	{
+		if((transition->from_state == state) && (transition->input == c))
+		{
+			found = 1;
+			break;
+		}
+		if((transition->from_state == state) && (transition->input == 0))
+			next_match = transition;
+	}
+	if(!found)
+	{
+		if(next_match)
+			transition = next_match;
+		else
+			transition = &default_transition;
+	}
+	if(transition->keep_value)
+		*result = c;
+	else
+		*result = 0;
+	return transition->to_state;
+}
+
+static int is_varstate(enum paser_state_t s)
+{
+	if((s == PARSER_STATE_VARNAME) || (s == PARSER_STATE_VARNAME2) || (s == PARSER_STATE_QVARNAME) || (s == PARSER_STATE_QVARNAME2))
+		return 1;
+	else
+		return 0;
+}
+
+static int shell_parser(const char * cmdline, int * argc, char *** argv, char ** pos)
+{
+	enum paser_state_t state = PARSER_STATE_TEXT;
+	enum paser_state_t newstate;
+	char *rd = (char *)cmdline;
+	char c, *args, *val;
+	char *buffer, *bp;
+	char *varname, *vp;
+	int i;
+
+	*argc = 0;
+	*pos = 0;
+	bp = buffer = malloc(SZ_4K);
+	if(!buffer)
+	{
+		*argc = 0;
+		return 0;
+	}
+	vp = varname = malloc(SZ_256);
+	if(!varname)
+	{
+		*argc = 0;
+		return 0;
+	}
+	do {
+		if(!(*rd))
+			break;
+		for(; *rd; rd++)
+		{
+			newstate = shell_parser_state(state, *rd, &c);
+			if(is_varstate(state) && !is_varstate(newstate))
+			{
+				*(vp++) = '\0';
+				val = getenv(varname);
+				vp = varname;
+				if(val)
+				{
+					for(; *val; val++)
+						*(bp++) = *val;
+				}
+			}
+			if(is_varstate(newstate))
+			{
+				if(c)
+					*(vp++) = c;
+			}
+			else
+			{
+				if(newstate == PARSER_STATE_TEXT && state != PARSER_STATE_ESC && c == ' ')
+				{
+					if(bp != buffer && *(bp - 1))
+					{
+						*(bp++) = '\0';
+						(*argc)++;
+					}
+				}
+				else if(newstate == PARSER_STATE_TEXT && state != PARSER_STATE_ESC && c == ';')
+				{
+					if(bp != buffer && *(bp - 1))
+					{
+						*(bp++) = '\0';
+						(*argc)++;
+					}
+					*pos = rd + 1;
+					break;
+				}
+				else if(c)
+				{
+					*(bp++) = c;
+				}
+			}
+			state = newstate;
+		}
+	} while((state != PARSER_STATE_TEXT) && (!is_varstate(state)));
+	*(bp++) = '\0';
+	if(is_varstate(state) && !is_varstate(PARSER_STATE_TEXT))
+	{
+		*(vp++) = '\0';
+		val = getenv(varname);
+		vp = varname;
+		if(val)
+		{
+			for(; *val; val++)
+				*(bp++) = *val;
+		}
+	}
+	args = malloc(bp - buffer);
+	if(!args)
+	{
+		*argc = 0;
+		return 0;
+	}
+	memcpy(args, buffer, bp - buffer);
+	*argv = malloc(sizeof(char *) * (*argc + 1));
+	if(!*argv)
+	{
+		*argc = 0;
+		free(args);
+		return 0;
+	}
+	bp = args;
+	for(i = 0; i < *argc; i++)
+	{
+		(*argv)[i] = bp;
+		while(*bp)
+			bp++;
+		bp++;
+	}
+	(*argv)[i] = 0;
+	free(buffer);
+	free(varname);
+
+	return 1;
+}
+
 static int __vmexec(const char * path, const char * fb, const char * input)
 {
 	return -1;
@@ -174,7 +370,7 @@ int shell_system(const char * cmdline)
 	p = buf;
 	while(*p)
 	{
-		if(parser(p, &n, &args, &pos))
+		if(shell_parser(p, &n, &args, &pos))
 		{
 			if(n > 0)
 			{
@@ -199,13 +395,11 @@ int shell_system(const char * cmdline)
 			free(args[0]);
 			free(args);
     	}
-
 		if(!pos)
 			*p = 0;
 		else
 			p = pos;
     }
-
 	free(buf);
 	return 1;
 }

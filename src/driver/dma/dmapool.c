1697fd9de3ece4168aa1ed7ad94ebe3c61f7ab6a
JianjunJiang <8192542@qq.com>
2020-12-16 10:42:05 +0800

    [dma]using weak function for dma pool

--- src/driver/dma/dmapool.c
+++ src/driver/dma/dmapool.c
@@ -29,83 +29,32 @@
 #include <xboot.h>
 #include <dma/dmapool.h>
 
-static void * __dma_pool = NULL;
-static spinlock_t __dma_lock = SPIN_LOCK_INIT();
-
-void * dma_alloc_coherent(unsigned long size)
-{
-	irq_flags_t flags;
-	void * m;
-
-	spin_lock_irqsave(&__dma_lock, flags);
-	m = mm_memalign(__dma_pool, SZ_4K, size);
-	spin_unlock_irqrestore(&__dma_lock, flags);
-
-	return m;
-}
-
-void dma_free_coherent(void * addr)
-{
-	irq_flags_t flags;
-
-	spin_lock_irqsave(&__dma_lock, flags);
-	mm_free(__dma_pool, addr);
-	spin_unlock_irqrestore(&__dma_lock, flags);
-}
-
-void * dma_alloc_noncoherent(unsigned long size)
+static void * __dma_alloc_coherent(unsigned long size)
 {
 	return memalign(SZ_4K, size);
 }
+extern __typeof(__dma_alloc_coherent) dma_alloc_coherent __attribute__((weak, alias("__dma_alloc_coherent")));
 
-void dma_free_noncoherent(void * addr)
+static void __dma_free_coherent(void * addr)
 {
 	free(addr);
 }
+extern __typeof(__dma_free_coherent) dma_free_coherent __attribute__((weak, alias("__dma_free_coherent")));
+
+static void * __dma_alloc_noncoherent(unsigned long size)
+{
+	return memalign(SZ_4K, size);
+}
+extern __typeof(__dma_alloc_noncoherent) dma_alloc_noncoherent __attribute__((weak, alias("__dma_alloc_noncoherent")));
+
+static void __dma_free_noncoherent(void * addr)
+{
+	free(addr);
+}
+extern __typeof(__dma_free_noncoherent) dma_free_noncoherent __attribute__((weak, alias("__dma_free_noncoherent")));
 
 static void __dma_cache_sync(void * addr, unsigned long size, int dir)
 {
 }
 extern __typeof(__dma_cache_sync) dma_cache_sync __attribute__((weak, alias("__dma_cache_sync")));
 
-static struct kobj_t * search_class_memory_kobj(void)
-{
-	struct kobj_t * kclass = kobj_search_directory_with_create(kobj_get_root(), "class");
-	return kobj_search_directory_with_create(kclass, "memory");
-}
-
-static ssize_t memory_read_dmainfo(struct kobj_t * kobj, void * buf, size_t size)
-{
-	void * mm = (void *)kobj->priv;
-	size_t mused, mfree;
-	char * p = buf;
-	int len = 0;
-
-	mm_info(mm, &mused, &mfree);
-	len += sprintf((char *)(p + len), " dma used: %ld\r\n", mused);
-	len += sprintf((char *)(p + len), " dma free: %ld\r\n", mfree);
-	return len;
-}
-
-static __init void dma_pure_init(void)
-{
-	void * dma;
-	size_t size;
-
-#ifdef __SANDBOX__
-	extern void * sandbox_get_dma_buffer(void);
-	extern size_t sandbox_get_dma_size(void);
-	dma = sandbox_get_dma_buffer();
-	size = sandbox_get_dma_size();
-#else
-	extern unsigned char __dma_start[];
-	extern unsigned char __dma_end[];
-	dma = (void *)__dma_start;
-	size = (size_t)(__dma_end - __dma_start);
-#endif
-
-	spin_lock_init(&__dma_lock);
-	__dma_pool = mm_create(dma, size);
-	kobj_add_regular(search_class_memory_kobj(), "dmainfo", memory_read_dmainfo, NULL, mm_get(__dma_pool));
-}
-pure_initcall(dma_pure_init);

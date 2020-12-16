5a6bae5872b9f7d414b986a54a58ccf2ff4b7567
JianjunJiang <8192542@qq.com>
2020-12-16 17:34:10 +0800

    [arm64]add -mstrict-align for arm64 platform

--- src/arch/arm64/mach-nswitch/xboot.mk
+++ src/arch/arm64/mach-nswitch/xboot.mk
@@ -7,7 +7,7 @@ DEFINES		+= -D__ARM64_ARCH__=8 -D__CORTEX_A57__ -D__ARM64_NEON__
 ASFLAGS		:= -g -ggdb -Wall -O2
 CFLAGS		:= -g -ggdb -Wall -O2
 LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
-MCFLAGS		:= -march=armv8-a -mcpu=cortex-a57 -mtune=cortex-a57
+MCFLAGS		:= -march=armv8-a -mcpu=cortex-a57 -mtune=cortex-a57 -mstrict-align
 
 LIBDIRS		:=
 LIBS 		:=

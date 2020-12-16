a267427990ca04b8424390ce3f2c4d055373eff2
JianjunJiang <8192542@qq.com>
2020-12-16 11:29:28 +0800

    [arm32]add -mno-unaligned-access for some arm32 platform

--- src/arch/arm32/mach-f1c500s/xboot.mk
+++ src/arch/arm32/mach-f1c500s/xboot.mk
@@ -7,7 +7,7 @@ DEFINES		+= -D__ARM32_ARCH__=5 -D__ARM926EJS__
 ASFLAGS		:= -g -ggdb -Wall -O3
 CFLAGS		:= -g -ggdb -Wall -O3
 LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
-MCFLAGS		:= -march=armv5te -mtune=arm926ej-s -mfloat-abi=soft -marm -mno-thumb-interwork
+MCFLAGS		:= -march=armv5te -mtune=arm926ej-s -mfloat-abi=soft -marm -mno-thumb-interwork -mno-unaligned-access
 
 LIBDIRS		:=
 LIBS 		:=

ea4f1d10b0cf80aa1b724b4fc6f7c8d8d5928bf7
JianjunJiang <8192542@qq.com>
2020-12-18 11:34:48 +0800

    [math]add some macro for math

--- src/include/math.h
+++ src/include/math.h
@@ -10,90 +10,64 @@ extern "C" {
 #include <endian.h>
 #include <limits.h>
 
-typedef float	float_t;
-typedef double	double_t;
+typedef float					float_t;
+typedef double					double_t;
 
-#define FORCE_EVAL(x) do {									\
-	if (sizeof(x) == sizeof(float)) {						\
-		volatile float __x __attribute__((unused));			\
-		__x = (x);											\
-	} else if (sizeof(x) == sizeof(double)) {				\
-		volatile double __x __attribute__((unused));		\
-		__x = (x);											\
-	} else {												\
-		volatile long double __x __attribute__((unused));	\
-		__x = (x);											\
-	}														\
+#define FORCE_EVAL(x) do { \
+	if (sizeof(x) == sizeof(float)) { \
+		volatile float __x __attribute__((unused)); \
+		__x = (x); \
+	} else if (sizeof(x) == sizeof(double)) { \
+		volatile double __x __attribute__((unused)); \
+		__x = (x); \
+	} else { \
+		volatile long double __x __attribute__((unused)); \
+		__x = (x); \
+	} \
 } while(0)
 
-/* Get two 32 bit ints from a double */
-#define EXTRACT_WORDS(hi, lo, d)							\
-do {														\
-	union {double f; uint64_t i;} __u;						\
-	__u.f = (d);											\
-	(hi) = __u.i >> 32;										\
-	(lo) = (uint32_t)__u.i;									\
-} while (0)
+#define asuint(f)				((union {float _f; uint32_t _i;}){f})._i
+#define asfloat(i)				((union {uint32_t _i; float _f;}){i})._f
+#define asuint64(f)				((union {double _f; uint64_t _i;}){f})._i
+#define asdouble(i)				((union {uint64_t _i; double _f;}){i})._f
 
-/* Get the more significant 32 bit int from a double */
-#define GET_HIGH_WORD(hi, d)								\
-do {														\
-	union {double f; uint64_t i;} __u;						\
-	__u.f = (d);											\
-	(hi) = __u.i >> 32;										\
-} while (0)
+#define EXTRACT_WORDS(hi, lo, d) \
+do { \
+	uint64_t __u = asuint64(d); \
+	(hi) = __u >> 32; \
+	(lo) = (uint32_t)__u; \
+} while(0)
 
-/* Get the less significant 32 bit int from a double */
-#define GET_LOW_WORD(lo, d)									\
-do {														\
-	union {double f; uint64_t i;} __u;						\
-	__u.f = (d);											\
-	(lo) = (uint32_t)__u.i;									\
-} while (0)
+#define GET_HIGH_WORD(hi, d) \
+do { \
+	(hi) = asuint64(d) >> 32; \
+} while(0)
 
-/* Set a double from two 32 bit ints */
-#define INSERT_WORDS(d, hi, lo)								\
-do {														\
-	union {double f; uint64_t i;} __u;						\
-	__u.i = ((uint64_t)(hi)<<32) | (uint32_t)(lo);			\
-	(d) = __u.f;											\
-} while (0)
+#define GET_LOW_WORD(lo, d) \
+do { \
+	(lo) = (uint32_t)asuint64(d); \
+} while(0)
 
-/* Set the more significant 32 bits of a double from an int */
-#define SET_HIGH_WORD(d, hi)								\
-do {														\
-	union {double f; uint64_t i;} __u;						\
-	__u.f = (d);											\
-	__u.i &= 0xffffffff;									\
-	__u.i |= (uint64_t)(hi) << 32;							\
-	(d) = __u.f;											\
-} while (0)
+#define INSERT_WORDS(d, hi, lo) \
+do { \
+	(d) = asdouble(((uint64_t)(hi) << 32) | (uint32_t)(lo)); \
+} while(0)
 
-/* Set the less significant 32 bits of a double from an int */
-#define SET_LOW_WORD(d, lo)									\
-do {														\
-	union {double f; uint64_t i;} __u;						\
-	__u.f = (d);											\
-	__u.i &= 0xffffffff00000000ull;							\
-	__u.i |= (uint32_t)(lo);								\
-	(d) = __u.f;											\
-} while (0)
+#define SET_HIGH_WORD(d, hi) \
+	INSERT_WORDS(d, hi, (uint32_t)asuint64(d))
 
-/* Get a 32 bit int from a float */
-#define GET_FLOAT_WORD(w, d)								\
-do {														\
-	union {float f; uint32_t i;} __u;						\
-	__u.f = (d);											\
-	(w) = __u.i;											\
-} while (0)
+#define SET_LOW_WORD(d, lo) \
+	INSERT_WORDS(d, asuint64(d) >> 32, lo)
 
-/* Set a float from a 32 bit int */
-#define SET_FLOAT_WORD(d, w)								\
-do {														\
-	union {float f; uint32_t i;} __u;						\
-	__u.i = (w);											\
-	(d) = __u.f;											\
-} while (0)
+#define GET_FLOAT_WORD(w, d) \
+do { \
+	(w) = asuint(d); \
+} while(0)
+
+#define SET_FLOAT_WORD(d, w) \
+do { \
+	(d) = asfloat(w); \
+} while(0)
 
 #define FP_NAN					0
 #define FP_INFINITE				1
@@ -135,23 +109,28 @@ static __inline unsigned long long __DOUBLE_BITS(double __f)
 
 #define isinf(x) ( \
 	sizeof(x) == sizeof(float) ? (__FLOAT_BITS(x) & 0x7fffffff) == 0x7f800000 : \
-	sizeof(x) == sizeof(double) ? (__DOUBLE_BITS(x) & -1ULL>>1) == 0x7ffULL<<52 : \
+	sizeof(x) == sizeof(double) ? (__DOUBLE_BITS(x) & -1ULL >> 1) == 0x7ffULL << 52 : \
 	__fpclassify(x) == FP_INFINITE)
 
 #define isnan(x) ( \
 	sizeof(x) == sizeof(float) ? (__FLOAT_BITS(x) & 0x7fffffff) > 0x7f800000 : \
-	sizeof(x) == sizeof(double) ? (__DOUBLE_BITS(x) & -1ULL>>1) > 0x7ffULL<<52 : \
+	sizeof(x) == sizeof(double) ? (__DOUBLE_BITS(x) & -1ULL >> 1) > 0x7ffULL << 52 : \
 	__fpclassify(x) == FP_NAN)
 
 #define isnormal(x) ( \
-	sizeof(x) == sizeof(float) ? ((__FLOAT_BITS(x)+0x00800000) & 0x7fffffff) >= 0x01000000 : \
-	sizeof(x) == sizeof(double) ? ((__DOUBLE_BITS(x)+(1ULL<<52)) & -1ULL>>1) >= 1ULL<<53 : \
+	sizeof(x) == sizeof(float) ? ((__FLOAT_BITS(x) + 0x00800000) & 0x7fffffff) >= 0x01000000 : \
+	sizeof(x) == sizeof(double) ? ((__DOUBLE_BITS(x) + (1ULL << 52)) & -1ULL >> 1) >= 1ULL << 53 : \
 	__fpclassify(x) == FP_NORMAL)
 
 #define isfinite(x) ( \
 	sizeof(x) == sizeof(float) ? (__FLOAT_BITS(x) & 0x7fffffff) < 0x7f800000 : \
-	sizeof(x) == sizeof(double) ? (__DOUBLE_BITS(x) & -1ULL>>1) < 0x7ffULL<<52 : \
+	sizeof(x) == sizeof(double) ? (__DOUBLE_BITS(x) & -1ULL >> 1) < 0x7ffULL << 52 : \
 	__fpclassify(x) > FP_INFINITE)
+
+#define signbit(x) ( \
+	sizeof(x) == sizeof(float) ? (int)(__FLOAT_BITS(x) >> 31) : \
+	sizeof(x) == sizeof(double) ? (int)(__DOUBLE_BITS(x) >> 63) : \
+	(int)(__DOUBLE_BITS(x) >> 63))
 
 #define M_E						2.7182818284590452354	/* e */
 #define M_LOG2E					1.4426950408889634074	/* log_2 e */

f4b3a14cf802caf147672af48dfe7a60759930a6
JianjunJiang <8192542@qq.com>
2020-12-18 11:04:34 +0800

    [stdint]add int max and min macro

--- src/include/stdint.h
+++ src/include/stdint.h
@@ -19,10 +19,20 @@ typedef u32_t			uint32_t;
 typedef s64_t			int64_t;
 typedef u64_t			uint64_t;
 
+#define INT8_MIN		(-1 - 0x7f)
+#define INT16_MIN		(-1 - 0x7fff)
+#define INT32_MIN		(-1 - 0x7fffffff)
+#define INT64_MIN		(-1 - 0x7fffffffffffffff)
+
+#define INT8_MAX		(0x7f)
+#define INT16_MAX		(0x7fff)
+#define INT32_MAX		(0x7fffffff)
+#define INT64_MAX		(0x7fffffffffffffff)
+
 #define UINT8_MAX		(0xff)
 #define UINT16_MAX		(0xffff)
-#define UINT32_MAX		(0xffffffff)
-#define UINT64_MAX		(0xffffffffffffffffULL)
+#define UINT32_MAX		(0xffffffffU)
+#define UINT64_MAX		(0xffffffffffffffffU)
 
 #ifdef __cplusplus
 }

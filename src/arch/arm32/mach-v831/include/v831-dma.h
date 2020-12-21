9cbc66a72476574e1962b4b4837703fb0bbe452f
JianjunJiang <8192542@qq.com>
2020-12-21 14:50:25 +0800

    [v831]add v831 dma driver

---
+++ src/arch/arm32/mach-v831/include/v831-dma.h
@@ -0,0 +1,45 @@
+#ifndef __V831_DMA_H__
+#define __V831_DMA_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define V831_DMA_CHANNEL0			(0)
+#define V831_DMA_CHANNEL1			(1)
+#define V831_DMA_CHANNEL2			(2)
+#define V831_DMA_CHANNEL3			(3)
+#define V831_DMA_CHANNEL4			(4)
+#define V831_DMA_CHANNEL5			(5)
+#define V831_DMA_CHANNEL6			(6)
+#define V831_DMA_CHANNEL7			(7)
+
+#define V831_DMA_PORT_SRAM			(0)
+#define V831_DMA_PORT_DRAM			(1)
+#define V831_DMA_PORT_I2S_PCM0		(3)
+#define V831_DMA_PORT_I2S_PCM1		(4)
+#define V831_DMA_PORT_AUDIO_CODEC	(5)
+#define V831_DMA_PORT_GPADC			(12)
+#define V831_DMA_PORT_UART0			(14)
+#define V831_DMA_PORT_UART1			(15)
+#define V831_DMA_PORT_UART2			(16)
+#define V831_DMA_PORT_UART3			(17)
+#define V831_DMA_PORT_SPI0			(22)
+#define V831_DMA_PORT_SPI1			(23)
+#define V831_DMA_PORT_SPI2			(24)
+#define V831_DMA_PORT_OTG_EP1		(30)
+#define V831_DMA_PORT_OTG_EP2		(31)
+#define V831_DMA_PORT_OTG_EP3		(32)
+#define V831_DMA_PORT_OTG_EP4		(33)
+#define V831_DMA_PORT_OTG_EP5		(34)
+#define V831_DMA_PORT_TWI0			(43)
+#define V831_DMA_PORT_TWI1			(44)
+#define V831_DMA_PORT_TWI2			(45)
+#define V831_DMA_PORT_TWI3			(46)
+#define V831_DMA_PORT_S_TWI0		(48)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __V831_DMA_H__ */

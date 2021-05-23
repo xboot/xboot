3a41e56e6f4cfdb8f60b503517b081354628863a
JianjunJiang <8192542@qq.com>
2021-05-23 17:50:38 +0800

    [d1]add get uniqueid interface

--- src/arch/riscv64/mach-d1/include/configs.h
+++ src/arch/riscv64/mach-d1/include/configs.h
@@ -1,6 +1,4 @@
 #ifndef __OVERRIDE_CONFIGS_H__
 #define __OVERRIDE_CONFIGS_H__
 
-#define CONFIG_MAX_SMP_CPUS		(1)
-
 #endif /* __OVERRIDE_CONFIGS_H__ */
--- src/arch/riscv64/mach-d1/nezha.c
+++ src/arch/riscv64/mach-d1/nezha.c
@@ -71,7 +71,16 @@ static void mach_logger(struct machine_t * mach, const char * buf, int count)
 
 static const char * mach_uniqueid(struct machine_t * mach)
 {
-	return NULL;
+	static char uniqueid[32 + 1] = { 0 };
+	virtual_addr_t virt = phys_to_virt(0x03006200);
+	uint32_t sid[4];
+
+	sid[0] = read32(virt + 0 * 4);
+	sid[1] = read32(virt + 1 * 4);
+	sid[2] = read32(virt + 2 * 4);
+	sid[3] = read32(virt + 3 * 4);
+	snprintf(uniqueid, sizeof(uniqueid), "%08x%08x%08x%08x",sid[0], sid[1], sid[2], sid[3]);
+	return uniqueid;
 }
 
 static int mach_keygen(struct machine_t * mach, const char * msg, void * key)

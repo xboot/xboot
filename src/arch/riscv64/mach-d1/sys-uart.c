292f674e25afd8fb1db00a37ce09e08a2f247fe4
JianjunJiang <8192542@qq.com>
2021-05-23 19:23:49 +0800

    [d1]fix sys-uart's pin config

--- src/arch/riscv64/mach-d1/sys-uart.c
+++ src/arch/riscv64/mach-d1/sys-uart.c
@@ -30,6 +30,50 @@
 
 void sys_uart_init(void)
 {
+	virtual_addr_t addr;
+	u32_t val;
+
+	/* Config GPIOB8 and GPIOB9 to txd0 and rxd0 */
+	addr = 0x02000030 + 0x04;
+	val = read32(addr);
+	val &= ~(0xf << ((8 & 0x7) << 2));
+	val |= ((0x6 & 0xf) << ((8 & 0x7) << 2));
+	write32(addr, val);
+
+	val = read32(addr);
+	val &= ~(0xf << ((9 & 0x7) << 2));
+	val |= ((0x6 & 0xf) << ((9 & 0x7) << 2));
+	write32(addr, val);
+
+	/* Open the clock gate for uart0 */
+	addr = 0x0200190c;
+	val = read32(addr);
+	val |= 1 << 0;
+	write32(addr, val);
+
+	/* Deassert uart0 reset */
+	addr = 0x0200190c;
+	val = read32(addr);
+	val |= 1 << 16;
+	write32(addr, val);
+
+	/* Config uart0 to 115200-8-1-0 */
+	addr = 0x02500000;
+	write32(addr + 0x04, 0x0);
+	write32(addr + 0x08, 0xf7);
+	write32(addr + 0x10, 0x0);
+	val = read32(addr + 0x0c);
+	val |= (1 << 7);
+	write32(addr + 0x0c, val);
+	write32(addr + 0x00, 0xd & 0xff);
+	write32(addr + 0x04, (0xd >> 8) & 0xff);
+	val = read32(addr + 0x0c);
+	val &= ~(1 << 7);
+	write32(addr + 0x0c, val);
+	val = read32(addr + 0x0c);
+	val &= ~0x1f;
+	val |= (0x3 << 0) | (0 << 2) | (0x0 << 3);
+	write32(addr + 0x0c, val);
 }
 
 void sys_uart_putc(char c)

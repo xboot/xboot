3f6460b091c77545d37557b80a81d236e1857413
JianjunJiang <8192542@qq.com>
2021-06-17 18:08:11 +0800

    [d1]add -fno-stack-protector flag

--- src/arch/riscv64/mach-d1/xboot.mk
+++ src/arch/riscv64/mach-d1/xboot.mk
@@ -7,7 +7,7 @@ DEFINES		+=
 ASFLAGS		:= -g -ggdb -Wall -O3
 CFLAGS		:= -g -ggdb -Wall -O3
 LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
-MCFLAGS		:= -march=rv64imafdc -mabi=lp64d -mcmodel=medlow
+MCFLAGS		:= -march=rv64imafdc -mabi=lp64d -mcmodel=medlow -fno-stack-protector
 
 LIBDIRS		:=
 LIBS 		:=

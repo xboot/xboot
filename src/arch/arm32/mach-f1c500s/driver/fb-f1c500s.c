5053bb2f0e13eaf3bba9f65906adf80886e28c8a
Jianjun Jiang <8192542@qq.com>
2021-06-28 22:25:56 +0800

    [f1c500s]fix some space fb-f1c500s.c

--- src/arch/arm32/mach-f1c500s/driver/fb-f1c500s.c
+++ src/arch/arm32/mach-f1c500s/driver/fb-f1c500s.c
@@ -417,7 +417,7 @@ static int fb_getbl(struct framebuffer_t * fb)
 static struct surface_t * fb_create(struct framebuffer_t * fb)
 {
 	struct fb_f1c500s_pdata_t * pdat = (struct fb_f1c500s_pdata_t *)fb->priv;
-return surface_alloc(pdat->width, pdat->height, NULL);
+	return surface_alloc(pdat->width, pdat->height, NULL);
 }
 
 static void fb_destroy(struct framebuffer_t * fb, struct surface_t * s)

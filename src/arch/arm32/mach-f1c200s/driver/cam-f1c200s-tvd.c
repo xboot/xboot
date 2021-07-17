1f100398622798865ba7c3365f19e1d74c10fb5e
JianjunJiang <8192542@qq.com>
2021-07-17 09:25:50 +0800

    [tvd]update tvd driver

--- src/arch/arm32/mach-f1c200s/driver/cam-f1c200s-tvd.c
+++ src/arch/arm32/mach-f1c200s/driver/cam-f1c200s-tvd.c
@@ -32,9 +32,20 @@
 #include <interrupt/interrupt.h>
 #include <camera/camera.h>
 
+enum {
+	TVD_ENHANCE1	= 0x000c,
+	TVD_ENHANCE2	= 0x0010,
+	TVD_YADDR		= 0x0080,
+	TVD_CADDR		= 0x0084,
+	TVD_IRQ_STATUS	= 0x0094,
+	TVD_IRQ_CTL		= 0x009c,
+	TVD_MODE		= 0x0f14,
+	TVD_CHROM		= 0x0f1c,
+};
+
 enum tvd_source_t {
-	TVD_SOURCE_PAL,
 	TVD_SOURCE_NTSC,
+	TVD_SOURCE_PAL,
 };
 
 enum tvd_foramt_t {
@@ -54,12 +65,87 @@ struct cam_f1c200s_tvd_pdata_t {
 	enum video_format_t fmt;
 	int width;
 	int height;
+	int buflen;
 	int ready;
 };
+
+static inline void f1c200s_tvd_set_gain(struct cam_f1c200s_tvd_pdata_t * pdat, int gain)
+{
+	u32_t val;
+
+	val = read32(pdat->virt + TVD_CHROM);
+	val &= ~(0xff << 16);
+	val |= (gain & 0xff) << 16;
+	write32(pdat->virt + TVD_CHROM, val);
+}
+
+static inline int f1c200s_tvd_get_gain(struct cam_f1c200s_tvd_pdata_t * pdat)
+{
+	return (read32(pdat->virt + TVD_CHROM) >> 16) & 0xff;
+}
+
+static inline void f1c200s_tvd_set_saturation(struct cam_f1c200s_tvd_pdata_t * pdat, int saturation)
+{
+	u32_t val;
+
+	val = read32(pdat->virt + TVD_ENHANCE2);
+	val &= ~(0xff << 0);
+	val |= (saturation & 0xff) << 0;
+	write32(pdat->virt + TVD_ENHANCE2, val);
+}
+
+static inline int f1c200s_tvd_get_saturation(struct cam_f1c200s_tvd_pdata_t * pdat)
+{
+	return (read32(pdat->virt + TVD_ENHANCE2) >> 0) & 0xff;
+}
+
+static inline void f1c200s_tvd_set_brightness(struct cam_f1c200s_tvd_pdata_t * pdat, int brightness)
+{
+	u32_t val;
+
+	val = read32(pdat->virt + TVD_ENHANCE1);
+	val &= ~(0xff << 8);
+	val |= (brightness & 0xff) << 8;
+	write32(pdat->virt + TVD_ENHANCE1, val);
+}
+
+static inline int f1c200s_tvd_get_brightness(struct cam_f1c200s_tvd_pdata_t * pdat)
+{
+	return (read32(pdat->virt + TVD_ENHANCE1) >> 8) & 0xff;
+}
+
+static inline void f1c200s_tvd_set_contrast(struct cam_f1c200s_tvd_pdata_t * pdat, int contrast)
+{
+	u32_t val;
+
+	val = read32(pdat->virt + TVD_ENHANCE1);
+	val &= ~(0xff << 0);
+	val |= (contrast & 0xff) << 0;
+	write32(pdat->virt + TVD_ENHANCE1, val);
+}
+
+static inline int f1c200s_tvd_get_contrast(struct cam_f1c200s_tvd_pdata_t * pdat)
+{
+	return (read32(pdat->virt + TVD_ENHANCE1) >> 0) & 0xff;
+}
+
+static inline int f1c200s_tvd_get_source(struct cam_f1c200s_tvd_pdata_t * pdat)
+{
+	u32_t val;
+
+	val = read32(pdat->virt + 0x0e40);
+	if((val & 0xe) == 0xe)
+	{
+		if((val & (1 << 18)) && (val & (1 << 16)))
+			return TVD_SOURCE_PAL;
+	}
+	return TVD_SOURCE_NTSC;
+}
 
 static inline void f1c200s_tvd_set_hstart(struct cam_f1c200s_tvd_pdata_t * pdat, int hstart)
 {
 	u32_t val;
+
 	val = read32(pdat->virt + 0x001c);
 	val &= ~(0xfff << 16);
 	val |= (hstart & 0xfff) << 16;
@@ -69,6 +155,7 @@ static inline void f1c200s_tvd_set_hstart(struct cam_f1c200s_tvd_pdata_t * pdat,
 static inline void f1c200s_tvd_set_vstart(struct cam_f1c200s_tvd_pdata_t * pdat, int vstart)
 {
 	u32_t val;
+
 	val = read32(pdat->virt + 0x001c);
 	val &= ~(0x7ff << 0);
 	val |= (vstart & 0x7ff) << 0;
@@ -78,6 +165,7 @@ static inline void f1c200s_tvd_set_vstart(struct cam_f1c200s_tvd_pdata_t * pdat,
 static inline void f1c200s_tvd_set_width(struct cam_f1c200s_tvd_pdata_t * pdat, int width)
 {
 	u32_t val;
+
 	val = read32(pdat->virt + 0x008c + 0x100 * pdat->channel);
 	val &= ~(0xfff << 0);
 	val |= ((width > 720) ? 720 : width) << 0;
@@ -92,6 +180,7 @@ static inline void f1c200s_tvd_set_width_jump(struct cam_f1c200s_tvd_pdata_t * p
 static inline void f1c200s_tvd_set_height(struct cam_f1c200s_tvd_pdata_t * pdat, int height)
 {
 	u32_t val;
+
 	val = read32(pdat->virt + 0x008c + 0x100 * pdat->channel);
 	val &= ~(0x7ff << 16);
 	val |= height << 16;
@@ -122,55 +211,82 @@ static inline void f1c200s_tvd_set_fmt(struct cam_f1c200s_tvd_pdata_t *pdat, enu
 	write32(pdat->virt + 0x0088 + 0x100 * pdat->channel, val);
 }
 
-static inline void f1c200s_tvd_config(struct cam_f1c200s_tvd_pdata_t * pdat, enum tvd_source_t s, enum tvd_foramt_t fmt)
+static inline void f1c200s_tvd_set_blue(struct cam_f1c200s_tvd_pdata_t * pdat, int flag)
 {
 	u32_t val;
 
-	if(s == TVD_SOURCE_PAL)
-	{
-		write32(pdat->virt + 0x0008, 0x01111001);
-		write32(pdat->virt + 0x000c, 0x03714080);
-		write32(pdat->virt + 0x0010, 0x00310080);
-		write32(pdat->virt + 0x0018, 0x2a098acb);
-		write32(pdat->virt + 0x001c, 0x0087002f);
-		write32(pdat->virt + 0x0f08, 0x11590902);
-		write32(pdat->virt + 0x0f0c, 0x00000016);
-		write32(pdat->virt + 0x0f10, 0x008a32ec);
-		write32(pdat->virt + 0x0f14, 0x80000080);
-		write32(pdat->virt + 0x0f1c, 0x00930000);
-		write32(pdat->virt + 0x0f2c, 0x00000d74);
-		write32(pdat->virt + 0x0f44, 0x0000412d);
-		write32(pdat->virt + 0x0f74, 0x00000343);
-		write32(pdat->virt + 0x0f80, 0x00500000);
-		write32(pdat->virt + 0x0f84, 0x00c10000);
-		write32(pdat->virt + 0x0000, 0x00000001);
-		if(fmt == TVD_MB_YUV420)
-		{
-			f1c200s_tvd_set_width(pdat, 704);
-			f1c200s_tvd_set_width_jump(pdat, 704);
-			f1c200s_tvd_set_height(pdat, 224);
-			f1c200s_tvd_set_vstart(pdat, 0x28 + (576 - 448) / 2);
+	val = read32(pdat->virt + TVD_MODE);
+	val &= ~((0x3 << 4) | (0x1 << 8));
+	val |= ((flag & 0x3) << 4);
+	write32(pdat->virt + TVD_MODE, val);
+}
 
-		}
-		else
-		{
-			f1c200s_tvd_set_width(pdat, 720);
-			f1c200s_tvd_set_width_jump(pdat, 720);
-			f1c200s_tvd_set_height(pdat, 288);
-		}
-	}
-	else if(s == TVD_SOURCE_NTSC)
+static inline void f1c200s_tvd_set_addr(struct cam_f1c200s_tvd_pdata_t * pdat, void * y, void * c)
+{
+	u32_t val;
+
+	write32(pdat->virt + TVD_YADDR, (u32_t)y);
+	write32(pdat->virt + TVD_CADDR, (u32_t)c);
+	val = read32(pdat->virt + 0x0088);
+	val |= 1 << 28;
+	write32(pdat->virt + 0x0088, val);
+}
+
+static inline void f1c200s_tvd_irq_enable(struct cam_f1c200s_tvd_pdata_t * pdat)
+{
+	u32_t val;
+
+	val = read32(pdat->virt + TVD_IRQ_CTL);
+	val |= 1 << 24;
+	write32(pdat->virt + TVD_IRQ_CTL, val);
+}
+
+static inline void f1c200s_tvd_irq_disable(struct cam_f1c200s_tvd_pdata_t * pdat)
+{
+	u32_t val;
+
+	val = read32(pdat->virt + TVD_IRQ_CTL);
+	val &= ~(1 << 24);
+	write32(pdat->virt + TVD_IRQ_CTL, val);
+}
+
+static inline void f1c200s_tvd_irq_clear(struct cam_f1c200s_tvd_pdata_t * pdat)
+{
+	write32(pdat->virt + TVD_IRQ_STATUS, 1 << 24);
+}
+
+static inline void f1c200s_tvd_capture_on(struct cam_f1c200s_tvd_pdata_t * pdat)
+{
+	u32_t val;
+
+	val = read32(pdat->virt + 0x0088 + 0x100 * pdat->channel);
+	val |= 1 << 0;
+	write32(pdat->virt + 0x0088 + 0x100 * pdat->channel, val);
+}
+
+static inline void f1c200s_tvd_capture_off(struct cam_f1c200s_tvd_pdata_t * pdat)
+{
+	u32_t val;
+
+	val = read32(pdat->virt + 0x0088 + 0x100 * pdat->channel);
+	val &= ~(1 << 0);
+	write32(pdat->virt + 0x0088 + 0x100 * pdat->channel, val);
+}
+
+static inline void f1c200s_tvd_config(struct cam_f1c200s_tvd_pdata_t * pdat, enum tvd_source_t s, enum tvd_foramt_t fmt)
+{
+	if(s == TVD_SOURCE_NTSC)
 	{
 		write32(pdat->virt + 0x0008, 0x00010001);
-		write32(pdat->virt + 0x000c, 0x00202068);
-		write32(pdat->virt + 0x0010, 0x00300080);
+		write32(pdat->virt + TVD_ENHANCE1, 0x00202068);
+		write32(pdat->virt + TVD_ENHANCE2, 0x00300080);
 		write32(pdat->virt + 0x0018, 0x21f07c1f);
 		write32(pdat->virt + 0x001c, 0x00820022);
 		write32(pdat->virt + 0x0f08, 0x00590100);
 		write32(pdat->virt + 0x0f0c, 0x00000010);
 		write32(pdat->virt + 0x0f10, 0x008A32DD);
-		write32(pdat->virt + 0x0f14, 0x80000080);
-		write32(pdat->virt + 0x0f1c, 0x008A0000);
+		write32(pdat->virt + TVD_MODE, 0x800000a0);
+		write32(pdat->virt + TVD_CHROM, 0x008A0000);
 		write32(pdat->virt + 0x0f2c, 0x0000CB74);
 		write32(pdat->virt + 0x0f44, 0x00004632);
 		write32(pdat->virt + 0x0f74, 0x000003c3);
@@ -191,80 +307,50 @@ static inline void f1c200s_tvd_config(struct cam_f1c200s_tvd_pdata_t * pdat, enu
 			f1c200s_tvd_set_height(pdat, 240);
 		}
 	}
-	val = read32(pdat->virt + 0x0f20);
-	val &= ~((1 << 6) | (1 << 7));
-	val |= ((0 << 6) | (1 << 7));
-	write32(pdat->virt + 0x0f20, val);
-}
-
-static inline void f1c200s_tvd_set_blue(struct cam_f1c200s_tvd_pdata_t * pdat, int flag)
-{
-	u32_t val = read32(pdat->virt + 0xf14);
-	val &= ~(0x3 << 4);
-	val |= ((flag & 0x3) << 4);
-	write32(pdat->virt + 0xf14, val);
-}
-
-static inline void f1c200s_tvd_set_addr_y(struct cam_f1c200s_tvd_pdata_t * pdat, void * addr)
-{
-	u32_t val;
-	write32(pdat->virt + 0x0080, (u32_t)addr);
-	val = read32(pdat->virt + 0x0088);
-	val |= 0x10000000;
-	write32(pdat->virt + 0x0088, val);
-}
-
-static inline void f1c200s_tvd_set_addr_c(struct cam_f1c200s_tvd_pdata_t * pdat, void * addr)
-{
-	u32_t val;
-	write32(pdat->virt + 0x0084, (u32_t)addr);
-	val = read32(pdat->virt + 0x0088);
-	val |= 0x10000000;
-	write32(pdat->virt + 0x0088, val);
-}
-
-static inline void f1c200s_tvd_irq_enable(struct cam_f1c200s_tvd_pdata_t * pdat)
-{
-	u32_t val = read32(pdat->virt + 0x009c);
-	val |= 1 << (24 + pdat->channel);
-	write32(pdat->virt + 0x009c, val);
-}
-
-static inline void f1c200s_tvd_irq_disable(struct cam_f1c200s_tvd_pdata_t * pdat)
-{
-	u32_t val = read32(pdat->virt + 0x009c);
-	val &= ~(1 << (24 + pdat->channel));
-	write32(pdat->virt + 0x009c, val);
-}
-
-static inline void f1c200s_tvd_irq_clear(struct cam_f1c200s_tvd_pdata_t * pdat)
-{
-	write32(pdat->virt + 0x0094, 1 << (24 + pdat->channel));
-}
-
-static inline void f1c200s_tvd_capture_on(struct cam_f1c200s_tvd_pdata_t * pdat)
-{
-	u32_t val = read32(pdat->virt + 0x0088 + 0x100 * pdat->channel);
-	val |= 1 << 0;
-	write32(pdat->virt + 0x0088 + 0x100 * pdat->channel, val);
-}
-
-static inline void f1c200s_tvd_capture_off(struct cam_f1c200s_tvd_pdata_t * pdat)
-{
-	u32_t val = read32(pdat->virt + 0x0088 + 0x100 * pdat->channel);
-	val &= ~(1 << 0);
-	write32(pdat->virt + 0x0088 + 0x100 * pdat->channel, val);
+	else if(s == TVD_SOURCE_PAL)
+	{
+		write32(pdat->virt + 0x0008, 0x01101001);
+		write32(pdat->virt + TVD_ENHANCE1, 0x00202068);
+		write32(pdat->virt + TVD_ENHANCE2, 0x00300050);
+		write32(pdat->virt + 0x0018, 0x2a098acb);
+		write32(pdat->virt + 0x001c, 0x0087002a);
+		write32(pdat->virt + 0x0f08, 0x11590902);
+		write32(pdat->virt + 0x0f0c, 0x00000016);
+		write32(pdat->virt + 0x0f10, 0x008a32ec);
+		write32(pdat->virt + TVD_MODE, 0x800000a0);
+		write32(pdat->virt + TVD_CHROM, 0x00930000);
+		write32(pdat->virt + 0x0f2c, 0x00000d74);
+		write32(pdat->virt + 0x0f44, 0x0000412d);
+		write32(pdat->virt + 0x0f74, 0x00000343);
+		write32(pdat->virt + 0x0f80, 0x00500000);
+		write32(pdat->virt + 0x0f84, 0x00c10000);
+		write32(pdat->virt + 0x0000, 0x00000001);
+		if(fmt == TVD_MB_YUV420)
+		{
+			f1c200s_tvd_set_width(pdat, 704);
+			f1c200s_tvd_set_width_jump(pdat, 704);
+			f1c200s_tvd_set_height(pdat, 288);
+		}
+		else
+		{
+			f1c200s_tvd_set_width(pdat, 720);
+			f1c200s_tvd_set_width_jump(pdat, 720);
+			f1c200s_tvd_set_height(pdat, 288);
+		}
+	}
 }
 
 static inline void f1c200s_tvd_init(struct cam_f1c200s_tvd_pdata_t * pdat)
 {
+	enum tvd_source_t s;
+
 	write32(pdat->virt + 0x0088, 0x04000000);
 	write32(pdat->virt + 0x0070, 0x00000100);
 	if(pdat->channel == 0)
 		write32(pdat->virt + 0x0e04, 0x8002aaa8);
 	else
 		write32(pdat->virt + 0x0e04, 0x8002aaa9);
-	write32(pdat->virt + 0x0e2c, 0x000b0000);
+	write32(pdat->virt + 0x0e2c, 0x00110000);
 	write32(pdat->virt + 0x0040, 0x04000310);
 	write32(pdat->virt + 0x0000, 0x00000000);
 	mdelay(1);
@@ -278,24 +364,43 @@ static inline void f1c200s_tvd_init(struct cam_f1c200s_tvd_pdata_t * pdat)
 	write32(pdat->virt + 0x0f58, 0x00000082);
 	write32(pdat->virt + 0x0f6c, 0x00fffad0);
 	write32(pdat->virt + 0x0f70, 0x0000a010);
+	mdelay(50);
 
-	f1c200s_tvd_config(pdat, TVD_SOURCE_PAL, TVD_PL_YUV420);
-	f1c200s_tvd_set_fmt(pdat, TVD_PL_YUV420);
+	s = f1c200s_tvd_get_source(pdat);
+	if(s == TVD_SOURCE_NTSC)
+	{
+		pdat->fmt = VIDEO_FORMAT_NV12;
+		pdat->width = 720;
+		pdat->height = 480;
+		pdat->buflen = pdat->width * pdat->height * 2;
+		pdat->ready = 0;
+		f1c200s_tvd_config(pdat, s, TVD_PL_YUV420);
+		f1c200s_tvd_set_fmt(pdat, TVD_PL_YUV420);
+		f1c200s_tvd_set_addr(pdat, &pdat->yc[0], &pdat->yc[pdat->width * pdat->height]);
+	}
+	else if(s == TVD_SOURCE_PAL)
+	{
+		pdat->fmt = VIDEO_FORMAT_NV12;
+		pdat->width = 720;
+		pdat->height = 576;
+		pdat->buflen = pdat->width * pdat->height * 2;
+		pdat->ready = 0;
+		f1c200s_tvd_config(pdat, s, TVD_PL_YUV420);
+		f1c200s_tvd_set_fmt(pdat, TVD_PL_YUV420);
+		f1c200s_tvd_set_addr(pdat, &pdat->yc[0], &pdat->yc[pdat->width * pdat->height]);
+	}
 	f1c200s_tvd_set_blue(pdat, 2);
-	f1c200s_tvd_set_addr_y(pdat, &pdat->yc[0]);
-	f1c200s_tvd_set_addr_c(pdat, &pdat->yc[720 * 576]);
-	f1c200s_tvd_irq_disable(pdat);
-	f1c200s_tvd_irq_clear(pdat);
-	f1c200s_tvd_irq_enable(pdat);
 	f1c200s_tvd_capture_off(pdat);
+	f1c200s_tvd_irq_clear(pdat);
+	f1c200s_tvd_irq_disable(pdat);
 }
 
 static int cam_start(struct camera_t * cam, enum video_format_t fmt, int width, int height)
 {
 	struct cam_f1c200s_tvd_pdata_t * pdat = (struct cam_f1c200s_tvd_pdata_t *)cam->priv;
-	pdat->fmt = VIDEO_FORMAT_NV12;
-	pdat->width = 720;
-	pdat->height = 576;
+	f1c200s_tvd_init(pdat);
+	f1c200s_tvd_irq_enable(pdat);
+	f1c200s_tvd_irq_clear(pdat);
 	f1c200s_tvd_capture_on(pdat);
 	return 1;
 }
@@ -304,6 +409,8 @@ static int cam_stop(struct camera_t * cam)
 {
 	struct cam_f1c200s_tvd_pdata_t * pdat = (struct cam_f1c200s_tvd_pdata_t *)cam->priv;
 	f1c200s_tvd_capture_off(pdat);
+	f1c200s_tvd_irq_clear(pdat);
+	f1c200s_tvd_irq_disable(pdat);
 	return 1;
 }
 
@@ -315,7 +422,7 @@ static int cam_capture(struct camera_t * cam, struct video_frame_t * frame)
 		frame->fmt = pdat->fmt;
 		frame->width = pdat->width;
 		frame->height = pdat->height;
-		frame->buflen = pdat->width * pdat->height * 2;
+		frame->buflen = pdat->buflen;
 		frame->buf = pdat->yc;
 		pdat->ready = 0;
 		return 1;
@@ -325,11 +432,24 @@ static int cam_capture(struct camera_t * cam, struct video_frame_t * frame)
 
 static int cam_ioctl(struct camera_t * cam, const char * cmd, void * arg)
 {
+	struct cam_f1c200s_tvd_pdata_t * pdat = (struct cam_f1c200s_tvd_pdata_t *)cam->priv;
+	int * p = arg;
+
 	switch(shash(cmd))
 	{
 	case 0x440a6553: /* "camera-set-gain" */
+		if(p)
+		{
+			f1c200s_tvd_set_gain(pdat, *p);
+			return 0;
+		}
 		break;
 	case 0x30f46ac7: /* "camera-get-gain" */
+		if(p)
+		{
+			*p = f1c200s_tvd_get_gain(pdat);
+			return 0;
+		}
 		break;
 	case 0x8531e7af: /* "camera-set-exposure" */
 		break;
@@ -348,16 +468,46 @@ static int cam_ioctl(struct camera_t * cam, const char * cmd, void * arg)
 	case 0x4e4eff83: /* "camera-get-mirror" */
 		break;
 	case 0xd5d73dfe: /* "camera-set-saturation" */
+		if(p)
+		{
+			f1c200s_tvd_set_saturation(pdat, *p);
+			return 0;
+		}
 		break;
 	case 0x0ed48a72: /* "camera-get-saturation" */
+		if(p)
+		{
+			*p = f1c200s_tvd_get_saturation(pdat);
+			return 0;
+		}
 		break;
 	case 0xdae4842d: /* "camera-set-brightness" */
+		if(p)
+		{
+			f1c200s_tvd_set_brightness(pdat, *p);
+			return 0;
+		}
 		break;
 	case 0x13e1d0a1: /* "camera-get-brightness" */
+		if(p)
+		{
+			*p = f1c200s_tvd_get_brightness(pdat);
+			return 0;
+		}
 		break;
 	case 0xf3916322: /* "camera-set-contrast" */
+		if(p)
+		{
+			f1c200s_tvd_set_contrast(pdat, *p);
+			return 0;
+		}
 		break;
 	case 0xa8290296: /* "camera-get-contrast" */
+		if(p)
+		{
+			*p = f1c200s_tvd_get_contrast(pdat);
+			return 0;
+		}
 		break;
 	default:
 		break;
@@ -417,7 +567,6 @@ static struct device_t * cam_f1c200s_tvd_probe(struct driver_t * drv, struct dtn
 	if(pdat->reset >= 0)
 		reset_deassert(pdat->reset);
 	request_irq(pdat->irq, f1c200s_tvd_interrupt, IRQ_TYPE_NONE, cam);
-	f1c200s_tvd_init(pdat);
 
 	if(!(dev = register_camera(cam, drv)))
 	{

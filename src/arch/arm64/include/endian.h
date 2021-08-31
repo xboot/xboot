8b254d986a785ea8a388dfe6a457bc9bbf5fe483
JianjunJiang <8192542@qq.com>
2021-08-31 17:27:14 +0800

    [endian]update arch endian.h

--- src/arch/arm64/include/endian.h
+++ src/arch/arm64/include/endian.h
@@ -5,8 +5,12 @@
 extern "C" {
 #endif
 
+#ifndef LITTLE_ENDIAN
 #define LITTLE_ENDIAN	(0x1234)
+#endif
+#ifndef BIG_ENDIAN
 #define BIG_ENDIAN		(0x4321)
+#endif
 
 #if ( !defined(__LITTLE_ENDIAN) && !defined(__BIG_ENDIAN) )
 #define __LITTLE_ENDIAN

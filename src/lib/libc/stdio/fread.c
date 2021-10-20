7fb5f19b6bb18978d906c08cccbed5ae721f5794
JianjunJiang <8192542@qq.com>
2021-10-20 11:54:10 +0800

    [stdio]optimize stdio's fread and fwrite function

--- src/lib/libc/stdio/fread.c
+++ src/lib/libc/stdio/fread.c
@@ -7,17 +7,8 @@
 
 size_t fread(void * buf, size_t size, size_t count, FILE * f)
 {
-	unsigned char * p = buf;
-	size_t i;
-
-	for(i = 0; i < count; i++)
-	{
-		if(__stdio_read(f, p, size) != size)
-			break;
-
-		p += size;
-	}
-
-	return i;
+	size_t l = size * count;
+	size_t r = __stdio_read(f, (unsigned char *)buf, l);
+	return r / size;
 }
 EXPORT_SYMBOL(fread);

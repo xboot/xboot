30e7b914537f4e99da36b9941afca6aed2d5f7ae
JianjunJiang <8192542@qq.com>
2022-03-30 14:36:19 +0800

    [l-gpio]fix l-gpio bug, offset must sub chip->base

--- src/framework/hardware/l-gpio.c
+++ src/framework/hardware/l-gpio.c
@@ -36,13 +36,13 @@ struct lgpio_t {
 
 static int l_gpio_new(lua_State * L)
 {
-	int offset = luaL_checkinteger(L, 1);
-	struct gpiochip_t * chip = search_gpiochip(offset);
-	if(chip && (offset >= 0) && (offset < (chip->base + chip->ngpio)))
+	int pin = luaL_checkinteger(L, 1);
+	struct gpiochip_t * chip = search_gpiochip(pin);
+	if(chip)
 	{
 		struct lgpio_t * gpio = lua_newuserdata(L, sizeof(struct lgpio_t));
 		gpio->chip = chip;
-		gpio->offset = offset;
+		gpio->offset = pin - chip->base;
 		luaL_setmetatable(L, MT_HARDWARE_GPIO);
 		return 1;
 	}

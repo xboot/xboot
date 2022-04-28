b9c9cf6225500a465de89e1fd6c6ba921954b91d
Jianjun Jiang <8192542@qq.com>
2022-04-28 22:16:49 +0800

    [localtime_r]add timezone support for localtime_r

--- src/lib/libc/time/localtime_r.c
+++ src/lib/libc/time/localtime_r.c
@@ -3,10 +3,15 @@
  */
 
 #include <time.h>
+#include <xboot/setting.h>
 #include <xboot/module.h>
 
 struct tm * localtime_r(const time_t * t, struct tm * tm)
 {
-	return gmtime_r(t, tm);
+	if(__secs_to_tm(*t + timezone(setting_get("timezone", NULL)) * 60, tm) < 0)
+		return NULL;
+	tm->__tm_gmtoff = 0;
+	tm->__tm_zone = "UTC";
+	return tm;
 }
 EXPORT_SYMBOL(localtime_r);

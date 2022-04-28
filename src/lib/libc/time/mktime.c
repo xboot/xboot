63cbcd59a7db9da288245ae53cc0e9d10fe34a77
Jianjun Jiang <8192542@qq.com>
2022-04-29 00:12:51 +0800

    [time]fix mktime, add timezone support

--- src/lib/libc/time/mktime.c
+++ src/lib/libc/time/mktime.c
@@ -3,10 +3,11 @@
  */
 
 #include <time.h>
+#include <xboot/setting.h>
 #include <xboot/module.h>
 
 time_t mktime(struct tm * tm)
 {
-	return timegm(tm);
+	return __tm_to_secs(tm) - timezone(setting_get("timezone", NULL));
 }
 EXPORT_SYMBOL(mktime);

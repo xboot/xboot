dc24a2581a2dcf918fae0b84dac9a93379001df8
Jianjun Jiang <8192542@qq.com>
2022-04-28 23:04:46 +0800

    [timezone]fix timezone function

--- src/lib/libc/time/localtime_r.c
+++ src/lib/libc/time/localtime_r.c
@@ -8,7 +8,7 @@
 
 struct tm * localtime_r(const time_t * t, struct tm * tm)
 {
-	if(__secs_to_tm(*t + timezone(setting_get("timezone", NULL)) * 60, tm) < 0)
+	if(__secs_to_tm(*t + timezone(setting_get("timezone", NULL)), tm) < 0)
 		return NULL;
 	tm->__tm_gmtoff = 0;
 	tm->__tm_zone = "UTC";

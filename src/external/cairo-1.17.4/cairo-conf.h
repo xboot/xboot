c8b5fb0b0828aa362875921b47516b1786516b1d
JianjunJiang <8192542@qq.com>
2022-04-28 19:51:14 +0800

    [cairo]add HAVE_LOCALTIME_R and HAVE_GMTIME_R support

--- src/external/cairo-1.17.4/cairo-conf.h
+++ src/external/cairo-1.17.4/cairo-conf.h
@@ -13,6 +13,8 @@
 #define CAIRO_NO_MUTEX			(1)
 #define HAVE_UINT64_T			(1)
 #define HAVE_STDINT_H			(1)
+#define HAVE_LOCALTIME_R		(1)
+#define HAVE_GMTIME_R			(1)
 
 #endif /* __CAIRO_CONF_H__ */
 

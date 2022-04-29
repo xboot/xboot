5da2bfe9fd354da9189bb27d330f0c786e07754c
JianjunJiang <8192542@qq.com>
2022-04-29 10:16:32 +0800

    [sandbox]remove to_rtc_time, using secs_to_rtc_time

--- src/arch/x64/mach-sandbox/driver/rtc-sandbox.c
+++ src/arch/x64/mach-sandbox/driver/rtc-sandbox.c
@@ -30,53 +30,6 @@
 #include <rtc/rtc.h>
 #include <sandbox.h>
 
-#define LEAPS_THRU_END(y)	((y) / 4 - (y) / 100 + (y) / 400)
-#define LEAP_YEAR(year)		((!(year % 4) && (year % 100)) || !(year % 400))
-
-static int rtc_month_days(int year, int month)
-{
-	const unsigned char rtc_days_in_month[13] = {
-		0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-	};
-	return rtc_days_in_month[month] + ((LEAP_YEAR(year) && (month == 2)) ? 1 : 0);
-}
-
-static void to_rtc_time(u32_t time, struct rtc_time_t * rt)
-{
-	u32_t month, year;
-	int days;
-	int newdays;
-
-	days = time / 86400;
-	time -= (u32_t)days * 86400;
-
-	rt->week = (days + 4) % 7;
-	year = 1970 + days / 365;
-	days -= (year - 1970) * 365	+ LEAPS_THRU_END(year - 1) - LEAPS_THRU_END(1970 - 1);
-
-	if(days < 0)
-	{
-		year -= 1;
-		days += 365 + LEAP_YEAR(year);
-	}
-	rt->year = year;
-	rt->day = days + 1;
-
-	for(month = 1; month < 12; month++)
-	{
-		newdays = days - rtc_month_days(year, month);
-		if(newdays < 0)
-			break;
-		days = newdays;
-	}
-	rt->month = month;
-	rt->day = days + 1;
-	rt->hour = time / 3600;
-	time -= rt->hour * 3600;
-	rt->minute = time / 60;
-	rt->second = time - rt->minute * 60;
-}
-
 static bool_t rtc_sandbox_settime(struct rtc_t * rtc, struct rtc_time_t * time)
 {
 	return FALSE;
@@ -84,7 +37,7 @@ static bool_t rtc_sandbox_settime(struct rtc_t * rtc, struct rtc_time_t * time)
 
 static bool_t rtc_sandbox_gettime(struct rtc_t * rtc, struct rtc_time_t * time)
 {
-	to_rtc_time((u32_t)(sandbox_realtime() / 1000000000ULL), time);
+	secs_to_rtc_time(sandbox_realtime() / 1000000000ULL, time);
 	return TRUE;
 }
 

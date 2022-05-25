68d950fa85649715fd2644b7235ff3550a403cb0
JianjunJiang <8192542@qq.com>
2022-05-25 10:18:19 +0800

    [stdbool]add stdbool.h header file

---
+++ src/include/stdbool.h
@@ -0,0 +1,21 @@
+#ifndef __STDBOOL_H__
+#define __STDBOOL_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <types.h>
+
+typedef bool_t	bool;
+
+enum {
+	false		= 0,
+	true		= 1,
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STDBOOL_H__ */

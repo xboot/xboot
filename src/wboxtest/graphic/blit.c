beee05d7b23d1f941aaf38b4be2cbdd60df1c21c
JianjunJiang <8192542@qq.com>
2023-05-06 14:55:13 +0800

    [wboxtest][blit]add mini game for blit wboxtest

--- src/wboxtest/graphic/blit.c
+++ src/wboxtest/graphic/blit.c
@@ -8,6 +8,10 @@ struct wbt_blit_pdata_t
 {
 	struct window_t * w;
 	struct surface_t * logo;
+	float minx, miny;
+	float maxx, maxy;
+	float x, y;
+	float vx, vy;
 };
 
 static inline struct surface_t * surface_logo(void)
@@ -39,6 +43,16 @@ static void * blit_setup(struct wboxtest_t * wbt)
 		free(pdat);
 		return NULL;
 	}
+
+	pdat->minx = 0;
+	pdat->miny = 0;
+	pdat->maxx = window_get_width(pdat->w) - surface_get_width(pdat->logo);
+	pdat->maxy = window_get_height(pdat->w) - surface_get_height(pdat->logo);
+	pdat->x = wboxtest_random_float(pdat->minx, pdat->maxx);
+	pdat->y = wboxtest_random_float(pdat->miny, pdat->maxy);
+	pdat->vx = wboxtest_random_float(-10, 10);
+	pdat->vy = wboxtest_random_float(-10, 10);
+
 	return pdat;
 }
 
@@ -60,11 +74,30 @@ static void draw_blit(struct window_t * w, void * o)
 	struct surface_t * s = pdat->w->s;
 	struct surface_t * logo = pdat->logo;
 	struct matrix_t m;
-	int tx, ty;
 
-	tx = wboxtest_random_int(0, surface_get_width(s));
-	ty = wboxtest_random_int(0, surface_get_height(s));
-	matrix_init_translate(&m, tx, ty);
+	pdat->x += pdat->vx;
+	pdat->y += pdat->vy;
+	if(pdat->x > pdat->maxx)
+	{
+		pdat->x = pdat->maxx;
+		pdat->vx = -pdat->vx;
+	}
+	else if(pdat->y > pdat->maxy)
+	{
+		pdat->y = pdat->maxy;
+		pdat->vy = -pdat->vy;
+	}
+	if(pdat->x < pdat->minx)
+	{
+		pdat->x = pdat->minx;
+		pdat->vx = -pdat->vx;
+	}
+	else if(pdat->y < pdat->miny)
+	{
+		pdat->y = pdat->miny;
+		pdat->vy = -pdat->vy;
+	}
+	matrix_init_translate(&m, pdat->x, pdat->y);
 	surface_blit(s, NULL, &m, logo);
 }
 

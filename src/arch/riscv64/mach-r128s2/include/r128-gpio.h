fd6b91c162713695d8aae2c957218967205c2a13
Jianjun Jiang <8192542@qq.com>
2023-05-20 18:08:54 +0800

    [r128]add gpio driver

---
+++ src/arch/riscv64/mach-r128s2/include/r128-gpio.h
@@ -0,0 +1,51 @@
+#ifndef __R128_GPIO_H__
+#define __R128_GPIO_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R128_GPIOA0			(0 + 0)
+#define R128_GPIOA1			(0 + 1)
+#define R128_GPIOA2			(0 + 2)
+#define R128_GPIOA3			(0 + 3)
+#define R128_GPIOA4			(0 + 4)
+#define R128_GPIOA5			(0 + 5)
+#define R128_GPIOA6			(0 + 6)
+#define R128_GPIOA7			(0 + 7)
+#define R128_GPIOA8			(0 + 8)
+#define R128_GPIOA9			(0 + 9)
+#define R128_GPIOA10		(0 + 10)
+#define R128_GPIOA11		(0 + 11)
+#define R128_GPIOA12		(0 + 12)
+#define R128_GPIOA13		(0 + 13)
+#define R128_GPIOA14		(0 + 14)
+#define R128_GPIOA15		(0 + 15)
+#define R128_GPIOA16		(0 + 16)
+#define R128_GPIOA17		(0 + 17)
+#define R128_GPIOA18		(0 + 18)
+#define R128_GPIOA19		(0 + 19)
+#define R128_GPIOA20		(0 + 20)
+#define R128_GPIOA21		(0 + 21)
+#define R128_GPIOA22		(0 + 22)
+#define R128_GPIOA23		(0 + 23)
+#define R128_GPIOA24		(0 + 24)
+#define R128_GPIOA25		(0 + 25)
+#define R128_GPIOA26		(0 + 26)
+#define R128_GPIOA27		(0 + 27)
+#define R128_GPIOA28		(0 + 28)
+#define R128_GPIOA29		(0 + 29)
+
+#define R128_GPIOB0			(32 + 0)
+#define R128_GPIOB1			(32 + 1)
+#define R128_GPIOB2			(32 + 2)
+#define R128_GPIOB3			(32 + 3)
+#define R128_GPIOB4			(32 + 4)
+#define R128_GPIOB14		(32 + 14)
+#define R128_GPIOB15		(32 + 15)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __R128_GPIO_H__ */

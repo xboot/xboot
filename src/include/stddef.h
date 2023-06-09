b52eb3c52936aa4aca0c3d2e611df6030fd4591d
JianjunJiang <8192542@qq.com>
2023-06-09 17:31:07 +0800

    [stddef]add macro of map function

--- src/include/stddef.h
+++ src/include/stddef.h
@@ -6,9 +6,9 @@ extern "C" {
 #endif
 
 #if defined(__cplusplus)
-#define NULL			(0)
+#define NULL								(0)
 #else
-#define NULL			((void *)0)
+#define NULL								((void *)0)
 #endif
 
 #if (defined(__GNUC__) && (__GNUC__ >= 4))
@@ -19,27 +19,28 @@ extern "C" {
 #define container_of(ptr, type, member)		({const typeof(((type *)0)->member) *__mptr = (ptr); (type *)((char *)__mptr - offsetof(type,member));})
 
 #if (defined(__GNUC__) && (__GNUC__ >= 3))
-#define likely(expr)	(__builtin_expect(!!(expr), 1))
-#define unlikely(expr)	(__builtin_expect(!!(expr), 0))
+#define likely(expr)						(__builtin_expect(!!(expr), 1))
+#define unlikely(expr)						(__builtin_expect(!!(expr), 0))
 #else
-#define likely(expr)	(!!(expr))
-#define unlikely(expr)	(!!(expr))
+#define likely(expr)						(!!(expr))
+#define unlikely(expr)						(!!(expr))
 #endif
 
-#define min(a, b)		({typeof(a) _amin = (a); typeof(b) _bmin = (b); (void)(&_amin == &_bmin); _amin < _bmin ? _amin : _bmin;})
-#define max(a, b)		({typeof(a) _amax = (a); typeof(b) _bmax = (b); (void)(&_amax == &_bmax); _amax > _bmax ? _amax : _bmax;})
-#define clamp(v, a, b)	min(max(a, v), b)
+#define map(x, ia, ib, oa, ob)				((x - ia) * (ob - oa) / (ib - ia) + oa)
+#define min(a, b)							({typeof(a) _amin = (a); typeof(b) _bmin = (b); (void)(&_amin == &_bmin); _amin < _bmin ? _amin : _bmin;})
+#define max(a, b)							({typeof(a) _amax = (a); typeof(b) _bmax = (b); (void)(&_amax == &_bmax); _amax > _bmax ? _amax : _bmax;})
+#define clamp(v, a, b)						min(max(a, v), b)
 
-#define ifloor(x)		((x) > 0 ? (int)(x) : (int)((x) - 0.9999999999))
-#define iround(x)		((x) > 0 ? (int)((x) + 0.5) : (int)((x) - 0.5))
-#define iceil(x)		((x) > 0 ? (int)((x) + 0.9999999999) : (int)(x))
-#define idiv255(x)		((((int)(x) + 1) * 257) >> 16)
+#define ifloor(x)							((x) > 0 ? (int)(x) : (int)((x) - 0.9999999999))
+#define iround(x)							((x) > 0 ? (int)((x) + 0.5) : (int)((x) - 0.5))
+#define iceil(x)							((x) > 0 ? (int)((x) + 0.9999999999) : (int)(x))
+#define idiv255(x)							((((int)(x) + 1) * 257) >> 16)
 
-#define X(...)			("" #__VA_ARGS__ "")
+#define X(...)								("" #__VA_ARGS__ "")
 
 enum {
-	FALSE				= 0,
-	TRUE				= 1,
+	FALSE									= 0,
+	TRUE									= 1,
 };
 
 #ifdef __cplusplus
